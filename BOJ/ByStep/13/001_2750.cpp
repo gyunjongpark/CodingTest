f3a6808654420f614548d0b34c7450c0a9b35547
gyunjongpark <idjkda4034@gmail.com>
2024-05-17 21:40:19 +0900

    Create 001_2750_001.cpp

---
+++ BOJ/ByStep/13/001_2750.cpp
@@ -0,0 +1,41 @@
+#include<iostream>
+#include<vector>
+using namespace std;
+
+void selectSort(vector<int>& v) {
+	for (int i = 0; i < v.size(); i++) {
+		int minVal = 1e6; int idx = -1;
+		
+		for (int j = i; j < v.size(); j++) {
+			if (v[j] < minVal) {
+				minVal = v[j];
+				idx = j;
+			}
+		}
+		int temp = v[i];
+		v[i] = v[idx];
+		v[idx] = temp;
+	}
+
+	return;
+}
+
+int main() {
+	ios_base::sync_with_stdio(false);
+	cin.tie(NULL); cout.tie(NULL);
+
+	int n;
+	cin >> n;
+
+	vector<int> v(n);
+
+	for (int i = 0; i < n; i++) {
+		cin >> v[i];
+	}
+
+	selectSort(v);
+
+	for (int i : v) cout << i << '\n';
+
+	return 0;
+}
