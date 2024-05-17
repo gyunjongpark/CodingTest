43b50db0b05b290d0056e9df6025ca06ed22bda5
gyunjongpark <idjkda4034@gmail.com>
2024-05-18 01:26:44 +0900

    Create 003_25305.cpp

---
+++ BOJ/ByStep/13/003_25305.cpp
@@ -0,0 +1,23 @@
+#include<iostream>
+#include<algorithm>
+using namespace std;
+
+int a[1004];
+int n, k;
+
+int main() {
+	ios_base::sync_with_stdio(false);
+	cin.tie(NULL); cout.tie(NULL);
+
+	cin >> n >> k;
+
+	for (int i = 0; i < n; i++) {
+		cin >> a[i];
+	}
+	sort(a, a + n); // O(NlogN)
+	reverse(a, a + n); // O(N)
+
+	cout << a[k - 1];
+
+	return 0;
+}
