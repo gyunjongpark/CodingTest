6b907c67917d37ab5e50a4d69d6fbb359ad1b6a2
gyunjongpark <idjkda4034@gmail.com>
2024-05-18 01:27:42 +0900

    Create 004_2751.cpp

---
+++ BOJ/ByStep/13/004_2751.cpp
@@ -0,0 +1,21 @@
+#include<iostream>
+#include<algorithm>
+#include<vector>
+using namespace std;
+
+vector<int> v;
+int n,temp;
+
+int main() {
+	cin >> n;
+
+	for (int i = 0; i < n; i++) {
+		cin >> temp;
+		v.push_back(temp);
+	}
+	sort(v.begin(), v.end()); //O(NlogN)
+
+	for (int i : v) cout << i << '\n';
+
+	return 0;
+}
