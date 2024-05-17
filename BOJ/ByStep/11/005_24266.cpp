64aa4a3d49142f79de8c7c14d78e50115b00fab7
gyunjongpark <idjkda4034@gmail.com>
2024-05-17 18:19:17 +0900

    Create 005_24266.cpp

---
+++ BOJ/ByStep/11/005_24266.cpp
@@ -0,0 +1,17 @@
+#include<iostream>
+using namespace std;
+typedef long long ll;
+
+ll n;
+
+int main() {
+	ios_base::sync_with_stdio(false);
+	cin.tie(NULL); cout.tie(NULL);
+
+	cin >> n;
+
+	cout << n*n*n << '\n';
+	cout << 3 << '\n';
+
+	return 0;
+}
