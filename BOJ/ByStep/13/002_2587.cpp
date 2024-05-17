79b61d677f04dcd4057b9db08fcb651296a0607d
gyunjongpark <idjkda4034@gmail.com>
2024-05-18 01:24:35 +0900

    Create 002_2587.cpp

---
+++ BOJ/ByStep/13/002_2587.cpp
@@ -0,0 +1,21 @@
+#include<iostream>
+#include<algorithm>
+using namespace std;
+
+int a[5];
+int sum;
+
+int main() {
+	ios_base::sync_with_stdio(false);
+	cin.tie(NULL); cout.tie(NULL);
+
+	for (int i = 0; i < 5; i++) {
+		cin >> a[i];
+	}
+	sort(a, a + 5);
+
+	for (int i : a) sum += i;
+	cout << sum / 5 << '\n' << a[2];
+
+	return 0;
+}
