820dd22dd095bf9a7c68d32903d2de64439bc15c
gyunjongpark <idjkda4034@gmail.com>
2024-05-18 10:32:03 +0900

    Create 006_1427_01.cpp

---
+++ BOJ/ByStep/13/006_1427_01.cpp
@@ -0,0 +1,19 @@
+#include<iostream>
+#include<algorithm>
+using namespace std;
+
+string s;
+
+int main() {
+	ios_base::sync_with_stdio(false);
+	cin.tie(NULL); cout.tie(NULL);
+
+	cin >> s;
+
+	sort(s.begin(), s.end());
+	reverse(s.begin(), s.end());
+
+	cout << s;
+
+	return 0;
+}
