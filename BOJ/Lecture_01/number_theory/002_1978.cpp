cdcd6f43bdcab82e62a2b63d7c42d8a886bb2d39
gyunjongpark <idjkda4034@gmail.com>
2024-05-20 09:19:24 +0900

    Create 002_1978.cpp

---
+++ BOJ/Lecture_01/number_theory/002_1978.cpp
@@ -0,0 +1,28 @@
+#include<iostream>
+#include<math.h>
+using namespace std;
+
+int a, n, ret;
+
+bool check(int n) {
+	if (n == 1) return 0;
+	for (int i = 2; i <= (int)sqrt(n); i++) { //sqrt
+		if (n % i == 0) return 0;
+	}
+	return 1;
+}
+
+int main() {
+	ios_base::sync_with_stdio(false);
+	cin.tie(NULL); cout.tie(NULL);
+
+	cin >> n;
+
+	for (int i = 0; i < n; i++) {
+		cin >> a;
+		if (check(a)) ret++;
+	}
+	cout << ret;
+
+	return 0;
+}
