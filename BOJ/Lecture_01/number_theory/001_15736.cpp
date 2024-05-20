a25b042506828e42272c19f8931d6e07e1a0b856
gyunjongpark <idjkda4034@gmail.com>
2024-05-20 09:13:46 +0900

    Create 001_15736.cpp

---
+++ BOJ/Lecture_01/number_theory/001_15736.cpp
@@ -0,0 +1,15 @@
+#include<iostream>
+#include<math.h>
+using namespace std;
+
+int n;
+
+int main() {
+	ios_base::sync_with_stdio(false);
+	cin.tie(NULL); cout.tie(NULL);
+
+	cin >> n;
+	cout<<(int)sqrt(n);
+	
+	return 0;
+}
