895e15eede0e84bc53842a816a1edf063422eb9c
gyunjongpark <idjkda4034@gmail.com>
2024-05-20 11:22:13 +0900

    Create 004_14232.cpp

---
+++ BOJ/Lecture_01/number_theory/004_14232.cpp
@@ -0,0 +1,31 @@
+#include<iostream>
+#include<cmath>
+#include<vector>
+using namespace std;
+typedef long long ll;
+
+ll n;
+
+int main() {
+	ios_base::sync_with_stdio(false);
+	cin.tie(NULL); cout.tie(NULL);
+
+	cin >> n;
+
+	ll x = n;
+	vector<ll> v;
+
+  //소인수분해
+	for (int i = 2; i <= (int)sqrt(n); i++) {
+		while (x % i == 0) {
+			v.push_back(i);
+			x /= i;
+		}
+	}
+	if (x != 1) v.push_back(x);
+
+	cout << v.size() << '\n';
+	for (ll i : v) cout << i << ' ';
+
+	return 0;
+}
