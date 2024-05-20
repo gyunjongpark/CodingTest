fca11597a7b5bff4d1a7b43215564039eaf73153
gyunjongpark <idjkda4034@gmail.com>
2024-05-20 21:12:00 +0900

    Create 005_2581.cpp

---
+++ BOJ/ByStep/09/005_2581.cpp
@@ -0,0 +1,33 @@
+#include<iostream>
+#include<cmath>
+using namespace std;
+
+int n, m, ret, minVal=1e6;
+
+bool check(int n) {
+	if (n == 1) return 0;
+  
+	for (int i = 2; i <= (int)sqrt(n); i++) {
+		if (n % i == 0) return 0;
+	}
+  
+	return 1;
+}
+
+int main() {
+	ios_base::sync_with_stdio(false);
+	cin.tie(NULL); cout.tie(NULL);
+
+	cin >> n >> m;
+
+	for (int i = m; i >= n; i--) {
+		if (check(i)) {
+			ret += i;
+			minVal = i;
+		}
+	}
+	if (ret == 0) cout << -1;
+	else cout << ret << '\n' << minVal;
+
+	return 0;
+}
