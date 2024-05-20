653273e94074902732aa1446d7731556343b2565
gyunjongpark <idjkda4034@gmail.com>
2024-05-20 14:06:55 +0900

    Update 003_11653.cpp

--- BOJ/Lecture_01/number_theory/003_11653.cpp
+++ BOJ/Lecture_01/number_theory/003_11653.cpp
@@ -15,7 +15,9 @@ int main() {
 	
 	/*
 	sqrt(n)까지만 조사해도 되는 이유
-	제곱근 이상의 수는 반드시 제곱근 이하의 수와 짝을 이루기 때문입니다.
+	n=p*q에서 p>sqrt(n) && q>sqrt(n)인 합성수는 존재하지 않음
+ 	p>sqrt(n)라면 반드시 q<sqrt(n)이어야 함
+  	따라서, 제곱근보다 큰 소인수는 많아야 한 개이다.
 	*/
 	
 	for (int i = 2; i <= (int)sqrt(n); i++) {
@@ -24,7 +26,7 @@ int main() {
 			x /= i;
 		}
 	}
-	if (x != 1) cout << x << '\n';
+	if (x != 1) cout << x << '\n'; //가장 큰 소인수 출력
 
 	return 0;
 }
