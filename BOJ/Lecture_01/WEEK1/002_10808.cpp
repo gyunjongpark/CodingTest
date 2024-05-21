0bac7b812fef4faf31e27169d5800bddc044f48c
gyunjongpark <idjkda4034@gmail.com>
2024-05-21 22:56:41 +0900

    Create 002_10808.cpp

---
+++ BOJ/Lecture_01/WEEK1/002_10808.cpp
@@ -0,0 +1,18 @@
+#include<iostream>
+using namespace std;
+
+string s;
+int cnt[26]; //a~z
+
+int main() {
+	ios_base::sync_with_stdio(false);
+	cin.tie(NULL); cout.tie(NULL);
+
+	cin >> s;
+
+	for (char c : s) cnt[c - 'a']++;
+
+	for (int i : cnt) cout << i << ' ';
+
+	return 0;
+}
