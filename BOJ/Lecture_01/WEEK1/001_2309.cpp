ecc1c1f0fdeac2daa68362af497438e94a60ad4d
gyunjongpark <idjkda4034@gmail.com>
2024-05-21 22:46:33 +0900

    Update 001_2309.cpp

--- BOJ/Lecture_01/WEEK1/001_2309.cpp
+++ BOJ/Lecture_01/WEEK1/001_2309.cpp
@@ -1,35 +1,40 @@
 #include<iostream>
-#include<vector>
 #include<algorithm>
+#include<vector>
 using namespace std;
 
-int a[9], sum;
-pair<int, int> idx;
+int a[9];
+pair<int, int> pi;
 vector<int> v;
 
 int main() {
 	ios_base::sync_with_stdio(false);
 	cin.tie(NULL); cout.tie(NULL);
 
+	
+	int sum = 0;
 	for (int i = 0; i < 9; i++) {
 		cin >> a[i];
 		sum += a[i];
 	}
 
-	//완전탐색. 시간과 메모리가 허용된다면 무식하게 풀 생각을 하자!
+	//2초, 완전탐색으로 구현
+	//시간과 메모리가 허용된다면 무식하게 먼저 풀어야 한다.
 	for (int i = 0; i < 9; i++) {
 		for (int j = 0; j < i; j++) {
 			if (sum - a[i] - a[j] == 100) {
-				idx.first = i, idx.second = j;
+				pi.first = i, pi.second = j;
 			}
 		}
 	}
-	
+
 	for (int i = 0; i < 9; i++) {
-		if (i == idx.first || i == idx.second) continue;
+		if (i == pi.first || i == pi.second) continue;
 		v.push_back(a[i]);
 	}
-	sort(v.begin(), v.end()); //NlogN
+
+	sort(v.begin(), v.end());
+
 	for (int i : v) cout << i << '\n';
 
 	return 0;
