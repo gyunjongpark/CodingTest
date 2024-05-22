21b2e5a5963520fcea1537d5db89de91d55bc0ef
gyunjongpark <idjkda4034@gmail.com>
2024-05-22 11:47:19 +0900

    Create 015_4375.cpp

---
+++ BOJ/Lecture_01/WEEK1/015_4375.cpp
@@ -0,0 +1,28 @@
+#include<iostream>
+#include<string> //to_string
+using namespace std;
+
+int n;
+
+int main() {
+	ios_base::sync_with_stdio(false);
+	cin.tie(NULL); cout.tie(NULL);
+
+	while (cin >> n) {
+		int ret = 1, cnt = 1;
+
+		while (true) {
+			if (ret % n == 0) {
+				cout << cnt << '\n';
+				break;
+			}
+			else {
+				ret = ret * 10 + 1;
+				ret %= n;
+				cnt++;
+			}
+		}
+	}
+	
+	return 0;
+}
