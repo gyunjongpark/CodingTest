3ab9d42924b3d1e340fc89cbc7e9e3c88cec9531
gyunjongpark <idjkda4034@gmail.com>
2024-05-23 09:04:28 +0900

    Create 002_1012.cpp

---
+++ BOJ/Lecture_01/WEEK2/002_1012.cpp
@@ -0,0 +1,55 @@
+#include<iostream>
+#define MAX 51
+using namespace std;
+
+const int dy[] = { -1,0,1,0 };
+const int dx[] = { 0,1,0,-1 };
+int m, n, t, k, x, y;
+bool a[MAX][MAX], visited[MAX][MAX];
+
+void dfs(int y, int x) {
+	visited[y][x] = 1;
+
+	for (int i = 0; i < 4; i++) {
+		int ny = y + dy[i];
+		int nx = x + dx[i];
+
+		if (ny < 0 || ny >= n || nx < 0 || nx >= m) continue;
+		if (a[ny][nx] == 1 && !visited[ny][nx]) {
+			dfs(ny, nx);
+		}
+	}
+
+	return;
+}
+
+int main() {
+	ios_base::sync_with_stdio(false);
+	cin.tie(NULL); cout.tie(NULL);
+
+	cin >> t;
+
+	while (t--) {
+		fill(&visited[0][0], &visited[0][0] + MAX * MAX, 0);
+		fill(&a[0][0], &a[0][0] + MAX * MAX, 0);
+		int ret = 0;
+
+		cin >> m >> n >> k; // x>>y>>개수
+		
+		while (k--) {
+			cin >> x >> y;
+			a[y][x] = 1;
+		}
+
+		for (int i = 0; i < n; i++) {
+			for (int j = 0; j < m; j++) {
+				if (a[i][j] == 1 && !visited[i][j]) {
+					dfs(i, j); ret++;
+				}
+			}
+		}
+		cout << ret << '\n';
+	}
+
+	return 0;
+}
