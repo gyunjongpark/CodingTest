df6e532c38feda14a07475fce7743eeecb9af971
gyunjongpark <idjkda4034@gmail.com>
2024-05-25 21:55:52 +0900

    Update 016_14502.cpp

--- BOJ/Lecture_01/WEEK2/016_14502.cpp
+++ BOJ/Lecture_01/WEEK2/016_14502.cpp
@@ -1,42 +1,41 @@
 #include<iostream>
 #include<vector>
+#define MAX 65
 using namespace std;
 
+vector<pair<int, int>> virusList, wallList;
 const int dy[] = { -1,0,1,0 };
 const int dx[] = { 0,1,0,-1 };
-int ret, n, m, a[10][10];
-bool visited[10][10];
-vector<pair<int, int>> virusList, wallList;
+bool visited[MAX][MAX];
+int n, m, a[MAX][MAX];
 
 void dfs(int y, int x) {
-	visited[y][x] = 1;
+	visited[y][x] = 1; //방문 처리로 오염시키기
 
 	for (int i = 0; i < 4; i++) {
 		int ny = y + dy[i];
 		int nx = x + dx[i];
 
 		if (ny < 0 || ny >= n || nx < 0 || nx >= m) continue;
-		if (a[ny][nx]!=1 && !visited[ny][nx]) dfs(ny, nx);
+		if (a[ny][nx] == 0 && !visited[ny][nx]) { //오염 가능한 지역 dfs
+			dfs(ny, nx);
+		}
 	}
 
 	return;
 }
 
-int solve() {
-	fill(&visited[0][0], &visited[0][0] + 10 * 10, 0);
-
-	for (pair<int, int> vList : virusList) {
-		dfs(vList.first, vList.second); //sperse virus
+int go() {
+	for (pair<int, int> virus : virusList) {
+		dfs(virus.first, virus.second); //바이러스 시작점에서부터 dfs 시작
 	}
 
 	int cnt = 0;
-
 	for (int i = 0; i < n; i++) {
 		for (int j = 0; j < m; j++) {
-			if (a[i][j] == 0 && !visited[i][j]) cnt++;
+			if (a[i][j] == 0 && !visited[i][j]) cnt++; //오염되지 않은 지역 count
 		}
 	}
-  
 	return cnt;
 }
 
@@ -49,23 +48,26 @@ int main() {
 	for (int i = 0; i < n; i++) {
 		for (int j = 0; j < m; j++) {
 			cin >> a[i][j];
-			if (a[i][j] == 2) virusList.push_back({ i,j });
 			if (a[i][j] == 0) wallList.push_back({ i,j });
+			else if (a[i][j] == 2) virusList.push_back({ i,j });
 		}
 	}
 
+	int ret = -1; //max count
 	for (int i = 0; i < wallList.size(); i++) {
 		for (int j = 0; j < i; j++) {
 			for (int k = 0; k < j; k++) {
-				a[wallList[i].first][wallList[i].second] = 1;
-				a[wallList[j].first][wallList[j].second] = 1;
-				a[wallList[k].first][wallList[k].second] = 1;
+				fill(&visited[0][0], &visited[0][0] + MAX * MAX, 0); //방문 초기화
 
-				ret = max(ret, solve());
+				visited[wallList[i].first][wallList[i].second] = 1;
+				visited[wallList[j].first][wallList[j].second] = 1;
+				visited[wallList[k].first][wallList[k].second] = 1;
 
-				a[wallList[i].first][wallList[i].second] = 0;
-				a[wallList[j].first][wallList[j].second] = 0;
-				a[wallList[k].first][wallList[k].second] = 0;
+				ret = max(ret, go());
+
+				visited[wallList[i].first][wallList[i].second] = 0;
+				visited[wallList[j].first][wallList[j].second] = 0;
+				visited[wallList[k].first][wallList[k].second] = 0;
 			}
 		}
 	}
