1e0a63592e9ad9866ba229f643f763e7b23b3da8
gyunjongpark <idjkda4034@gmail.com>
2024-05-25 14:39:28 +0900

    Create 017_2636.cpp

---
+++ BOJ/Lecture_01/WEEK2/017_2636.cpp
@@ -0,0 +1,68 @@
+#include<iostream>
+#include<vector>
+#define MAX 101
+using namespace std;
+
+bool visited[MAX][MAX], a[MAX][MAX];
+const int dy[] = { -1,0,1,0 };
+const int dx[] = { 0,1,0,-1 };
+int n, m, cnt, ret;
+vector<pair<int, int>> v;
+
+void dfs(int y, int x) {
+	visited[y][x] = 1;
+
+	if (a[y][x] == 1) {
+		v.push_back({ y,x }); //녹을 치즈 좌표 저장하기
+		return;
+	}
+
+	for (int i = 0; i < 4; i++) {
+		int ny = y + dy[i];
+		int nx = x + dx[i];
+
+		if (ny < 0 || ny >= n || nx < 0 || nx >= m) continue;
+		if (!visited[ny][nx]) {
+			dfs(ny, nx);
+		}
+	}
+
+	return;
+}
+
+int main() {
+	ios_base::sync_with_stdio(false);
+	cin.tie(NULL); cout.tie(NULL);
+
+	cin >> n >> m;
+
+	for (int i = 0; i < n; i++) {
+		for (int j = 0; j < m; j++) {
+			cin >> a[i][j];
+		}
+	}
+	while (true) {
+		fill(&visited[0][0], &visited[0][0] + MAX * MAX, 0);
+		v.clear();
+
+		dfs(0, 0);
+
+		for (pair<int, int> i : v) { //치즈 녹이기
+			a[i.first][i.second] = 0;
+		}
+		ret = v.size(); //녹인 치즈의 개수
+		cnt++; // 녹인 횟수 count
+
+		bool flag = 0;
+
+		for (int i = 0; i < n; i++) {
+			for (int j = 0; j < m; j++) {
+				if (a[i][j]) flag = 1; //녹일 치즈가 남아있다면 true
+			}
+		}
+		if (!flag) break;
+	}
+	cout << cnt << '\n' << ret;
+	
+	return 0;
+}
