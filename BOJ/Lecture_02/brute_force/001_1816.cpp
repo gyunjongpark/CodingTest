1e780b77f3fe9a8181428540c558dc8a4a37bc1d
gyunjongpark <idjkda4034@gmail.com>
2024-05-31 16:16:59 +0900

    Create 001_1816.cpp

---
+++ BOJ/Lecture_02/brute_force/001_1816.cpp
@@ -0,0 +1,31 @@
+#include<iostream>
+using namespace std;
+typedef long long ll;
+
+int n;
+ll s;
+
+int main() {
+	ios_base::sync_with_stdio(false);
+	cin.tie(NULL); cout.tie(NULL);
+
+
+	cin >> n;
+
+	while (n--) {
+		bool flag = 0;
+		cin >> s;
+
+		for (int i = 2; i < 1000001; i++) {
+			if (s % i == 0) {
+				flag = 1;
+				break;
+			}
+		}
+    
+		if(flag) cout << "NO\n";
+		else cout << "YES\n";
+	}
+
+	return 0;
+}
