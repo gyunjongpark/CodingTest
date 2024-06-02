06403a0c02be4ee7d74544ee0b0040ed8005bfb7
gyunjongpark <idjkda4034@gmail.com>
2024-06-02 21:45:05 +0900

    Create 005_1407.cpp

---
+++ BOJ/Lecture_02/number_theory/005_1407.cpp
@@ -0,0 +1,28 @@
+#include<iostream>
+#include<cmath>
+#include<vector>
+using namespace std;
+typedef long long ll;
+
+ll a, b;
+
+ll solve(ll N) { //1부터 N까지의 2의 거듭 제곱 수의 합
+	ll ret = N;
+
+	for (ll i = 2; i <= N; i *= 2) {
+		ret += (N / i) * (i / 2);
+	}
+
+	return ret;
+}
+
+int main() {
+	ios_base::sync_with_stdio(false);
+	cin.tie(NULL); cout.tie(NULL);
+
+	cin >> a >> b;
+
+	cout << solve(b) - solve(a - 1);
+	
+	return 0;
+}
