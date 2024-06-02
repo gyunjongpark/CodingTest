8d5c224481b63c55a440136dca999051ce5b3be4
gyunjongpark <idjkda4034@gmail.com>
2024-06-02 19:53:27 +0900

    Create 003_11653.cpp

---
+++ BOJ/Lecture_02/number_theory/003_11653.cpp
@@ -0,0 +1,26 @@
+#include<iostream>
+#include<cmath>
+using namespace std;
+
+int n;
+
+int main() {
+	ios_base::sync_with_stdio(false);
+	cin.tie(NULL); cout.tie(NULL);
+
+	cin >> n;
+
+	int x = n; //소인수 분해를 위한 변수 x
+	for (int i = 2; i <= (int)sqrt(n); i++) {
+		while (true) {
+			if (x % i == 0) {
+				cout << i << '\n';
+				x /= i;
+			}
+			else break;
+		}
+	}
+	if (x != 1) cout << x << '\n'; //가장 큰 소인수 출력
+
+	return 0;
+}
