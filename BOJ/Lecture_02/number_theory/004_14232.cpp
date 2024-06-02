faf061b303ac996e691597b07792051517bc02bb
gyunjongpark <idjkda4034@gmail.com>
2024-06-02 19:55:02 +0900

    Create 004_14232.cpp

---
+++ BOJ/Lecture_02/number_theory/004_14232.cpp
@@ -0,0 +1,30 @@
+#include<iostream>
+#include<cmath> //sqrt
+#include<vector>
+using namespace std;
+typedef long long ll;
+
+ll n;
+
+int main() {
+	ios_base::sync_with_stdio(false);
+	cin.tie(NULL); cout.tie(NULL);
+
+	cin >> n;
+
+	ll x = n;
+	vector<ll> v;
+
+	for (int i = 2; i <= (int)sqrt(n); i++) {
+		while (x % i == 0) {
+			v.push_back(i);
+			x /= i;
+		}
+	}
+	if (x != 1) v.push_back(x);
+
+	cout << v.size() << '\n';
+	for (ll i : v) cout << i << ' ';
+
+	return 0;
+}
