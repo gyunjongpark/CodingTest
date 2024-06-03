4847746920e68db1d7f424765e72e27c42254c7b
gyunjongpark <idjkda4034@gmail.com>
2024-06-03 15:27:20 +0900

    Update 002_2589_02.cpp

--- BOJ/Lecture_01/WEEK3/002_2589_02.cpp
+++ BOJ/Lecture_01/WEEK3/002_2589_02.cpp
@@ -8,50 +8,55 @@ const int dy[] = { -1, 0, 1, 0 };
 const int dx[] = { 0, 1, 0, -1 };
 int n, m, ypos, xpos, visited[MAX][MAX];
 int maxTime = -1;
-string s, a[MAX][MAX];
+char a[MAX][MAX];
+string s;
 
 void bfs(int y, int x) {
-    fill(&visited[0][0], &visited[0][0] + MAX * MAX, 0); //init
+	fill(&visited[0][0], &visited[0][0] + MAX * MAX, 0);
 
-    visited[y][x] = 1;
-    queue<pair<int, int>> q;
-    q.push({ y, x });
+	visited[y][x] = 1;
+	queue<pair<int, int>> q;
+	q.push({ y,x });
 
-    while (q.size()) {
-        tie(ypos, xpos) = q.front(); q.pop();
-        for (int i = 0; i < 4; i++) {
-            int ny = ypos + dy[i];
-            int nx = xpos + dx[i];
-            if (ny < 0 || ny >= n || nx < 0 || nx >= m) continue;
-            if (a[ny][nx] == "L" && !visited[ny][nx]) {
-                visited[ny][nx] = visited[ypos][xpos] + 1;
-                q.push({ ny, nx });
-                maxTime = max(maxTime, visited[ny][nx]);
-            }
-        }
-    }
+	while (q.size()) {
+		tie(ypos, xpos) = q.front(); q.pop();
 
-    return;
+		for (int i = 0; i < 4; i++) {
+			int ny = ypos + dy[i];
+			int nx = xpos + dx[i];
+            
+			if (ny < 0 || ny >= n || nx < 0 || nx >= m) continue;
+			if (a[ny][nx] == 'L' && !visited[ny][nx]) {
+				visited[ny][nx] = visited[ypos][xpos] + 1;
+				q.push({ ny,nx });
+                
+				maxTime = max(maxTime, visited[ny][nx]);
+			}
+		}
+	}
+
+	return;
 }
 
 int main() {
-    ios_base::sync_with_stdio(false);
-    cin.tie(NULL); cout.tie(NULL);
+	ios_base::sync_with_stdio(false);
+	cin.tie(NULL); cout.tie(NULL);
 
-    cin >> n >> m;
-    for (int i = 0; i < n; i++) {
-        cin >> s;
-        for (int j = 0; j < m; j++) {
-            a[i][j] = s[j];
-        }
-    }
+	cin >> n >> m;
 
-    for (int i = 0; i < n; i++) {
-        for (int j = 0; j < m; j++) {
-            if (a[i][j] == "L") bfs(i, j);
-        }
-    }
-    cout << maxTime - 1 << "\n";
+	for (int i = 0; i < n; i++) {
+		cin >> s;
+		for (int j = 0; j < m; j++) {
+			a[i][j] = s[j];
+		}
+	}
 
-    return 0;
+	for (int i = 0; i < n; i++) {
+		for (int j = 0; j < m; j++) {
+			if (a[i][j] == 'L') bfs(i, j);
+		}
+	}
+	cout << maxTime - 1;
+
+	return 0;
 }
