d861eb8f939d0451360a60bc81f09d13e49081d0
gyunjongpark <idjkda4034@gmail.com>
2024-06-08 16:06:36 +0900

    Update 007_14252.cpp

--- BOJ/Lecture_02/number_theory/007_14252.cpp
+++ BOJ/Lecture_02/number_theory/007_14252.cpp
@@ -16,35 +16,30 @@ int main() {
 	cin.tie(NULL); cout.tie(NULL);
 
 	cin >> n;
-
 	for (int i = 0; i < n; i++) {
 		cin >> a[i];
 	}
 	sort(a, a + n);
 
-	int ret = 0;
 	for (int i = 0; i < n - 1; i++) {
 		if (gcd(a[i], a[i + 1]) == 1) continue;
 
 		for (int j = a[i] + 1; j < a[i + 1]; j++) {
-			int cnt = 0;
-			if (gcd(a[i], j) == 1) cnt++;
-			if (gcd(j, a[i + 1]) == 1) cnt++;
-
-			//j 하나만 삽입해도 조건 만족. +1
-			if (cnt > 1) { 
-				ret++;
-				break;
+			if (gcd(a[i], j) == 1) {
+				if (gcd(a[i + 1], j) == 1) {
+					cnt++;
+					break;
+				}
 			}
 
-			//범위의 마지막 요소까지 접근 시 2개 추가. +2
+			//마지막 요소 접근 시 +2
 			if (j == a[i + 1] - 1) {
-				ret += 2;
+				cnt += 2;
 				break;
 			}
 		}
 	}
-	cout << ret;
+	cout << cnt;
 
 	return 0;
 }
