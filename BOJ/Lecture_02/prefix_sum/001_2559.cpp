51a8bdddea1e67e9bf3f1916a69f6ec54ba7cc70
gyunjongpark <idjkda4034@gmail.com>
2024-06-09 08:52:51 +0900

    Update 001_2559.cpp

--- BOJ/Lecture_02/prefix_sum/001_2559.cpp
+++ BOJ/Lecture_02/prefix_sum/001_2559.cpp
@@ -3,7 +3,6 @@
 using namespace std;
 
 int n, k, temp, s[MAX];
-int maxVal = -10000001; //최악의 경우 -100 * 10만
 
 int main() {
 	ios_base::sync_with_stdio(false);
@@ -11,12 +10,12 @@ int main() {
 
 	cin >> n >> k;
 
-	//누적합
 	for (int i = 1; i <= n; i++) {
 		cin >> temp;
 		s[i] = s[i - 1] + temp;
 	}
 
+	int maxVal = -10000001; //최악의 경우 -100*10만
 	for (int i = k; i <= n; i++) {
 		maxVal = max(maxVal, (s[i] - s[i - k]));
 	}
