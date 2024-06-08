c8c7de4546d6aae7089fa28cd38519c222eefa91
gyunjongpark <idjkda4034@gmail.com>
2024-06-08 15:33:55 +0900

    Update 002_1978.cpp

--- BOJ/Lecture_02/number_theory/002_1978.cpp
+++ BOJ/Lecture_02/number_theory/002_1978.cpp
@@ -6,7 +6,7 @@ int n, temp, cnt;
 
 bool check(int n) {
 	if (n <= 1) return 0;
-	for (int i = 2; i <= sqrt(n); i++) {
+	for (int i = 2; i <= (int)sqrt(n); i++) {
 		if (n % i == 0) return 0;
 	}
 	return 1;
