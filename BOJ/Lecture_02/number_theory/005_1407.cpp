073a4f1a606d171c760197842f504131a89732f6
gyunjongpark <idjkda4034@gmail.com>
2024-06-08 15:52:01 +0900

    Update 005_1407.cpp

--- BOJ/Lecture_02/number_theory/005_1407.cpp
+++ BOJ/Lecture_02/number_theory/005_1407.cpp
@@ -4,7 +4,9 @@ typedef long long ll;
 
 ll a, b;
 
-ll solve(ll N) { //1부터 N까지 각각의 수에 포함된 2의 최대 승수를 구해서 더함
+//1부터 N까지 각각의 수에 포함된 2의 최대 승수를 구해서 더함
+//이해보다 암기가 좋다
+ll solve(ll N) {
 	ll ret = N;
 
 	for (ll i = 2; i <= N; i *= 2) {
