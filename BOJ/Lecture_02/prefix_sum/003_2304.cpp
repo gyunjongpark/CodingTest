b99aa693bc26c6d22ec8fd0fb57ea14639381580
gyunjongpark <idjkda4034@gmail.com>
2024-06-09 10:55:28 +0900

    Create 003_2304.cpp

---
+++ BOJ/Lecture_02/prefix_sum/003_2304.cpp
@@ -0,0 +1,41 @@
+#include<iostream>
+#define MAX 1004
+using namespace std;
+
+int n, x, y, a[MAX], s[MAX];
+int maxPos, maxVal;
+
+int main() {
+	ios_base::sync_with_stdio(false);
+	cin.tie(NULL); cout.tie(NULL);
+
+	cin >> n;
+
+	for (int i = 0; i < n; i++) {
+		cin >> x >> y;
+		a[x] = y;
+		if (y >= maxVal) {
+			maxVal = y;
+			maxPos = x;
+		}
+	}
+
+	for (int i = 1; i < maxPos; i++) {
+		if (a[i - 1] >= a[i]) {
+			a[i] = a[i - 1];
+		}
+	}
+
+	for (int i = 1000; i>maxPos; i--) {
+		if (a[i + 1] >= a[i]) {
+			a[i] = a[i + 1];
+		}
+	}
+
+	for (int i = 1; i <= 1000; i++) {
+		s[i] = s[i - 1] + a[i];
+	}
+	cout << s[1000];
+
+	return 0;
+}
