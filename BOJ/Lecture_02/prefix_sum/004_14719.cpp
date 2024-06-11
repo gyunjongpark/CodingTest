55e3839a791a8f1c9923c4bf0112160b1630a49a
gyunjongpark <idjkda4034@gmail.com>
2024-06-11 14:23:58 +0900

    Create 004_14719.cpp

---
+++ BOJ/Lecture_02/prefix_sum/004_14719.cpp
@@ -0,0 +1,45 @@
+#include<iostream>
+#define MAX 504
+using namespace std;
+
+int n, m, temp, maxVal, maxPos, sum, a[MAX];
+
+int main() {
+	ios_base::sync_with_stdio(false);
+	cin.tie(NULL); cout.tie(NULL);
+
+	cin >> n >> m;
+
+	for (int i = 1; i <= m; i++) {
+		cin >> temp;
+		a[i] = temp;
+
+		if (temp >= maxVal) {
+			maxVal = temp;
+			maxPos = i;
+		}
+	}
+
+	int maxVal_front = a[1];
+	for (int i = 1; i < maxPos; i++) {
+		if (a[i - 1] >= a[i]) {
+			maxVal_front = a[i - 1];
+			sum += maxVal_front - a[i];
+			a[i] = maxVal_front;
+		}
+		else maxVal_front = a[i];
+	}
+
+	int maxVal_back = a[m];
+	for (int i = m; i > maxPos; i--) {
+		if (a[i + 1] >= a[i]) {
+			maxVal_back = a[i + 1];
+			sum += maxVal_back - a[i];
+			a[i] = maxVal_back;
+		}
+		else maxVal_back = a[i];
+	}
+	cout << sum;
+
+	return 0;
+}
