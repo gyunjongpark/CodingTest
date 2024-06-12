93e48453e03ffe868ef1dba8c81042c48bd449bd
gyunjongpark <idjkda4034@gmail.com>
2024-06-12 21:42:10 +0900

    Update 001_15736.cpp

--- BOJ/Lecture_02/number_theory/001_15736.cpp
+++ BOJ/Lecture_02/number_theory/001_15736.cpp
@@ -8,7 +8,7 @@ int main() {
 	ios_base::sync_with_stdio(false);
 	cin.tie(NULL); cout.tie(NULL);
 
-	//표 또는 도식화를 활용해 문제 해석->규칙 발견->코드 작성
+	//표 또는 도식화를 활용해 문제 해석->규칙 발견
 	cin >> n;
 	cout<<(int)sqrt(n);
 	
