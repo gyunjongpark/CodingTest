69bc9bfb9bc72d6d50310516db2ee6a2f17f0d34
gyunjongpark <idjkda4034@gmail.com>
2024-06-14 14:07:52 +0900

    Create 006_10101.cpp

---
+++ BOJ/ByStep/10/006_10101.cpp
@@ -0,0 +1,26 @@
+#include<iostream>
+using namespace std;
+
+int a, b, c;
+
+int main() {
+	ios_base::sync_with_stdio(false);
+	cin.tie(NULL); cout.tie(NULL);
+
+	cin >> a >> b >> c;
+
+	if (a + b + c != 180) {
+		cout << "Error";
+	}
+	else {
+		if (a == 60 && b == 60 && c == 60) {
+			cout << "Equilateral";
+		}
+		else if (a == b || b == c || a == c) {
+			cout << "Isosceles";
+		}
+		else cout << "Scalene";
+	}
+
+	return 0;
+}
