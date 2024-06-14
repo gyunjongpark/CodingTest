e76acb01340e65fb52c0a5d06eae3c04e8440f9b
gyunjongpark <idjkda4034@gmail.com>
2024-06-14 10:09:27 +0900

    Create 001_10807.cpp

---
+++ BOJ/ByStep/04/001_10807.cpp
@@ -0,0 +1,24 @@
+#include<iostream>
+#define MAX 104
+using namespace std;
+
+int n, v, a[MAX], cnt;
+
+int main() {
+	ios_base::sync_with_stdio(false);
+	cin.tie(NULL); cout.tie(NULL);
+
+	cin >> n;
+	for (int i = 0; i < n; i++) {
+		cin >> a[i];
+	}
+
+	cin >> v;
+	for (int i = 0; i < n; i++) {
+		if (v == a[i]) cnt++;
+	}
+
+	cout << cnt;
+
+	return 0;
+}
