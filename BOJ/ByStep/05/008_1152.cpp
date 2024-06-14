55dc7f17f96fad8dade7e47014194b5ecdb749db
gyunjongpark <idjkda4034@gmail.com>
2024-06-14 11:37:01 +0900

    Create 008_1152.cpp

---
+++ BOJ/ByStep/05/008_1152.cpp
@@ -0,0 +1,25 @@
+#include<iostream>
+#include<string> //getline
+using namespace std;
+
+string s;
+int cnt;
+
+int main() {
+	cin.tie(NULL), cout.tie(NULL);
+	ios_base::sync_with_stdio(false);
+
+	getline(cin, s);
+	int cnt = 0;
+
+	for (int i = 0; i < s.size(); i++) {
+		if (s[i] == ' ')cnt++;
+	}
+
+	if (s[0] == ' ') cnt--;
+	if (s[s.size() - 1] == ' ') cnt--;
+
+	cout << cnt+1;
+
+	return 0;
+}
