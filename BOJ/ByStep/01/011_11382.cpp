26e8fe94cf0dd7db921f13151430ccb0015fbafd
gyunjongpark <idjkda4034@gmail.com>
2024-06-14 09:44:56 +0900

    Create 011_11382.cpp

---
+++ BOJ/ByStep/01/011_11382.cpp
@@ -0,0 +1,15 @@
+#include<iostream>
+using namespace std;
+typedef long long ll;
+
+ll a, b, c;
+
+int main() {
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL); cout.tie(NULL);
+
+    cin >> a >> b >> c;
+    cout << a + b + c;
+
+    return 0;
+}
