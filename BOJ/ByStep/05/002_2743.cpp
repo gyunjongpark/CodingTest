91714cb33341c56a76bdfebf7a9f65c0ab12b4c7
gyunjongpark <idjkda4034@gmail.com>
2024-06-14 11:29:27 +0900

    Create 002_2743.cpp

---
+++ BOJ/ByStep/05/002_2743.cpp
@@ -0,0 +1,14 @@
+#include<iostream>
+using namespace std;
+
+string s;
+
+int main() {
+	cin.tie(NULL), cout.tie(NULL);
+	ios_base::sync_with_stdio(false);
+
+	cin >> s;
+	cout << s.size();
+
+	return 0;
+}
