94f10050a25437864805e417102014ca05341e07
gyunjongpark <idjkda4034@gmail.com>
2024-06-14 10:00:33 +0900

    Create 005_25314.cpp

---
+++ BOJ/ByStep/03/005_25314.cpp
@@ -0,0 +1,18 @@
+#include<iostream>
+using namespace std;
+
+int x, n, a, b, sum;
+
+int main() {
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL); cout.tie(NULL);
+
+	cin >> n;
+
+	for (int i = 0; i < n / 4; i++) {
+		cout << "long ";
+	}
+	cout << "int";
+
+    return 0;
+}
