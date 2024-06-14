79c865b4725ce8a4f6259a6e18ddc4da6620a4c9
gyunjongpark <idjkda4034@gmail.com>
2024-06-14 09:39:37 +0900

    Create 007_10926.cpp

---
+++ BOJ/ByStep/01/007_10926.cpp
@@ -0,0 +1,14 @@
+#include<iostream>
+using namespace std;
+
+string s;
+
+int main() {
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL); cout.tie(NULL);
+
+    cin >> s;
+    cout << s << "??!";
+
+    return 0;
+}
