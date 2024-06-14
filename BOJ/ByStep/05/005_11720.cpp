96d1516ee7f466f1148c8a98a33cbf101be30d59
gyunjongpark <idjkda4034@gmail.com>
2024-06-14 11:31:50 +0900

    Create 005_11720.cpp

---
+++ BOJ/ByStep/05/005_11720.cpp
@@ -0,0 +1,20 @@
+#include<iostream>
+using namespace std;
+
+int n, ret;
+string s;
+
+int main() {
+	cin.tie(NULL), cout.tie(NULL);
+	ios_base::sync_with_stdio(false);
+
+	cin >> n >> s;
+
+	for (int i = 0; i < n; i++) {
+		ret += s[i] - 48;
+	}
+  
+	cout << ret;
+  
+	return 0;
+}
