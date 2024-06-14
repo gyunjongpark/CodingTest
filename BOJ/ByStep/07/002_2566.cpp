9eb94fe95e0c206225c5e386ca39f5ef029b5abe
gyunjongpark <idjkda4034@gmail.com>
2024-06-14 13:56:04 +0900

    Create 002_2566.cpp

---
+++ BOJ/ByStep/07/002_2566.cpp
@@ -0,0 +1,30 @@
+#include<iostream>
+#define MAX 104
+using namespace std;
+
+int maxVal=-1, a[MAX][MAX];
+pair<int, int> pi;
+
+int main() {
+	cin.tie(NULL), cout.tie(NULL);
+	ios_base::sync_with_stdio(false);
+
+	for (int i = 0; i < 9; i++) {
+		for (int j = 0; j < 9; j++) {
+			cin >> a[i][j];
+		}
+	}
+
+	for (int i = 0; i < 9; i++) {
+		for (int j = 0; j < 9; j++) {
+			if (maxVal < a[i][j]) {
+				maxVal = a[i][j];
+				pi.first = i + 1, pi.second = j + 1;
+			}
+		}
+	}
+	cout << maxVal << '\n';
+	cout << pi.first << ' ' << pi.second;
+
+	return 0;
+}
