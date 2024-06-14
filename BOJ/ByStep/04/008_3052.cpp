e37c3d0eca4351fbeba97fb7981644cb0e9697ad
gyunjongpark <idjkda4034@gmail.com>
2024-06-14 10:19:39 +0900

    Create 008_3052.cpp

---
+++ BOJ/ByStep/04/008_3052.cpp
@@ -0,0 +1,24 @@
+#include<iostream>
+#include<algorithm>
+#include<vector>
+using namespace std;
+
+vector<int> a;
+int n;
+
+int main() {
+	cin.tie(NULL), cout.tie(NULL);
+	ios_base::sync_with_stdio(false);
+
+	for (int i = 0; i < 10; i++) {
+		cin >> n;
+		a.push_back(n % 42);
+	}
+
+	sort(a.begin(), a.end());
+	a.erase(unique(a.begin(), a.end()), a.end());
+
+	cout << a.size();
+
+	return 0;
+}
