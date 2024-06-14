4b1c923ab4d51e86828a74c3aee1746515d9e985
gyunjongpark <idjkda4034@gmail.com>
2024-06-14 14:09:02 +0900

    Update 006_10101.cpp

--- BOJ/ByStep/10/006_10101.cpp
+++ BOJ/ByStep/10/006_10101.cpp
@@ -9,16 +9,10 @@ int main() {
 
 	cin >> a >> b >> c;
 
-	if (a + b + c != 180) {
-		cout << "Error";
-	}
+	if (a + b + c != 180) cout << "Error";
 	else {
-		if (a == 60 && b == 60 && c == 60) {
-			cout << "Equilateral";
-		}
-		else if (a == b || b == c || a == c) {
-			cout << "Isosceles";
-		}
+		if (a == 60 && b == 60 && c == 60) cout << "Equilateral";
+		else if (a == b || b == c || a == c) cout << "Isosceles";
 		else cout << "Scalene";
 	}
 
