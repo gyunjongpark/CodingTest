8b4d5e6ffcc131dd59e4598fb9f84ef3f025d3b6
gyunjongpark <idjkda4034@gmail.com>
2024-06-14 14:05:10 +0900

    Create 004_15894.cpp

---
+++ BOJ/ByStep/10/004_15894.cpp
@@ -0,0 +1,15 @@
+#include<iostream>
+using namespace std;
+typedef long long ll;
+
+ll n;
+
+int main() {
+	ios_base::sync_with_stdio(false);
+	cin.tie(NULL); cout.tie(NULL);
+
+	cin >> n;
+	cout << 4 * n;
+
+	return 0;
+}
