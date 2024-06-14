30debb2c271f28372cd7a18debcc17fd0f1369e7
gyunjongpark <idjkda4034@gmail.com>
2024-06-14 09:53:09 +0900

    Create 005_2884.cpp

---
+++ BOJ/ByStep/02/005_2884.cpp
@@ -0,0 +1,22 @@
+#include<iostream>
+using namespace std;
+
+int h, m;
+
+int main() {
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL); cout.tie(NULL);
+
+	cin >> h >> m;
+
+	int temp = h * 60 + m - 45;
+
+	if (temp < 0) {
+		temp += 24 * 60;
+	}
+
+	h = temp / 60, m = temp % 60;
+	cout << h << ' ' << m;
+
+    return 0;
+}
