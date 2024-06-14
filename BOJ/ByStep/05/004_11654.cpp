117caa47ce4c922fd982e8e8a6f1a0c9fbd5e04e
gyunjongpark <idjkda4034@gmail.com>
2024-06-14 11:31:04 +0900

    Create 004_11654.cpp

---
+++ BOJ/ByStep/05/004_11654.cpp
@@ -0,0 +1,14 @@
+#include<iostream>
+using namespace std;
+
+char n;
+
+int main() {
+	cin.tie(NULL), cout.tie(NULL);
+	ios_base::sync_with_stdio(false);
+
+	cin >> n;
+	cout << (int)n;
+
+	return 0;
+}
