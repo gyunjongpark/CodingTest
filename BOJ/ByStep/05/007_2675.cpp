803f2db1f372c9231bdd0b711a074baaf0d77384
gyunjongpark <idjkda4034@gmail.com>
2024-06-14 11:35:17 +0900

    Create 007_2675.cpp

---
+++ BOJ/ByStep/05/007_2675.cpp
@@ -0,0 +1,25 @@
+#include<iostream>
+using namespace std;
+
+int r,t;
+string s;
+
+int main() {
+	cin.tie(NULL), cout.tie(NULL);
+	ios_base::sync_with_stdio(false);
+
+	cin >> t;
+
+	while (t--) {
+		cin >> r >> s;
+    
+		for (char a : s) {
+			for (int i = 0; i < r; i++) {
+				cout << a;
+			}
+		}
+		cout << '\n';
+	}
+
+	return 0;
+}
