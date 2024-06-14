48f1b1f7b46abbd219190f69488a3ff499951508
gyunjongpark <idjkda4034@gmail.com>
2024-06-14 09:41:24 +0900

    Create 009_10430.cpp

---
+++ BOJ/ByStep/01/009_10430.cpp
@@ -0,0 +1,18 @@
+#include<iostream>
+using namespace std;
+
+int a, b, c;
+
+int main() {
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL); cout.tie(NULL);
+
+    cin >> a >> b >> c;
+
+    cout << (a + b) % c << '\n';
+    cout << ((a % c) + (b % c)) % c << '\n';
+    cout << (a * b) % c << '\n';
+    cout << ((a % c) * (b % c)) % c;
+
+    return 0;
+}
