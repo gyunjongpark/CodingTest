ac6e306afc9d103848605207da6e248ad9b57133
gyunjongpark <idjkda4034@gmail.com>
2024-06-14 09:59:12 +0900

    Create 004_25304.cpp

---
+++ BOJ/ByStep/03/004_25304.cpp
@@ -0,0 +1,21 @@
+#include<iostream>
+#define MAX 10004
+using namespace std;
+
+int x, n, a, b, sum;
+
+int main() {
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL); cout.tie(NULL);
+
+	cin >> x >> n;
+
+	for (int i = 0; i < n; i++) {
+		cin >> a >> b;
+		sum += a * b;
+	}
+	if (x == sum) cout << "Yes";
+	else cout << "No";
+
+    return 0;
+}
