d95c72ef902780afdef8cfacfc6ba20254ad6d25
gyunjongpark <idjkda4034@gmail.com>
2024-06-14 09:42:20 +0900

    Update 006_10869.cpp

--- BOJ/ByStep/01/006_10869.cpp
+++ BOJ/ByStep/01/006_10869.cpp
@@ -8,7 +8,11 @@ int main() {
     cin.tie(NULL); cout.tie(NULL);
 
     cin >> a >> b;
-    cout << a + b << '\n' << a - b << '\n' << a * b << '\n' << a / b << '\n' << a % b;
+    cout << a + b << '\n';
+    cout << a - b << '\n';
+    cout << a * b << '\n';
+    cout << a / b << '\n';
+    cout << a % b;
 
     return 0;
 }
