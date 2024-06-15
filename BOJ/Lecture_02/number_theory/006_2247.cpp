fd3ee05b7dbd797b3531b384dde2d6dbd8426615
gyunjongpark <idjkda4034@gmail.com>
2024-06-16 00:04:02 +0900

    Update 006_2247.cpp

--- BOJ/Lecture_02/number_theory/006_2247.cpp
+++ BOJ/Lecture_02/number_theory/006_2247.cpp
@@ -14,8 +14,8 @@ int main() {
 	//n은 최대 2억 -> 시간제한은 2초
 	for (int i = 2; i <= n; i++) {
 		int cnt = n / i; //i를 약수로 가지는 수의 갯수
-		answer += (cnt - 1) * i; //본인 제외 i의 곱을 합하면 sod
-		answer %= 1000000;
+		answer += (cnt - 1) * i; //본인을 제외한 갯수와 i의 곱의 총 합은 csod
+		answer %= 1000000; //루프마다 나머지 계산해서 메모리 맞추기
 	}
 	cout << answer;
 
