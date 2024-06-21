2b069179fa9a613b138ed1fb7fee3a714d296bec
gyunjongpark <idjkda4034@gmail.com>
2024-06-21 09:03:29 +0900

    Update 002_14568.cpp

--- BOJ/Lecture_02/brute_force/002_14568.cpp
+++ BOJ/Lecture_02/brute_force/002_14568.cpp
@@ -1,25 +1,25 @@
 #include<iostream>
 using namespace std;
 
-int n, cnt;
+int n;
 
 int main() {
 	ios_base::sync_with_stdio(false);
 	cin.tie(NULL); cout.tie(NULL);
 
 	cin >> n;
-	for (int i = 0; i < n; i++) { //택희
-		for (int j = 0; j < n; j++) { //영훈
-			for (int k = 0; k < n; k++) { //남규
-				if (i == 0 || j == 0 || k == 0) continue; //조건 3
-				if (i % 2) continue; //조건 4
-				if (i + j + k == n && k >= j + 2) { //조건1, 조건2
+
+	int cnt = 0;
+	for (int i = 1; i <= n; i++) { //택희
+		for (int j = 1; j <= n; j++) { //영훈
+			for (int k = 1; k <= n; k++) { //남규
+				if (i + j + k == n && k >= j + 2 && (i % 2 == 0)) {
 					cnt++;
 				}
 			}
 		}
 	}
 	cout << cnt;
-
+	
 	return 0;
 }
