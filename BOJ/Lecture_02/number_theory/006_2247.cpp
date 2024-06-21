01dc10df5d48eb0a32102a7c085ef04eda2153eb
gyunjongpark <idjkda4034@gmail.com>
2024-06-21 10:46:18 +0900

    Update 006_2247.cpp

--- BOJ/Lecture_02/number_theory/006_2247.cpp
+++ BOJ/Lecture_02/number_theory/006_2247.cpp
@@ -11,10 +11,13 @@ int main() {
 
 	int answer = 0;
 
-	//n은 최대 2억 -> 시간제한은 2초
-	for (int i = 2; i <= n; i++) {
-		int cnt = n / i; //i를 약수로 가지는 수의 갯수
-		answer += (cnt - 1) * i; //본인을 제외한 갯수와 i의 곱의 총 합
+	//n은 최대 2억 -> 시간제한 2초
+	for (int i = 2; i <= n; i++) { //1을 제외하고 i부터 계산(실질적 약수)
+		int cnt = n / i; //n!에서 i가 몇 개 있는가?
+		answer += (cnt - 1) * i;
+		//i 본인을 제외한(실질적 약수) 갯수와 i를 곱하면 csod(n)에서 i가 나온 횟수를 구할 수 있다
+		//i가 증가함에 따라 최종적인 csod(n)을 answer로 값을 누적하면서 구할 수 있게 된다
+		
 		answer %= 1000000; //메모리 초과 방지
 	}
 	cout << answer;
