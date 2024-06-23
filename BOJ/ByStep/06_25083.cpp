841924577c4730e9f65e7b7b45734c8d857c1c3b
gyunjongpark <idjkda4034@gmail.com>
2024-06-23 17:41:05 +0900

    Create 06_25083.cpp

---
+++ BOJ/ByStep/06_25083.cpp
@@ -0,0 +1,18 @@
+#include<iostream>
+using namespace std;
+
+string s;
+
+int main() {
+	cin.tie(NULL), cout.tie(NULL);
+	ios_base::sync_with_stdio(false);
+
+	cout << "         ,r'\"7\n"; /* //, \" */
+	cout << "r`-_   ,'  ,/\n";
+	cout << " \\. \". L_r'\n"; /* \\, \" */ 
+	cout << "   `~\\/\n"; /* \\ */
+	cout << "      |\n";
+	cout << "      |";
+
+	return 0;
+}
