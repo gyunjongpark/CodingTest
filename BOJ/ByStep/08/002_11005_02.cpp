042d20a8fe57c457b21cd3de985b5dcd61caafff
gyunjongpark <idjkda4034@gmail.com>
2024-06-24 21:56:37 +0900

    Create 002_11005_02.cpp

---
+++ BOJ/ByStep/08/002_11005_02.cpp
@@ -0,0 +1,29 @@
+#include<iostream>
+#include<vector>
+#include<algorithm>
+using namespace std;
+
+int n, b;
+vector<int> v;
+
+int main() {
+	cin.tie(NULL), cout.tie(NULL);
+	ios_base::sync_with_stdio(false);
+
+	cin >> n >> b;
+
+	while (n > 1) {
+		v.push_back(n % b);
+		n /= b;
+	}
+	if (n == 1) v.push_back(n);
+  
+	reverse(v.begin(), v.end());
+	
+	for (int a : v) {
+		if (a >= 10) cout << char(a + 55); //아스키 코드 참고
+		else cout << a;
+	}
+
+	return 0;
+}
