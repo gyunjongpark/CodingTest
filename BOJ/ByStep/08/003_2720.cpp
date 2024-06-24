d948e110faba89fa8a9418195dd90a99eba812af
gyunjongpark <idjkda4034@gmail.com>
2024-06-24 21:59:19 +0900

    Create 003_2720.cpp

---
+++ BOJ/ByStep/08/003_2720.cpp
@@ -0,0 +1,26 @@
+#include<iostream>
+#include<algorithm>
+using namespace std;
+
+int a, t, s[4]; //쿼터, 다임, 니켈, 페니
+
+int main() {
+	ios_base::sync_with_stdio(false);
+	cin.tie(NULL), cout.tie(NULL);
+
+	cin >> t;
+
+	while (t--) {
+		cin >> a;
+
+		s[0] = a / 25, a %= 25;
+		s[1] = a / 10, a %= 10;
+		s[2] = a / 5, a %= 5;
+		s[3] = a;
+
+		for (int a : s) cout << a << ' ';
+		cout << '\n';
+	}
+
+	return 0;
+}
