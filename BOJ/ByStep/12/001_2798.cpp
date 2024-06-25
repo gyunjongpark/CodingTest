9228a24378766b0954bef20db02d5d4aaf82a8b9
gyunjongpark <idjkda4034@gmail.com>
2024-06-25 21:26:03 +0900

    Create 001_2798.cpp

---
+++ BOJ/ByStep/12/001_2798.cpp
@@ -0,0 +1,30 @@
+#include<iostream>
+#define MAX 104
+using namespace std;
+
+int n, m;
+int a[MAX];
+
+int main() {
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL); cout.tie(NULL);
+
+    cin >> n >> m;
+
+    for (int i = 0; i < n; i++) {
+        cin >> a[i];
+    }
+
+    int maxVal = -1;
+    for (int i = 0; i < n; i++) {
+        for (int j = 0; j < i; j++) {
+            for (int k = 0; k < j; k++) {
+                int card = a[i] + a[j] + a[k];
+                if (card <= m && maxVal < card) maxVal = card;
+            }
+        }
+    }
+    cout << maxVal;
+
+    return 0;
+}
