10de275a30f8a9717f93631e086d89f6ee92d927
gyunjongpark <idjkda4034@gmail.com>
2024-06-25 21:58:36 +0900

    Create 004_1018.cpp

---
+++ BOJ/ByStep/12/004_1018.cpp
@@ -0,0 +1,83 @@
+#include<iostream>
+#define MAX 54
+using namespace std;
+
+int temp, n, m, cnt, minVal = 65;
+string board[MAX];
+
+string WB[8] = {
+        "WBWBWBWB",
+        "BWBWBWBW",
+        "WBWBWBWB",
+        "BWBWBWBW",
+        "WBWBWBWB",
+        "BWBWBWBW",
+        "WBWBWBWB",
+        "BWBWBWBW"
+};
+
+string BW[8] = {
+        "BWBWBWBW",
+        "WBWBWBWB",
+        "BWBWBWBW",
+        "WBWBWBWB",
+        "BWBWBWBW",
+        "WBWBWBWB",
+        "BWBWBWBW",
+        "WBWBWBWB"
+};
+
+int WB_cnt(int x, int y)
+{
+    int cnt = 0;
+    for (int i = 0; i < 8; i++)
+    {
+        for (int j = 0; j < 8; j++)
+        {
+            if (board[x + i][y + j] != WB[i][j])
+                cnt++;
+        }
+
+    }
+
+    return cnt;
+}
+
+int BW_cnt(int x, int y)
+{
+    int cnt = 0;
+    for (int i = 0; i < 8; i++)
+    {
+        for (int j = 0; j < 8; j++)
+        {
+            if (board[x + i][y + j] != BW[i][j])
+                cnt++;
+        }
+
+    }
+
+    return cnt;
+}
+
+int main() {
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL); cout.tie(NULL);
+
+    cin >> n >> m;
+    for (int i = 0; i < n; i++) {
+        cin >> board[i];
+    }
+
+    for (int i = 0; i <= n - 8; i++) {
+        for (int j = 0; j <= m - 8; j++) {
+            temp = min(WB_cnt(i, j), BW_cnt(i, j));
+
+            if (temp < minVal) {
+                minVal = temp;
+            }
+        }
+    }
+    cout << minVal;
+
+    return 0;
+}
