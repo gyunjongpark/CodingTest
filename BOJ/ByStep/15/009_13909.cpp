9a6f657d5f6916684a102840fd27ca4a00e219e7
gyunjongpark <idjkda4034@gmail.com>
2024-06-27 15:11:54 +0900

    Create 009_13909.cpp

---
+++ BOJ/ByStep/15/009_13909.cpp
@@ -0,0 +1,15 @@
+#include<iostream>
+#include<cmath>
+using namespace std;
+
+int n;
+
+int main() {
+	ios_base::sync_with_stdio(false);
+	cin.tie(NULL); cout.tie(NULL);
+
+	cin >> n;
+	cout << (int)sqrt(n);
+
+	return 0;
+}
