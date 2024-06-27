091889a9fd1e997fc36c8940f9155f5010a4093f
gyunjongpark <idjkda4034@gmail.com>
2024-06-27 23:10:20 +0900

    Create 011_24511.cpp

---
+++ BOJ/ByStep/16/011_24511.cpp
@@ -0,0 +1,34 @@
+#include <iostream>
+#include <deque>
+#define MAX 100004
+using namespace std;
+
+deque<int> dq;
+int n, m, x, y;
+bool flag[MAX]; //0:queue, 1:stack
+
+int main() {
+    ios::sync_with_stdio(false);
+    cin.tie(NULL); cout.tie(NULL);
+
+    cin >> n;
+    for (int i = 0; i < n; i++) {
+        cin >> flag[i];
+    }
+
+    for (int i = 0; i < n; i++) {
+        cin >> x;
+        if (flag[i] == 0) //queue일때만 deque에 원소 삽입
+            dq.push_back(x);
+    }
+
+    cin >> m;
+    for (int i = 0; i < m; i++) {
+        cin >> y;
+        dq.push_front(y);
+        cout << dq.back() << " ";
+        dq.pop_back();
+    }
+
+    return 0;
+}
