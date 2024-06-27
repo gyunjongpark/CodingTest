fa89b41d8f751161cb6d74876d00ee7567699ec3
gyunjongpark <idjkda4034@gmail.com>
2024-06-27 14:37:41 +0900

    Create 003_1735.cpp

---
+++ BOJ/ByStep/15/003_1735.cpp
@@ -0,0 +1,26 @@
+#include<iostream>
+using namespace std;
+
+int a, b, c, d;
+
+int getGCD(int a, int b) {
+	if (a == 0) return b;
+	return getGCD(b % a, a);
+}
+
+int main() {
+	ios_base::sync_with_stdio(false);
+	cin.tie(NULL); cout.tie(NULL);
+
+	cin >> a >> b >> c >> d;
+
+	int ja = a * d + b * c;
+	int mo = b * d;
+	int temp = getGCD(ja, mo);
+	ja /= temp;
+	mo /= temp;
+
+	cout << ja << ' ' << mo;
+
+	return 0;
+}
