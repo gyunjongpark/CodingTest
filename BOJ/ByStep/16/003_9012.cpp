24f0e1a830a169d5439031a474043ab62754aa16
gyunjongpark <idjkda4034@gmail.com>
2024-06-27 22:32:22 +0900

    Create 003_9012.cpp

---
+++ BOJ/ByStep/16/003_9012.cpp
@@ -0,0 +1,32 @@
+#include<iostream>
+#include<stack>
+using namespace std;
+
+int n;
+string s;
+
+int main() {
+	ios_base::sync_with_stdio(false);
+	cin.tie(NULL); cout.tie(NULL);
+
+	cin >> n;
+	while (n--) {
+		cin >> s;
+
+		stack<char> stk;
+		for (char a : s) {
+			if (stk.size()) {
+				if (stk.top() == '(' && a == ')') stk.pop();
+				else stk.push(a);
+			}
+			else {
+				stk.push(a);
+			}
+		}
+
+		if (stk.size()) cout << "NO\n";
+		else cout << "YES\n";
+	}
+
+	return 0;
+}
