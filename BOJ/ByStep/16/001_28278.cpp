964ffba1c964f888da99527a75dca6a576607e7d
gyunjongpark <idjkda4034@gmail.com>
2024-06-27 22:30:12 +0900

    Create 001_28278.cpp

---
+++ BOJ/ByStep/16/001_28278.cpp
@@ -0,0 +1,41 @@
+#include<iostream>
+#include<stack>
+using namespace std;
+
+int n, c, x;
+stack<int> stk;
+
+int main() {
+	ios_base::sync_with_stdio(false);
+	cin.tie(NULL); cout.tie(NULL);
+
+	cin >> n;
+
+	while (n--) {
+		cin >> c;
+		if (c == 1) {
+			cin >> x;
+			stk.push(x);
+		}
+		else if (c == 2) {
+			if (stk.size()) {
+				cout << stk.top() << '\n';
+				stk.pop();
+			}
+			else cout << -1 << '\n';
+		}
+		else if (c == 3) {
+			cout << stk.size() << '\n';
+		}
+		else if (c == 4) {
+			if (stk.size()) cout << 0 << '\n';
+			else cout << 1 << '\n';
+		}
+		else if (c == 5) {
+			if (stk.size()) cout << stk.top() << '\n';
+			else cout << -1 << '\n';
+		}
+	}
+
+	return 0;
+}
