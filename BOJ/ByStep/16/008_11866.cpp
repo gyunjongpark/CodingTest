cfaac05a7e873644995b26acf545df5f523e87c1
gyunjongpark <idjkda4034@gmail.com>
2024-06-27 22:45:23 +0900

    Create 008_11866.cpp

---
+++ BOJ/ByStep/16/008_11866.cpp
@@ -0,0 +1,31 @@
+#include<iostream>
+#include<queue>
+using namespace std;
+
+int n, k;
+queue<int> q;
+
+int main() {
+	ios_base::sync_with_stdio(false);
+	cin.tie(NULL); cout.tie(NULL);
+
+	cin >> n >> k;
+
+	for (int i = 1; i <= n; i++) {
+		q.push(i);
+	}
+
+  //출력 형식 신경쓰자!
+	cout << '<';
+	while (q.size() != 1) {
+		for (int i = 1; i < k; i++) {
+			q.push(q.front());
+			q.pop();
+		}
+		cout << q.front() << ", ";
+		q.pop();
+	}
+	cout << q.front() << '>';
+
+	return 0;
+}
