e0b55a33d9907fe7723f881c6295c1eae66094c9
gyunjongpark <idjkda4034@gmail.com>
2024-06-27 14:23:20 +0900

    Create 008_11478.cpp

---
+++ BOJ/ByStep/14/008_11478.cpp
@@ -0,0 +1,24 @@
+#include<iostream>
+#include<set>
+using namespace std;
+
+string s, str;
+set<string> st;
+
+int main() {
+	ios_base::sync_with_stdio(false);
+	cin.tie(NULL); cout.tie(NULL);
+
+	cin >> s;
+
+	for (int i = 0; i < s.size(); i++) {
+		for (int j = i; j < s.size(); j++) {
+			str += s[j];
+			st.insert(str);
+		}
+		str = ""; //init
+	}
+	cout << st.size();
+
+	return 0;
+}
