f09b6ac620039dec5773b944da903e533cae3944
gyunjongpark <idjkda4034@gmail.com>
2024-06-28 09:38:28 +0900

    Update 007_2436.cpp

--- BOJ/Lecture_02/number_theory/007_2436.cpp
+++ BOJ/Lecture_02/number_theory/007_2436.cpp
@@ -1,5 +1,5 @@
 #include<iostream>
-#include<cmath>
+#include<cmath> //sqrt
 using namespace std;
 typedef long long ll;
 
@@ -17,16 +17,16 @@ void solve() {
 	//sqrt까지만 조사하기 때문에 반복문을 돌수록 서로소 관계의 두 수의 차이가 줄어든다
 	for (ll i = 1; i <= (int)sqrt(div); i++) {
 		if (div % i == 0) {
-			ll a = i, b = div / i;
+			ll a = i, b = div / i; //init
 
-			//가장 차이가 적은 서로소 관계의 두 수를 결정한다
+			//반복문을 거칠수록 가장 차이가 적은 서로소 관계의 a, b가 결정된다
 			if (getGCD(a, b) == 1) {
 				ret_a = a;
 				ret_b = b;
 			}
 		}
 	}
-	//결정된 두 수에 최대 공약수를 곱해 조건에 만족하는 자연수를 구한다
+	//결정된 두 수에 최대 공약수를 곱해 최종적으로 조건에 만족하는 자연수를 구한다
 	cout << ret_a * gcd << ' ' << ret_b * gcd;
 }
 
