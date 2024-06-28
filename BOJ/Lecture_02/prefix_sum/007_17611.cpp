d0a4f4c1625b0a8809b27b51a319a341c19643eb
gyunjongpark <idjkda4034@gmail.com>
2024-06-28 15:14:13 +0900

    Create 007_17611.cpp

---
+++ BOJ/Lecture_02/prefix_sum/007_17611.cpp
@@ -0,0 +1,56 @@
+#include<iostream>
+#include<vector>
+#define ALPHA 500000
+#define MAX 1000010
+using namespace std;
+
+int n, xpos, ypos;
+int cnt_x[MAX], cnt_y[MAX], s_x[MAX], s_y[MAX];
+vector<pair<int,int>> pos;
+
+int main() {
+	ios_base::sync_with_stdio(false);
+	cin.tie(NULL); cout.tie(NULL);
+
+	cin>>n;
+
+	for (int i = 0; i < n; i++) {
+		cin >> xpos >> ypos;
+		xpos += ALPHA;
+		ypos += ALPHA;
+		pos.push_back({ xpos,ypos });
+	}
+
+	for (int i = 0; i < n; i++) {
+		int x = pos[i].first;
+		int y = pos[i].second;
+		int nx = pos[(i + 1) % n].first;
+		int ny = pos[(i + 1) % n].second;
+
+		if (x == nx) {
+			int max_y = max(y, ny);
+			int min_y = min(y, ny);
+			cnt_y[min_y]++;
+			cnt_y[max_y]--;
+		}
+		else {
+			int max_x = max(x, nx);
+			int min_x = min(x, nx);
+			cnt_x[min_x]++;
+			cnt_x[max_x]--;
+		}
+	}
+
+	for (int i = 1; i < MAX; i++) {
+		s_x[i] = s_x[i - 1] + cnt_x[i - 1];
+		s_y[i] = s_y[i - 1] + cnt_y[i - 1];
+	}
+
+	int ret = 0;
+	for (int i = 1; i < MAX; i++) {
+		ret = max(ret, max(s_x[i], s_y[i]));
+	}
+	cout << ret;
+
+	return 0;
+}
