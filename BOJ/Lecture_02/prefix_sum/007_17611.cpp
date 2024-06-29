5b723ac48677cb16a2a98779fc3e123e129c69e4
gyunjongpark <idjkda4034@gmail.com>
2024-06-29 23:13:46 +0900

    Update 007_17611.cpp

--- BOJ/Lecture_02/prefix_sum/007_17611.cpp
+++ BOJ/Lecture_02/prefix_sum/007_17611.cpp
@@ -1,7 +1,7 @@
 #include<iostream>
 #include<vector>
 #define ALPHA 500000
-#define MAX 1000010
+#define MAX 1000004
 using namespace std;
 
 int n, xpos, ypos;
@@ -24,7 +24,7 @@ int main() {
 	for (int i = 0; i < n; i++) {
 		int x = pos[i].first;
 		int y = pos[i].second;
-		int nx = pos[(i + 1) % n].first;
+		int nx = pos[(i + 1) % n].first; //마지막 좌표는 첫 좌표로 돌아온다
 		int ny = pos[(i + 1) % n].second;
 
 		if (x == nx) {
@@ -41,13 +41,13 @@ int main() {
 		}
 	}
 
-	for (int i = 1; i < MAX; i++) {
+	for (int i = 1; i < 1000001; i++) {
 		s_x[i] = s_x[i - 1] + cnt_x[i - 1];
 		s_y[i] = s_y[i - 1] + cnt_y[i - 1];
 	}
 
 	int ret = 0;
-	for (int i = 1; i < MAX; i++) {
+	for (int i = 1; i < 1000001; i++) {
 		ret = max(ret, max(s_x[i], s_y[i]));
 	}
 	cout << ret;
