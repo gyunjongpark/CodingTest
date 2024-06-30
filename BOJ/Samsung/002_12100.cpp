45e02ae570eaf87dcaf9ff72106f181113d804d5
gyunjongpark <idjkda4034@gmail.com>
2024-06-30 22:12:42 +0900

    Update 002_12100.cpp

--- BOJ/Samsung/002_12100.cpp
+++ BOJ/Samsung/002_12100.cpp
@@ -77,8 +77,8 @@ void go(Board c, int here) {
 }
 
 int main() {
-  ios_base::sync_with_stdio(false);
-  cin.tie(NULL); cout.tie(NULL);
+  	ios_base::sync_with_stdio(false);
+  	cin.tie(NULL); cout.tie(NULL);
   
 	cin >> n;
   
