c5991c807d9aaacbc73cd95f37dfe7d6cbf4291c
gyunjongpark <idjkda4034@gmail.com>
2024-07-03 23:16:36 +0900

    Create 004_15652.cpp

---
+++ BOJ/Lecture_02/recursive, back_tracking/004_15652.cpp
@@ -0,0 +1,34 @@
+#include <iostream>
+#include <vector>
+using namespace std;
+
+int n, m; //n개 중에서 m개를 뽑는다
+
+void combi(int start, vector<int>& v) {
+    if (v.size() == m) {
+        for (int i : v) cout << i << ' ';
+        cout << '\n';
+
+        return;
+    }
+
+    for (int i = start; i <= n; i++) {
+        v.push_back(i);
+        combi(i, v);
+        v.pop_back();
+    }
+
+    return;
+}
+
+int main() {
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL); cout.tie(NULL);
+
+    cin >> n >> m;
+
+    vector<int> v; //조합, 인덱스를 나열한 배열 v
+    combi(1, v);
+
+    return 0;
+}
