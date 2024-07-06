e0ae030bd580355275e786c59cdc31568b6dd238
gyunjongpark <idjkda4034@gmail.com>
2024-07-06 23:51:09 +0900

    Update 004_14232.cpp

--- BOJ/Lecture_02/number_theory/004_14232.cpp
+++ BOJ/Lecture_02/number_theory/004_14232.cpp
@@ -17,11 +17,11 @@ int main() {
 	for (int i = 2; i <= (int)sqrt(k); i++) {
 		while (n % i == 0) {
 			v.push_back(i);
-			n /= i;
+			n /= i; //같은 무게의 보석들은 줄줄이 딸려 나온다
 		}
 	}
 	
-	//sqrt(n)보다 크고 유일하면서 마지막인 소인수
+	//sqrt(n)보다 크고 유일하면서 마지막인 소인수가 있다면 출력
 	if (n != 1) v.push_back(n);
 
 	cout << v.size() << '\n';
