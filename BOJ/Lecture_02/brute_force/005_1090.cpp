98110581a60ec3249ded4e3d7060656a746ed133
gyunjongpark <idjkda4034@gmail.com>
2024-07-06 23:18:14 +0900

    Update 005_1090.cpp

--- BOJ/Lecture_02/brute_force/005_1090.cpp
+++ BOJ/Lecture_02/brute_force/005_1090.cpp
@@ -1,11 +1,11 @@
 #include<iostream>
-#include<vector>
 #include<algorithm>
+#include<vector>
 using namespace std;
 
-vector<pair<int, int>> v;
-vector<int> xpos, ypos;
 int n, x, y;
+vector<int> xpos, ypos;
+vector<pair<int, int>> v;
 
 int main() {
 	ios_base::sync_with_stdio(false);
@@ -14,37 +14,40 @@ int main() {
 	cin >> n;
 	for (int i = 0; i < n; i++) {
 		cin >> x >> y;
-		v.push_back({ x,y }); //움직일 좌표 입력
-		xpos.push_back(x); //최소 거리가 될 수 있는 x 좌표 입력
-		ypos.push_back(y); //최소 거리가 될 수 있는 y 좌표 입력
+		xpos.push_back(x);
+		ypos.push_back(y);
+		v.push_back({ x,y });
 	}
 
 	vector<int> answer(n, -1); //init
-	for (int y : ypos) {
-		for (int x : xpos) {
-			vector<int> dist; //거리 차이 담기
+	for (int x : xpos) {
+		for (int y : ypos) {
+			vector<int> dist;
 
-			for (pair<int, int> pi : v) { //움직일 좌표 전체 조사
-				int d = abs(pi.first - x) + abs(pi.second - y);
+			for (pair<int, int> pi : v) {
+				int d = abs(x - pi.first) + abs(y - pi.second); //문제에 주어진 최소 이동거리 계산법
 				dist.push_back(d);
 			}
-
+			//하나의 좌표에 대하여 각 체커별로 최소 이동거리를 구한 후 정렬
 			sort(dist.begin(), dist.end());
 
-			int tmp = 0;
+			int temp = 0;
 			for (int i = 0; i < n; i++) {
-				tmp += dist[i]; //tmp는 i개의 체커가 모이는 횟수가 된다
+				temp += dist[i]; //i개의 체커가 모이기 위해 필요한 최소 이동거리
+
 				if (answer[i] == -1) {
-					answer[i] = tmp; //배열에 값이 처음 들어왔다면 tmp로 일단 배정
+					answer[i] = temp;
 				}
 				else {
-					//answer[i]는 i개의 체커가 모이는 최소 횟수가 된다
-					answer[i] = min(tmp, answer[i]);
+					answer[i] = min(answer[i], temp); //answer의 최소값은 좌표가 바뀌면서 변경될 수 있다
 				}
 			}
 		}
 	}
-	for (int ret : answer) cout << ret << ' '; //i에 따라 체커가 모이는 최소 횟수 출력
+
+	for (int i : answer) { //1~n개의 체커가 모이기 위한 최소 이동거리 차례대로 출력
+		cout << i << ' ';
+	}
 
 	return 0;
 }
