6d2c5f9fe3574b57c7e51aa60b42d26e80339bc7
gyunjongpark <idjkda4034@gmail.com>
2024-07-06 23:49:56 +0900

    Update 003_11653.cpp

--- BOJ/Lecture_02/number_theory/003_11653.cpp
+++ BOJ/Lecture_02/number_theory/003_11653.cpp
@@ -10,25 +10,25 @@ int main() {
 
 	cin >> n;
 
-	int x = n; //소인수 분해를 위한 변수 x 선언
-	for (int i = 2; i <= (int)sqrt(n); i++) { //while문에 의해 소수인 i로만 반복한다
-		while (x % i == 0) {
+	int x = n; //소인수 분해를 위한 변수 x
+	for (int i = 2; i <= (int)sqrt(n); i++) {
+		while (x % i == 0) { //while문의 조건 덕분에 소수가 되는 i만으로 반복문이 수행된다
 			cout << i << '\n';
 			x /= i;
 		}
 	}
 	
-	//sqrt(n)보다 크고 유일하면서 마지막인 소인수
+	//sqrt(n)보다 크고 유일하면서 마지막인 소인수가 있다면 출력
 	if (x != 1) cout << x << '\n';
 
 	return 0;
 }
 
 /*
-n=p*q를 만족하면서 p>sqrt(n) && q>sqrt(n)인 소수쌍 (p,q)는 존재하지 않는다
-즉, p>sqrt(n)라면 반드시 q<sqrt(n)이다
+n=p*q를 만족하면서 p>sqrt(n) && q>sqrt(n)인 소수쌍 (p,q)는 존재하지 않는다(n을 넘어버리므로)
+다시 말해 p>sqrt(n)라면 반드시 q<sqrt(n)이다
 
-만약 제곱근보다 큰 소인수가 p,q 두 개 이상이라면 p*q*r*s...>n 이므로 모순이다
+제곱근보다 큰 소인수가 p,q 두 개 이상이라면 p*q*r*s...>n 이므로 모순이다
 
 따라서, 제곱근보다 큰 소인수는 많아봐야 최대 한 개이다.
 */
