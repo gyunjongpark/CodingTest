869b9a139b2d0acb4c6ecc73779dd2d7407f5a69
gyunjongpark <idjkda4034@gmail.com>
2024-07-07 11:02:19 +0900

    Update 003_2304.cpp

--- BOJ/Lecture_02/prefix_sum/003_2304.cpp
+++ BOJ/Lecture_02/prefix_sum/003_2304.cpp
@@ -11,31 +11,30 @@ int main() {
 
 	cin >> n;
 
-	//가장 우축의 최대값을 저장한다
 	for (int i = 0; i < n; i++) {
 		cin >> x >> y;
-		a[x] = y;
-		if (y >= maxVal) {
-			maxVal = y;
+		a[x] = y; //높이 저장
+
+		if (y >= maxVal) { //가장 우측의 최대값을 maxPos, maxVal로 저장한다
 			maxPos = x;
+			maxVal = y;
 		}
 	}
 
-	//왼쪽에서는 마지막 maxPos가 나올 때까지 검사
+	//왼쪽부터 maxPos가 나올 때까지 검사
 	for (int i = 1; i < maxPos; i++) {
 		if (a[i - 1] >= a[i]) {
 			a[i] = a[i - 1];
 		}
 	}
 
-	//오른쪽에서는 첫 maxPos까지 검사
-	for (int i = 1000; i>maxPos; i--) {
+	//오른쪽부터 maxPos까지 검사
+	for (int i = 1000; i > maxPos; i--) {
 		if (a[i + 1] >= a[i]) {
 			a[i] = a[i + 1];
 		}
 	}
 
-	//a를 변형시킨 후 누적합 s 계산
 	for (int i = 1; i <= 1000; i++) {
 		s[i] = s[i - 1] + a[i];
 	}
