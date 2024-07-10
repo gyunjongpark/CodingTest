4b69af8381f592a9b8fd9c2d590dc9e4b802f970
gyunjongpark <idjkda4034@gmail.com>
2024-07-10 22:13:39 +0900

    Update 001_2961.cpp

--- BOJ/Lecture_02/05/001_2961.cpp
+++ BOJ/Lecture_02/05/001_2961.cpp
@@ -4,20 +4,20 @@
 #define INF 1e9
 using namespace std;
 
-vector<pair<int, int>> table;
+vector<pair<int, int>> ingre;
 int n, s, b;
 int answer = INF;
 
 void recur(int idx, int sour, int bitter, int used) {
     if (idx == n) {
         if (used > 0) {
-            answer = min(answer, abs(sour-bitter));
+            answer = min(answer, abs(sour - bitter));
         }
 
         return;
     }
 
-    recur(idx + 1, sour * table[idx].first, bitter + table[idx].second, used + 1);
+    recur(idx + 1, sour * ingre[idx].first, bitter + ingre[idx].second, used + 1);
     recur(idx + 1, sour, bitter, used);
 }
 
@@ -26,15 +26,15 @@ int main() {
     cin.tie(NULL); cout.tie(NULL);
 
     cin >> n;
-    
+
     for (int i = 0; i < n; i++) {
         cin >> s >> b;
-        table.push_back({ s,b });
+        ingre.push_back({ s,b });
     }
 
     recur(0, 1, 0, 0);
 
     cout << answer;
-    
+
     return 0;
 }
