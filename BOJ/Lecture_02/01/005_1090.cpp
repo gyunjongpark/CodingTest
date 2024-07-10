ca2417d961b9ad45a06b81d4deb7fbe0be9a720b
gyunjongpark <idjkda4034@gmail.com>
2024-07-10 15:44:38 +0900

    Update 005_1090.cpp

--- BOJ/Lecture_02/01/005_1090.cpp
+++ BOJ/Lecture_02/01/005_1090.cpp
@@ -25,27 +25,27 @@ int main() {
 			vector<int> dist;
 
 			for (pair<int, int> pi : v) {
-				int d = abs(x - pi.first) + abs(y - pi.second); //문제에 주어진 최소 이동거리 계산법
+				int d = abs(x - pi.first) + abs(y - pi.second); //문제에 주어진 최소 이동거리 계산식
 				dist.push_back(d);
 			}
-			//하나의 좌표에 대하여 각 체커별로 최소 이동거리를 구한 후 정렬
+			//하나의 좌표에 대하여 각 체커별로 최소 이동거리를 구한 후 오름차순 정렬
 			sort(dist.begin(), dist.end());
 
 			int temp = 0;
 			for (int i = 0; i < n; i++) {
-				temp += dist[i]; //i개의 체커가 모이기 위해 필요한 최소 이동거리
+				temp += dist[i]; //하나의 좌표에서 i개의 체커가 모이기 위해 필요한 최소 이동거리
 
 				if (answer[i] == -1) {
 					answer[i] = temp;
 				}
 				else {
-					answer[i] = min(answer[i], temp); //answer의 최소값은 좌표가 바뀌면서 변경될 수 있다
+					answer[i] = min(answer[i], temp); //answer[i]는 기준 좌표가 바뀌면서 변경될 수 있다
 				}
 			}
 		}
 	}
 
-	for (int i : answer) { //1~n개의 체커가 모이기 위한 최소 이동거리 차례대로 출력
+	for (int i : answer) { //n개의 체커가 모이기 위한 최소 이동거리 오름차순 출력
 		cout << i << ' ';
 	}
 
