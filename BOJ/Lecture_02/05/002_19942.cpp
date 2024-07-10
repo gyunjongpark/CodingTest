bcbd60dcfd65ee03a199329a88310fdbbac2678b
gyunjongpark <idjkda4034@gmail.com>
2024-07-10 12:51:08 +0900

    Update 002_19942.cpp

--- BOJ/Lecture_02/05/002_19942.cpp
+++ BOJ/Lecture_02/05/002_19942.cpp
@@ -10,22 +10,20 @@ int n, mp, mf, ms, mv;
 
 void recur(int idx, int p, int f, int s, int v, int cost) {
     if (p >= mp && f >= mf && s >= ms && v >= mv) { //영양 조건 만족 시
-        if (cost < answer) { //갱신되는 가격이 기존보다 싸다면 best_combi 변경
+        if (cost < answer) { //cost가 기존의 answer보다 싸다면 best_combi 변경
             answer = cost;
             best_combi = temp_combi;
         }
     }
 
     /*
-    if(idx==n) 내부에 위 조건문을 중첩하지 않는 이유 :
-    조건문을 이미 만족한다면 굳이 idx가 n이 될 때까지 억지로 cost를 늘려서 조합할 이유가 없음(best가 아니다)
-    조건을 만족한 이후 idx가 증가하면 cost는 기존의 answer보다 무조건 커지기 때문에 문제 의도에 부합하지 않음
-    cost가 최소가 되는 최고의 조합을 찾아야 하므로 idx에 상관없이 조건을 만족하면 answer값을 고정시켜 최소 cost를 유지한다
+    if(idx==n) 내부로 위 조건문을 중첩하지 않는 이유 : 같은 비용의 집합이 하나 이상이면 사전 순으로 가장 빠른 것을 출력한다    
+    즉, 영양 조건을 이미 만족한다면 굳이 idx가 n이 될 때까지 조합할 이유가 없다
 
     하지만, idx가 n이 될 때까지 조건을 만족하지 못할 수도 있기 때문에 idx=n까지는 recur는 수행되어야 한다
     */
 
-    //idx가 n이 되면 무조건 반환
+    //idx가 n이 되면 반환
     if (idx == n) {
         return;
     }
