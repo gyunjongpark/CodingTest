93885db1978298a9ea60f5b20a5bb70414278290
gyunjongpark <idjkda4034@gmail.com>
2024-07-11 10:04:14 +0900

    Update 001_14501.cpp

--- BOJ/Lecture_02/06/001_14501.cpp
+++ BOJ/Lecture_02/06/001_14501.cpp
@@ -16,6 +16,7 @@ int recur(int t) {
 
     //t > n일 때 recur는 -INF를 반환하므로 max로 선택되지 않는다
     //즉, t > n인 dp[t] 배열은 만들어지지 않는다
+    
     //dp[t]를 결정하는 것은 recur(t + 1)의 결과를 알고 비교해야 정할 수 있다
     //recur(t + 1)은 일단 dp[t + 1]이 정의되어야 하므로 dp배열은 최종적으로 n + 1의 size가 필요하다
     dp[t] = max(recur(t + 1), recur(t + table[t].first) + table[t].second);
