e47d2d241d0ab7f4d5895c35947328a01cfef53a
gyunjongpark <idjkda4034@gmail.com>
2024-07-12 09:24:10 +0900

    Update 002_19942.cpp

--- BOJ/Lecture_02/05/002_19942.cpp
+++ BOJ/Lecture_02/05/002_19942.cpp
@@ -11,16 +11,17 @@ int answer = INF;
 
 //같은 비용의 집합이 하나 이상이면 사전 순으로 가장 빠른 것을 출력한다
 //idx가 n이 아니어도 영양 기준을 만족한다면? idx == n에 묶일 필요가 없다!
-//idx == n 조건문 밖에서 answer 도출
+
 void recur(int idx, int p, int f, int s, int v, int cost) {
     if (p >= mp && f >= mf && s >= ms && v >= mv) {
-        if (cost < answer) {
+        if (cost < answer) { //idx == n 조건문 밖에서 answer 도출
             answer = cost;
 
             best_combi = temp_combi;
+            return; //최초로 조건을 만족하는 조합이 가장 비용이 적게 드는 조합이다
         }
     }
-    if (idx == n) return;
+    if (idx == n) return; //idx가 n이 되면 무조건 반환
 
     //식재료를 선택하는 경우
     temp_combi.push_back(idx + 1); //식재료의 번호는 1부터 시작한다
