b5b7a75afa85a4e16b539c4f83f841bfc68c61d0
gyunjongpark <idjkda4034@gmail.com>
2024-07-12 17:12:00 +0900

    Create 001_1937.cpp

---
+++ BOJ/Lecture_02/08/001_1937.cpp
@@ -0,0 +1,78 @@
+#include<iostream>
+#include<vector>
+#define INF 1e9
+#define MAX 504
+using namespace std;
+
+const int dy[] = { -1,0,1,0 };
+const int dx[] = { 0,1,0,-1 };
+vector<vector<int>> dp;
+int answer = -INF;
+int a[MAX][MAX], n;
+
+/* 시간 초과 (단순 재귀)
+int recur(int y, int x) {
+    int ret = 0;
+
+    for (int i = 0; i < 4; i++) {
+        int ny = y + dy[i];
+        int nx = x + dx[i];
+
+        if (ny < 0 || ny >= n || nx < 0 || nx >= n) continue;
+
+        if (a[ny][nx] > a[y][x]) {
+            ret = max(ret, recur(ny,nx) + 1);
+        }
+    }
+    return ret;
+
+    //return max(max(max(recur(y - 1, x), recur(y, x + 1)), recur(y + 1, x)), recur(y, x + 1));
+}
+*/
+
+int recur(int y, int x) {
+    if (dp[y][x]) return dp[y][x];
+
+    for (int i = 0; i < 4; i++) {
+        int ny = y + dy[i];
+        int nx = x + dx[i];
+
+        if (ny < 0 || ny >= n || nx < 0 || nx >= n) continue;
+
+        if (a[ny][nx] > a[y][x]) {
+            dp[y][x] = max(dp[y][x], recur(ny, nx) + 1);
+        }
+    }
+
+    return dp[y][x];
+}
+
+int main() {
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL); cout.tie(NULL);
+
+    cin >> n;
+
+    dp.resize(n + 1, vector<int>(n + 1, 0));
+
+    for (int i = 0; i < n; i++) {
+        for (int j = 0; j < n; j++) {
+            cin >> a[i][j];
+        }
+    }
+
+    for (int i = 0; i < n; i++) {
+        for (int j = 0; j < n; j++) {
+            recur(i, j); //dp[i][j] 채우기
+        }
+    }
+
+    for (int i = 0; i < n; i++) {
+        for (int j = 0; j < n; j++) {
+            answer = max(answer, dp[i][j]);
+        }
+    }
+    cout << answer + 1;
+
+    return 0;
+}
