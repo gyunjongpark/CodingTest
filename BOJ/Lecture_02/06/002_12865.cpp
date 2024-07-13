efe25234ec10d225265daeaaec4530095f9f7528
gyunjongpark <idjkda4034@gmail.com>
2024-07-13 23:54:17 +0900

    Update 002_12865.cpp

--- BOJ/Lecture_02/06/002_12865.cpp
+++ BOJ/Lecture_02/06/002_12865.cpp
@@ -44,33 +44,33 @@ using namespace std;
 vector<pair<int, int>> backpack;
 vector<vector<int>> dp;
 int n, k, w, v;
-int answer = -INF;
 
 int recur(int idx, int weight) {
-    if (idx > n || weight > k) return -INF;
-    if (idx == n) return 0;
+	if (idx > n || weight > k) return -INF;
+	if (idx == n) return 0;
 
-    if (dp[idx][weight]) return dp[idx][weight];
+	if (dp[idx][weight] != -1) return dp[idx][weight]; //이미 정의되었다면 그대로 반환(메모이제이션)
 
-    dp[idx][weight] = max(recur(idx + 1, weight), recur(idx + 1, weight + backpack[idx].first) + backpack[idx].second);
+	//idx >= n이거나 weight > k일 때 recur(idx, weight)는 배열을 만들지 않고 값을 반환한다
+	dp[idx][weight] = max(recur(idx + 1, weight), recur(idx + 1, weight + backpack[idx].first) + backpack[idx].second);
 
-    return dp[idx][weight];
+	return dp[idx][weight];
 }
 
 int main() {
-    ios_base::sync_with_stdio(false);
-    cin.tie(NULL); cout.tie(NULL);
+	ios_base::sync_with_stdio(false);
+	cin.tie(NULL); cout.tie(NULL);
 
-    cin >> n >> k;
+	cin >> n >> k;
 
-    dp.resize(n + 1, vector<int>(k + 1, 0));
+	//가치가 0일 수도 있으므로 -1로 초기화
+	dp.resize(n, vector<int>(k + 1, -1));
 
-    for (int i = 0; i < n; i++) {
-        cin >> w >> v;
-        backpack.push_back({ w,v });
-    }
-
-    cout << recur(0, 0);
-
-    return 0;
+	for (int i = 0; i < n; i++) {
+		cin >> w >> v;
+		backpack.push_back({ w,v });
+	}
+	cout << recur(0, 0); //탑다운 dp
+	
+	return 0;
 }
