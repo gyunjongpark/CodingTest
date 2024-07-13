595ebfe0bc53aac5026eb97e7070345f84e2c2f3
gyunjongpark <idjkda4034@gmail.com>
2024-07-13 23:39:30 +0900

    Update 001_14501.cpp

--- BOJ/Lecture_02/06/001_14501.cpp
+++ BOJ/Lecture_02/06/001_14501.cpp
@@ -12,10 +12,9 @@ int recur(int t) {
     if (t > n) return -INF;
     if (t == n) return 0;
 
-    if (dp[t]) return dp[t]; //이미 정의되었다면 그대로 반환
+    if (dp[t]!=-1) return dp[t]; //이미 정의되었다면 그대로 반환(메모이제이션)
 
-    //t > n일 때 recur(t)는 -INF를 반환하므로 max로 선택되지 않는다
-    //즉, t > n인 dp[t] 배열은 만들어지지 않는다
+    //t >= n일 때 recur(t)는 배열을 만들지 않고 곧바로 값을 반환한다
     dp[t] = max(recur(t + 1), recur(t + table[t].first) + table[t].second);
 
     return dp[t];
@@ -27,16 +26,14 @@ int main() {
 
     cin >> n;
 
-    //dp[t]를 결정하는 것은 recur(t + 1)의 결과를 알고 비교해야 정할 수 있다
-    //recur(t + 1)은 일단 dp[t + 1]이 정의되어야 하므로 dp배열은 최종적으로 n + 1의 size가 필요하다
-    dp.resize(n + 1, 0);
+    //무급(열정페이!)일 수도 있으므로 -1로 초기화
+    dp.resize(n, -1);
 
     for (int i = 0; i < n; i++) {
         cin >> t >> p;
         table.push_back({ t,p });
     }
-
-    cout << recur(0);
+    cout << recur(0); //탑다운 dp
 
     return 0;
 }
