9e0f7743955262c12e02f69ac07b1acf34800fc0
gyunjongpark <idjkda4034@gmail.com>
2024-07-14 21:26:41 +0900

    Update 001_2961.cpp

--- BOJ/Lecture_02/05/001_2961.cpp
+++ BOJ/Lecture_02/05/001_2961.cpp
@@ -9,7 +9,7 @@ int n, s, b;
 int answer = INF;
 
 void recur(int idx, int sour, int bitter, int used) {
-    //끝까지 가봐야 전체 중 최솟값을 알 수 있다 -> idx == n 조건문 내에서 answer 도출
+    //전체 경우의 수 중에서 최소 판단. idx == n 조건문 내에서 answer 도출
     if (idx == n) {
         if (used > 0) {
             answer = min(answer, abs(sour - bitter));
