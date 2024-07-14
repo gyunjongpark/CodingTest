e014c2f867706788da3ed73309a6afd17682153e
gyunjongpark <idjkda4034@gmail.com>
2024-07-14 16:16:32 +0900

    Update 003_14501.cpp

--- BOJ/Lecture_02/05/003_14501.cpp
+++ BOJ/Lecture_02/05/003_14501.cpp
@@ -8,9 +8,9 @@ int n, t, p;
 int answer = -INF;
 
 void recur(int t, int p) {
-    if (t > n) return; //table 범위를 벗어나므로 return
+    if (t > n) return; //table 범위를 벗어나면 return
 
-    //돈을 최대로 벌기 위해서는 끝까지 가보는 것이 무조건 좋다!
+    //돈을 최대로 벌기 위해서는 끝까지 가는 것이 무조건 좋다
     //idx == n 조건문 내에서 answer 도출
     if (t == n) {
         answer = max(answer, p);
