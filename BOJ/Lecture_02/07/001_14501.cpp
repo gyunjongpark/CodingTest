56a20da5ead3fa3ec2ca6d0310d15d2f12d77b19
gyunjongpark <idjkda4034@gmail.com>
2024-07-14 16:34:33 +0900

    Update 001_14501.cpp

--- BOJ/Lecture_02/07/001_14501.cpp
+++ BOJ/Lecture_02/07/001_14501.cpp
@@ -12,18 +12,19 @@ int main() {
 
     cin >> n;
 
-    //인덱스가 n을 넘으면 dp[idx + 1]을 가져오므로 바텀업 dp는 n + 1까지 정의한다
+    //idx = n - 1이고 idx + table[idx].first > n이라면 dp[idx] = dp[idx + 1]. 즉, dp[n]이 필요하다
     //무급(열정페이!)일 수도 있으므로 -1로 초기화
     dp.resize(n + 1, -1);
     
-    dp[n] = 0; //탑다운에서 idx == n일 때 return 0 반환 -> dp[n] = 0
+    //탑다운에서 idx == n일 때 return 0 반환 -> dp[n] = 0. 다시 말해, 변형을 주지 말아야 한다
+    dp[n] = 0; 
 
     for (int i = 0; i < n; i++) {
         cin >> t >> p;
         table.push_back({ t,p });
     }
     
-    //인덱스가 n을 넘으면 dp[idx + 1]을 가져오므로 바텀업 dp는 n + 1까지 정의한다
+    //idx = n - 1이고 idx + table[idx].first > n이라면 dp[idx] = dp[idx + 1]. 즉, dp[n]이 필요하다
     for (int idx = n - 1; idx >= 0; idx--) {
         if (idx + table[idx].first > n) dp[idx] = dp[idx + 1];
         else {
