ff0f3d74d524c0878fe39c54718769599fe50ca2
gyunjongpark <idjkda4034@gmail.com>
2024-07-14 15:57:29 +0900

    Update 001_14501.cpp

--- BOJ/Lecture_02/06/001_14501.cpp
+++ BOJ/Lecture_02/06/001_14501.cpp
@@ -13,7 +13,7 @@ int recur(int t) {
 
     if (dp[t]!=-1) return dp[t]; //이미 정의되었다면 그대로 반환(메모이제이션)
 
-    //t >= n일 때 recur(t)는 배열을 만들지 않고 값을 반환한다
+    //t >= n일 때 recur(t)는 dp 배열을 만들지 않고 값을 반환한다
     dp[t] = max(recur(t + 1), recur(t + table[t].first) + table[t].second);
 
     return dp[t];
@@ -25,6 +25,7 @@ int main() {
 
     cin >> n;
 
+    //t >= n일 때 recur(t)는 dp 배열을 만들지 않고 값을 반환한다
     //무급(열정페이!)일 수도 있으므로 -1로 초기화
     dp.resize(n, -1);
 
