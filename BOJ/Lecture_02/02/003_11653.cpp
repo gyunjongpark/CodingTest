f5f2dc1e49a1912f6b6d263093d63a86dc1842f5
gyunjongpark <idjkda4034@gmail.com>
2024-07-16 13:33:26 +0900

    Update 003_11653.cpp

--- BOJ/Lecture_02/02/003_11653.cpp
+++ BOJ/Lecture_02/02/003_11653.cpp
@@ -12,7 +12,7 @@ int main() {
 
 	int x = n; //소인수 분해를 위한 변수 x
 	for (int i = 2; i <= (int)sqrt(n); i++) {
-		while (x % i == 0) { //while문의 조건 덕분에 소수가 되는 i만으로 반복문이 수행된다
+		while (x % i == 0) { //while문의 조건 덕분에 소수인 i로만 반복문이 수행된다
 			cout << i << '\n';
 			x /= i;
 		}
@@ -25,8 +25,8 @@ int main() {
 }
 
 /*
-n=p*q를 만족하면서 p>sqrt(n) && q>sqrt(n)인 소수쌍 (p,q)는 존재하지 않는다(n을 넘어버리므로)
-다시 말해 p>sqrt(n)라면 반드시 q<sqrt(n)이다
+n=p*q를 만족하면서 p>sqrt(n) && q>sqrt(n)인 소수쌍 (p,q)는 n을 넘어버리므로 존재하지 않는다
+p>sqrt(n)인 p가 존재한다면 반드시 q<sqrt(n)이다
 
 제곱근보다 큰 소인수가 p,q 두 개 이상이라면 p*q*r*s...>n 이므로 모순이다
 
