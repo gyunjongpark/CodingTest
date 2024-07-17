6bdc4b77fe9f5970f30b98e5cb09d114447df962
gyunjongpark <idjkda4034@gmail.com>
2024-07-17 12:01:52 +0900

    Create 013_2309_01.cpp

---
+++ BOJ/BaaaaaaaaaaarkingDog/02/013_2309_01.cpp
@@ -0,0 +1,35 @@
+#include<iostream>
+#include<algorithm> //sort
+#include<utility> //pair
+using namespace std;
+
+int arr[9], sum;
+pair<int, int> not_dwarf;
+
+int main() {
+	ios_base::sync_with_stdio(false);
+	cin.tie(NULL); cout.tie(NULL);
+
+	for (int i = 0; i < 9; i++) {
+		cin >> arr[i];
+		sum += arr[i];
+	}
+
+	sort(arr, arr + 9);
+
+	for (int i = 0; i < 9; i++) {
+		for (int j = i + 1; j < 9; j++) {
+			if (sum - arr[i] - arr[j] == 100) {
+				not_dwarf = { i,j };
+			}
+		}
+	}
+
+	for (int i = 0; i < 9; i++) {
+		if (i == not_dwarf.first || i == not_dwarf.second) continue;
+
+		cout << arr[i] << '\n';
+	}
+
+	return 0;
+}
