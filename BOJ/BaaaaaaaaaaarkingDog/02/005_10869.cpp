31bd767b6878b316c30d73ba813ee84778a2cb53
gyunjongpark <idjkda4034@gmail.com>
2024-07-17 11:09:32 +0900

    Update 005_10869.cpp

--- BOJ/BaaaaaaaaaaarkingDog/02/005_10869.cpp
+++ BOJ/BaaaaaaaaaaarkingDog/02/005_10869.cpp
@@ -1,19 +1,19 @@
 #include<iostream>
 using namespace std;
 
-int a, b;
+int A, B;
 
 int main() {
     ios_base::sync_with_stdio(false);
     cin.tie(NULL); cout.tie(NULL);
 
-    cin >> a >> b;
+    cin >> A >> B;
 
-    cout << a + b << '\n';
-    cout << a - b << '\n';
-    cout << a * b << '\n';
-    cout << a / b << '\n';
-    cout << a % b;
+    cout << A + B << '\n';
+    cout << A - B << '\n';
+    cout << A * B << '\n';
+    cout << A / B << '\n';
+    cout << A % B;
 
     return 0;
 }
