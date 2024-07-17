a16978372f3aa13985764789ea4318dc94ded8e4
gyunjongpark <idjkda4034@gmail.com>
2024-07-17 21:09:27 +0900

    Create 004_3273_01.cpp

---
+++ BOJ/BaaaaaaaaaaarkingDog/03/004_3273_01.cpp
@@ -0,0 +1,22 @@
+#include<iostream>
+using namespace std;
+
+int n, x, answer, arr[1000001], ret[2000001];
+
+int main() {
+	ios_base::sync_with_stdio(false);
+	cin.tie(NULL); cout.tie(NULL);
+
+	cin >> n;
+	for (int i = 0; i < n; i++) cin >> arr[i];
+	cin >> x;
+
+	for (int i = 0; i < n; i++) {
+		if (x - arr[i] > 0 && ret[x - arr[i]]) answer++;
+		ret[arr[i]] = 1;
+	}
+
+	cout << answer;
+
+	return 0;
+}
