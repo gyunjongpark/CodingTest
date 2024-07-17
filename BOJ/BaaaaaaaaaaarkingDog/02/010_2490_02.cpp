e6f70eda15a34e1f5f74df82a9e07af602f09ba2
gyunjongpark <idjkda4034@gmail.com>
2024-07-17 11:31:16 +0900

    Create 010_2490_02.cpp

---
+++ BOJ/BaaaaaaaaaaarkingDog/02/010_2490_02.cpp
@@ -0,0 +1,22 @@
+#include<iostream>
+using namespace std;
+
+int result, input;
+string ret = "DCBAE";
+
+int main() {
+	ios_base::sync_with_stdio(false);
+	cin.tie(NULL); cout.tie(NULL);
+
+	
+    for (int i = 0; i < 3; i++) {
+        result = 0;
+        for (int j = 0; j < 4; j++) {
+            cin >> input;
+            result += input;
+        }
+        cout << ret[result] << '\n';
+    }
+
+	return 0;
+}
