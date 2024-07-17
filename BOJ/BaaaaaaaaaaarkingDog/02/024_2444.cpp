605c9dfb2f1ab900b2b50ab01870de986237f133
gyunjongpark <idjkda4034@gmail.com>
2024-07-17 14:03:34 +0900

    Update 024_2444.cpp

--- BOJ/BaaaaaaaaaaarkingDog/02/024_2444.cpp
+++ BOJ/BaaaaaaaaaaarkingDog/02/024_2444.cpp
@@ -13,12 +13,12 @@ int main() {
 		for (int j = 0; j < n - i - 1; j++) {
 			cout << ' ';
 		}
-		
 		for (int j = 0; j < 2 * i + 1; j++) {
 			cout << '*';
 		}
 		cout << '\n';
 	}
+	
 	for (int i = 0; i < n - 1; i++) {
 		for (int j = 0; j < i + 1; j++) {
 			cout << ' ';
