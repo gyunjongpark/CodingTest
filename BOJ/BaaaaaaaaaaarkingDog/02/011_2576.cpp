232c3ef8a9544b66a97d9d75dcf38b76b091d7bc
gyunjongpark <idjkda4034@gmail.com>
2024-07-17 11:34:30 +0900

    Create 011_2576.cpp

---
+++ BOJ/BaaaaaaaaaaarkingDog/02/011_2576.cpp
@@ -0,0 +1,28 @@
+#include<iostream>
+using namespace std;
+
+int minVal = 100, sum, temp;
+
+int main() {
+	ios_base::sync_with_stdio(false);
+	cin.tie(NULL); cout.tie(NULL);
+
+	
+	for (int i = 0; i < 7; i++) {
+		cin >> temp;
+		if (temp % 2) {
+			sum += temp;
+			minVal = min(minVal, temp);
+		}
+	}
+
+	if (!sum) {
+		cout << -1;
+	}
+	else {
+		cout << sum << '\n';
+		cout << minVal;
+	}
+
+	return 0;
+}
