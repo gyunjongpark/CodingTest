fec0498e9d4cd09d53f9006472fb8eb0a908d1aa
gyunjongpark <idjkda4034@gmail.com>
2024-07-17 11:17:12 +0900

    Create 007_2752_02.cpp

---
+++ BOJ/BaaaaaaaaaaarkingDog/02/007_2752_02.cpp
@@ -0,0 +1,19 @@
+#include<iostream>
+#include<algorithm> //sort
+using namespace std;
+
+int main() {
+    int a[3];
+
+    for (int i = 0; i < 3; i++) {
+        cin >> a[i];
+    }
+
+    sort(a, a + 3);
+
+    for (int i = 0; i < 3; i++) {
+        cout << a[i] << ' ';
+    }
+
+    return 0;
+}
