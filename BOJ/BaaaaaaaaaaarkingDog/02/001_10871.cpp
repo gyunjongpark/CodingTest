1fd0d70ec0e0fe68f34f15697a6c2964529888d0
gyunjongpark <idjkda4034@gmail.com>
2024-07-17 11:06:23 +0900

    Create 001_10871.cpp

---
+++ BOJ/BaaaaaaaaaaarkingDog/02/001_10871.cpp
@@ -0,0 +1,23 @@
+#include<iostream>
+using namespace std;
+
+int N, X, arr[10004];
+
+int main() {
+	ios_base::sync_with_stdio(false);
+	cin.tie(NULL); cout.tie(NULL);
+
+	cin >> N >> X;
+
+	for (int i = 0; i < N; i++) {
+		cin >> arr[i];
+	}
+
+	for (int i = 0; i < N; i++) {
+		if (arr[i] < X) {
+			cout << arr[i] << ' ';
+		}
+	}
+
+	return 0;
+}
