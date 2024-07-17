5a432b5d834e018c7509099f22cdaa3096e8c0f0
gyunjongpark <idjkda4034@gmail.com>
2024-07-17 13:35:27 +0900

    Update 019_2439.cpp

--- BOJ/BaaaaaaaaaaarkingDog/02/019_2439.cpp
+++ BOJ/BaaaaaaaaaaarkingDog/02/019_2439.cpp
@@ -10,13 +10,10 @@ int main() {
 	cin >> n;
 
 	for (int i = 0; i < n; i++) {
-		for (int j = 0; j < n - i - 1; j++) {
-			cout << ' ';
-		}
-		
-		for (int j = 0; j < i + 1; j++) {
+		for (int j = 0; j < n - i; j++) {
 			cout << '*';
 		}
+
 		cout << '\n';
 	}
 
