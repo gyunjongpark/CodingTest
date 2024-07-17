46fc85118e9ade5c14139a712f2149909389b6cc
gyunjongpark <idjkda4034@gmail.com>
2024-07-17 13:37:55 +0900

    Update 020_2440.cpp

--- BOJ/BaaaaaaaaaaarkingDog/02/020_2440.cpp
+++ BOJ/BaaaaaaaaaaarkingDog/02/020_2440.cpp
@@ -6,17 +6,14 @@ int n;
 int main() {
 	ios_base::sync_with_stdio(false);
 	cin.tie(NULL), cout.tie(NULL);
-	
+
 	cin >> n;
 
 	for (int i = 0; i < n; i++) {
 		for (int j = 0; j < n - i; j++) {
 			cout << '*';
 		}
-    
-		for (int j = 0; j < i + 1; j++) {
-			cout << ' ';
-		}
+
 		cout << '\n';
 	}
 
