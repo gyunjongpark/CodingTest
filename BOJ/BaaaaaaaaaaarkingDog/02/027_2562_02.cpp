1ead347d197fe2ea4b88ffb19c9e1115cfc9cebb
gyunjongpark <idjkda4034@gmail.com>
2024-07-17 14:20:22 +0900

    Create 027_2562_02.cpp

---
+++ BOJ/BaaaaaaaaaaarkingDog/02/027_2562_02.cpp
@@ -0,0 +1,18 @@
+#include<iostream>
+using namespace std;
+
+int a[9];
+
+int main() {
+	ios_base::sync_with_stdio(false);
+	cin.tie(NULL); cout.tie(NULL);
+
+	for (int i = 0; i < 9; i++) {
+		cin >> a[i];
+	}
+
+	cout << *max_element(a, a + 9) << '\n';
+	cout << max_element(a, a + 9) - a + 1;
+
+	return 0;
+}
