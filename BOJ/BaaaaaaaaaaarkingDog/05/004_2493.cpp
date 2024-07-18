bc7b33c4baf220b3fcbcbecdb63da97a60051cc4
gyunjongpark <idjkda4034@gmail.com>
2024-07-19 01:41:50 +0900

    Create 004_2493.cpp

---
+++ BOJ/BaaaaaaaaaaarkingDog/05/004_2493.cpp
@@ -0,0 +1,28 @@
+#include<iostream>
+#include<stack>
+using namespace std;
+
+stack<pair<int, int>> stk;
+int n, h;
+
+int main(void) {
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL); cout.tie(NULL);
+
+    cin >> n;
+    stk.push({ 100000001, 0 });
+
+    for (int i = 1; i <= n; i++) {
+        cin >> h;
+
+        while (h > stk.top().first) {
+            stk.pop();
+        }
+
+        cout << stk.top().second << ' ';
+
+        stk.push({ h,i });
+    }
+
+    return 0;
+}
