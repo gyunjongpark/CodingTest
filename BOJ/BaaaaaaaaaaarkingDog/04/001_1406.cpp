15e3940cd671f4e56111b1252fbcf8947934dab8
gyunjongpark <idjkda4034@gmail.com>
2024-07-18 18:33:40 +0900

    Update 001_1406.cpp

--- BOJ/BaaaaaaaaaaarkingDog/04/001_1406.cpp
+++ BOJ/BaaaaaaaaaaarkingDog/04/001_1406.cpp
@@ -2,45 +2,46 @@
 #include<list>
 using namespace std;
 
-list<char> s;
-string str;
-int n;
+list<char> L;
+string input_str;
 char ch, add;
+int n;
 
 int main(void) {
 	ios_base::sync_with_stdio(false);
 	cin.tie(NULL); cout.tie(NULL);
+	
+	cin >> input_str >> n;
 
-	cin >> str;
-	for (char c : str) s.push_back(c);
+	for (char c : input_str) L.push_back(c);
 
-	list<char>::iterator cur = s.end();
-
-	cin >> n;
+	list<char>::iterator cur = L.end();
 
 	while (n--) {
 		cin >> ch;
-		
+
 		if (ch == 'P') {
 			cin >> add;
-			s.insert(cur, add);
+			L.insert(cur, add);
 		}
 		else if (ch == 'L') {
-			if (cur != s.begin()) cur--;
+			if (cur != L.begin()) {
+				cur--;
+			}
 		}
 		else if (ch == 'D') {
-			if (cur != s.end()) cur++;
+			if (cur != L.end()) {
+				cur++;
+			}
 		}
 		else if (ch == 'B') {
-			if (cur != s.begin()) {
+			if (cur != L.begin()) {
 				cur--;
-				cur = s.erase(cur);
-				//erase는 cur 다음 요소의 iter를 반환
-
+				cur = L.erase(cur);
 			}
 		}
 	}
-	for (char c : s) cout << c;
+	for (char c : L) cout << c;
 
 	return 0;
 }
