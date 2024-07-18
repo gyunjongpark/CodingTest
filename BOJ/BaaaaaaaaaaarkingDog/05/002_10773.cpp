35a708e04fb577114adf7de3fa88546ac4451179
gyunjongpark <idjkda4034@gmail.com>
2024-07-18 14:30:22 +0900

    Create 002_10773.cpp

---
+++ BOJ/BaaaaaaaaaaarkingDog/05/002_10773.cpp
@@ -0,0 +1,34 @@
+#include<iostream>
+#include<stack>
+using namespace std;
+
+int k, n, ret;
+stack<int> stk;
+
+int main(void) {
+	ios_base::sync_with_stdio(false);
+	cin.tie(NULL); cout.tie(NULL);
+	
+	cin >> k;
+
+	while (k--) {
+		cin >> n;
+
+		if (n == 0) {
+			if (stk.size()) {
+				stk.pop();
+			}
+		}
+		else {
+			stk.push(n);
+		}
+	}
+
+	while (stk.size()) {
+		ret += stk.top();
+		stk.pop();
+	}
+	cout << ret;
+
+	return 0;
+}
