1783e497e978d2e5c936a582cd1bb35f9d420ed5
gyunjongpark <idjkda4034@gmail.com>
2024-07-19 20:58:49 +0900

    Update 003_1874.cpp

--- BOJ/BaaaaaaaaaaarkingDog/05/003_1874.cpp
+++ BOJ/BaaaaaaaaaaarkingDog/05/003_1874.cpp
@@ -1,12 +1,12 @@
 #include<iostream>
-#include<algorithm> //reverse
 #include<stack>
 #include<vector>
+#include<algorithm> //reverse
 using namespace std;
 
-int n, cnt, arr[100001];
+int n, arr[100001];
 stack<int> stk, ret_stk;
-vector<char> result; // '+'와 '-'를 저장할 벡터
+vector<char> ret;
 
 int main(void) {
     ios_base::sync_with_stdio(false);
@@ -21,32 +21,36 @@ int main(void) {
     reverse(arr, arr + n);
 
     for (int i = 0; i < n; i++) {
-        stk.push(arr[i]);
+        stk.push(arr[i]); //예시 구현
     }
-
+    
+    int cnt = 1;
     while (!stk.empty()) {
-        // 스택의 top이 현재 cnt보다 작다면 NO 출력
-        if (!ret_stk.empty() && stk.top() < ret_stk.top()) {
-            cout << "NO" << '\n';
-            return 0;
-        }
-
-        // 현재 스택의 top이 cnt와 같으면 pop
-        if (!ret_stk.empty() && stk.top() == ret_stk.top()) {
-            result.push_back('-');
-            stk.pop();
-            ret_stk.pop();
-        }
-        else {
-            result.push_back('+');
-            cnt++;
+        if (ret_stk.empty()) { //ret_stk이 비어있을 경우에는 무조건 채운다
+            ret.push_back('+');
             ret_stk.push(cnt);
+            cnt++;
+        }
+
+        if (!ret_stk.empty()) { 
+            if (stk.top() < ret_stk.top()) { //순서가 불가능한 경우
+                cout << "NO" << '\n';
+                return 0;
+            }
+
+            if (stk.top() == ret_stk.top()) { //같으면 소거
+                ret.push_back('-');
+                stk.pop();
+                ret_stk.pop();
+            }
+            else { //같지 않은 경우 stack 채우기
+                ret.push_back('+');
+                ret_stk.push(cnt);
+                cnt++;
+            }
         }
     }
-
-    for (char c : result) {
-        cout << c << '\n';
-    }
+    for (char c : ret) cout << c << '\n';
 
     return 0;
 }
