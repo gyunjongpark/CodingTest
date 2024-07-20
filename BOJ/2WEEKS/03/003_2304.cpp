10621981ea71c0db0b6f74d510dce8ea00c7c5b4
gyunjongpark <idjkda4034@gmail.com>
2024-07-21 02:02:01 +0900

    Update and rename 003_2304.cpp to 003_2304.cpp

--- BOJ/2WEEKS/03/003_2304.cpp
+++ BOJ/2WEEKS/03/003_2304.cpp
@@ -1,8 +1,7 @@
 #include<iostream>
-#define MAX 1004
 using namespace std;
 
-int n, x, y, a[MAX], s[MAX];
+int n, x, y, a[1001], s[1001];
 int maxPos, maxVal;
 
 int main() {
@@ -11,24 +10,24 @@ int main() {
 
 	cin >> n;
 
-	for (int i = 1; i <= n; i++) { //a[1] ~ a[1000]
+	for (int i = 1; i <= n; i++) { // a[1] ~ a[1000]
 		cin >> x >> y;
-		a[x] = y; //높이 저장
+		a[x] = y; // 높이 저장
 
-		if (y >= maxVal) { //가장 우측의 최대값을 maxPos, maxVal로 저장한다
+		if (y >= maxVal) { // 가장 우측의 최대값을 maxPos, maxVal로 저장
 			maxPos = x;
 			maxVal = y;
 		}
 	}
 
-	//왼쪽부터 maxPos 직전까지 검사, a[1]은 비교 대상 없이 그대로
+	// 왼쪽부터 maxPos 직전까지 검사, a[1]은 비교 대상 없이 그대로
 	for (int i = 2; i < maxPos; i++) {
 		if (a[i - 1] >= a[i]) {
 			a[i] = a[i - 1];
 		}
 	}
 
-	//오른쪽부터 maxPos 직전까지 검사, a[1000]은 비교 대상 없이 그대로
+	// 오른쪽부터 maxPos 직전까지 검사, a[1000]은 비교 대상 없이 그대로
 	for (int i = 999; i > maxPos; i--) {
 		if (a[i + 1] >= a[i]) {
 			a[i] = a[i + 1];
