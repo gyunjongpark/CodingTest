e276d1c757b8a5c181529cae346601b9ec78bb35
gyunjongpark <idjkda4034@gmail.com>
2024-07-21 02:16:09 +0900

    Update and rename 004_15652.cpp to 004_15652.cpp

--- BOJ/2WEEKS/04/004_15652.cpp
+++ BOJ/2WEEKS/04/004_15652.cpp
@@ -2,7 +2,7 @@
 #include<vector>
 using namespace std;
 
-int n, m; //n개 중에서 m개를 뽑는다
+int n, m; // n개 중에서 m개를 뽑는다
 
 void combi(int start, vector<int>& v, vector<bool>& visited) {
     if (v.size() == m) {
@@ -34,7 +34,7 @@ int main() {
     cin >> n >> m;
 
     vector<int> v;
-    vector<bool> visited(n + 1, false); //중복 방지
+    vector<bool> visited(n + 1, false);
 
     combi(0, v, visited);
 
