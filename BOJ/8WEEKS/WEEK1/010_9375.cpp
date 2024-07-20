fe9412e766636d94dadc0261e3147a4639739531
gyunjongpark <idjkda4034@gmail.com>
2024-07-20 16:51:44 +0900

    Create 010_9375.cpp

---
+++ BOJ/8WEEKS/WEEK1/010_9375.cpp
@@ -0,0 +1,34 @@
+#include<iostream>
+#include<map>
+using namespace std;
+
+int t, n;
+
+string cloth, type;
+
+int main() {
+	ios_base::sync_with_stdio(false);
+	cin.tie(NULL); cout.tie(NULL);
+
+	cin >> t;
+	
+	while (t--) {
+		cin >> n;
+
+		map<string, int> mp; //TC마다 초기화
+		for (int i = 0; i < n; i++) {
+			cin >> cloth >> type;
+			mp[type]++;
+		}
+
+		int ret = 1; //TC마다 초기화
+		for (pair<string, int> pi : mp) {
+			ret *= (pi.second + 1);	
+		}
+
+		ret--;
+		cout << ret << '\n';
+	}
+
+	return 0;
+}
