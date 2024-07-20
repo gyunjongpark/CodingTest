bc44adcb6fe6d4ad39108e87d1d7677d5fb1e1d0
gyunjongpark <idjkda4034@gmail.com>
2024-07-20 23:37:24 +0900

    Update 011_1213.cpp

--- BOJ/8WEEKS/WEEK1/011_1213.cpp
+++ BOJ/8WEEKS/WEEK1/011_1213.cpp
@@ -3,7 +3,7 @@ using namespace std;
 
 int flag, cnt['Z' + 1];
 string s, ret;
-char mid; //string mid도 가능하다
+char mid; // string mid도 가능하다
 
 int main() {
 	ios_base::sync_with_stdio(false);
@@ -20,7 +20,7 @@ int main() {
 			flag++;
 			cnt[i]--;
 		}
-		if (flag == 2) break; //mid는 1 이하
+		if (flag == 2) break; // mid는 1 이하
 
 		for (int j = 0; j < cnt[i]; j += 2) {
 			ret += (char)i;
@@ -28,8 +28,8 @@ int main() {
 		}
   	}
 	
-	//mid의 타입이 string이라면
-	//if (mid.size()) ret.insert(ret.size() / 2, mid);
+	// mid의 타입이 string이라면
+	// if (mid.size()) ret.insert(ret.size() / 2, mid);
   
 	if (mid) ret.insert(ret.begin() + ret.size() / 2, mid);
 	
