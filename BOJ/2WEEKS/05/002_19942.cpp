ba4bffd54390517b2b3e0125e9f7f38c05b7ce5b
gyunjongpark <idjkda4034@gmail.com>
2024-07-21 02:30:39 +0900

    Update and rename 002_19942.cpp to 002_19942.cpp

--- BOJ/2WEEKS/05/002_19942.cpp
+++ BOJ/2WEEKS/05/002_19942.cpp
@@ -9,28 +9,30 @@ int p, f, s, v, cost;
 int n, mp, mf, ms, mv;
 int answer = INF;
 
-//같은 비용의 집합이 하나 이상이면 사전 순으로 가장 빠른 것을 출력한다
-//idx가 n이 아니어도 영양 기준을 만족한다면 idx == n에 묶일 필요가 없다
+// 같은 비용의 집합이 하나 이상이면 사전 순으로 가장 빠른 것을 출력한다
+// 영양 기준을 만족한다면 굳이 idx는 n까지 가지 않아도 된다
 
 void recur(int idx, int p, int f, int s, int v, int cost) {
     if (p >= mp && f >= mf && s >= ms && v >= mv) {
-        if (cost < answer) { 
-            answer = cost; //idx == n 조건문 밖에서 answer 도출
+
+        // idx == n 조건문 밖에서 answer 도출
+        if (cost < answer) {
+            answer = cost; 
             best_combi = temp_combi;
-            
-            return; //최초로 조건을 만족하는 조합이 가장 비용이 적게 드는 조합이며, 사전 순으로도 가장 빠르다
+
+            return; // 최초로 조건을 만족하는 조합이 가장 비용이 적게 드는 조합이며, 사전 순으로도 가장 빠르다
         }
     }
-    
-    if (idx == n) return; //idx가 n이 되면 무조건 반환
 
-    
-    //식재료를 선택하는 경우
-    temp_combi.push_back(idx + 1); //idx 변화가 없는 시점에 조합을 우선적으로 짠 후, idx + 1로 재귀 함수에 들어간다
+    if (idx == n) return; // idx가 n이 된다면 무조건 반환
+
+
+    // 식재료를 선택하는 경우
+    temp_combi.push_back(idx + 1); // idx 변화가 없는 시점에서 조합을 우선적으로 생성한 후 재귀 함수를 호출한다
     recur(idx + 1, p + ingre[idx][0], f + ingre[idx][1], s + ingre[idx][2], v + ingre[idx][3], cost + ingre[idx][4]);
     temp_combi.pop_back();
 
-    //식재료를 선택하지 않는 경우
+    // 식재료를 선택하지 않는 경우
     recur(idx + 1, p, f, s, v, cost);
 
     return;
@@ -47,7 +49,7 @@ int main() {
         ingre.push_back({ p,f,s,v,cost });
     }
 
-    recur(0, 0, 0, 0, 0, 0); //idx, p, f, s, v, cost
+    recur(0, 0, 0, 0, 0, 0); // idx, p, f, s, v, cost
 
     if (answer == INF) {
         cout << -1;
