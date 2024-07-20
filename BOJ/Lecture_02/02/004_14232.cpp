e74c44dc6af59aa1c311744cfe334599cbc4905a
gyunjongpark <idjkda4034@gmail.com>
2024-07-21 01:45:56 +0900

    Update 004_14232.cpp

--- BOJ/Lecture_02/02/004_14232.cpp
+++ BOJ/Lecture_02/02/004_14232.cpp
@@ -13,15 +13,15 @@ int main() {
 
 	cin >> k;
 	
-	ll n = k; //소인수 분해를 위한 변수 n
+	ll n = k; // 소인수 분해를 위한 변수 n
 	for (int i = 2; i <= (int)sqrt(k); i++) {
 		while (n % i == 0) {
 			v.push_back(i);
-			n /= i; //같은 무게의 보석들은 줄줄이 딸려 나온다
+			n /= i; // 같은 무게의 보석들은 줄줄이 딸려 나온다
 		}
 	}
 	
-	//sqrt(n)보다 크고 유일하면서 마지막인 소인수가 있다면 출력
+	// sqrt(n)보다 큰 소인수가 있다면 출력
 	if (n != 1) v.push_back(n);
 
 	cout << v.size() << '\n';
