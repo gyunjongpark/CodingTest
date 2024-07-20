8d1776cb51c42e394dcf9c6de883e40a692ea3a8
gyunjongpark <idjkda4034@gmail.com>
2024-07-20 15:25:27 +0900

    Update and rename isPrime.cpp to BOJ/8WEEKS/Basic_algorithms/isPrime.cpp

--- BOJ/8WEEKS/Basic_algorithms/isPrime.cpp
+++ BOJ/8WEEKS/Basic_algorithms/isPrime.cpp
@@ -1,30 +1,30 @@
 #include<iostream>
 #include<vector>
-#define MAX 1000 //문제에 제시된 최대 범위
+#define MAX 1000 // 문제에 제시된 최대 범위
 using namespace std;
 
 /*
 에라토스테네스의 체 : O(NloglogN)
 
 지정한 범위 내에서 소수의 개수를 묻는 문제가 있고, 전체 MAX 범위에서 전부 접근해야 하는 문제가 있다
-후자의 경우 vector를 이용하지 말고 chae 배열 자체를 완성하고 시작하는 것이 효율적이다
+후자의 경우 vector를 이용하지 말고 chae 배열 자체를 완성하고 활용하는 것이 효율적이다
 */
 
-bool chae[MAX + 1]; //최대 범위 MAX까지 담는 배열 chae
-int n; //문제에 제시된 범위
+bool chae[MAX + 1]; // 최대 범위 MAX까지 담는 배열 chae
+int n; // 입력으로 제시되는 범위
 
 vector<int> era(int n) {
 	vector<int> v;
 
 	for (int i = 2; i <= n; i++) {
-		if (chae[i]) continue; //이미 지워졌다면 continue
+		if (chae[i]) continue; // 이미 지워졌다면 continue
 		for (int j = i + i; j <= n; j += i) {
-			chae[j] = 1; //i의 배수 지우기(소수만 남기기)
+			chae[j] = 1; // i의 배수 지우기(소수만 남기기)
 		}
 	}
 
 	for (int i = 2; i <= n; i++) {
-		if (!chae[i]) v.push_back(i); //소수라면 push back
+		if (!chae[i]) v.push_back(i); // 소수라면 push back
 	}
 
 	return v;
@@ -34,7 +34,7 @@ vector<int> era(int n) {
 
 /* 어떤 수 하나가 소수인지 판별하기 : O(sqrt(N)) */
 
-int num; //확인하고 싶은 수
+int num; // 확인하고 싶은 수
 
 bool checkPrime(int num) {
 	if (num <= 1) return 0;
