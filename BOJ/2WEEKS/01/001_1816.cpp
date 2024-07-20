fb5e1a3dea9640e5cd9418dea460bae3aef3e849
gyunjongpark <idjkda4034@gmail.com>
2024-07-20 23:16:59 +0900

    Update and rename 001_1816.cpp to 001_1816.cpp

--- BOJ/2WEEKS/01/001_1816.cpp
+++ BOJ/2WEEKS/01/001_1816.cpp
@@ -15,13 +15,14 @@ int main() {
 	while (n--) {
 		cin >> s; //init
 		flag = 0; //init
-
+		
 		for (int i = 2; i < 1000001; i++) {
 			if (s % i == 0) {
 				flag = 1;
 				break;
 			}
 		}
+		
 		if (flag) cout << "NO\n";
 		else cout << "YES\n";
 	}
