ab52a679a8e8b4122d1131c0753f3832bb3567a7
gyunjongpark <idjkda4034@gmail.com>
2024-07-21 02:27:25 +0900

    Update and rename 001_2961.cpp to 001_2961.cpp

--- BOJ/2WEEKS/05/001_2961.cpp
+++ BOJ/2WEEKS/05/001_2961.cpp
@@ -1,16 +1,16 @@
 #include<iostream>
 #include<vector>
 #include<cmath> //abs
-#define INF 1e9
 using namespace std;
 
 vector<pair<int, int>> ingre;
 int n, s, b;
-int answer = INF;
+int answer = 1e9;
 
 void recur(int idx, int sour, int bitter, int used) {
-    
-    //전체 경우의 수 중에서 최솟값 찾기. idx == n 조건문 내에서 answer 도출
+
+    // 전체 경우의 수 중에서 최솟값 찾기
+    // idx == n 조건문 내에서 answer 도출
     if (idx == n) {
         if (used > 0) {
             answer = min(answer, abs(sour - bitter));
@@ -34,9 +34,9 @@ int main() {
         ingre.push_back({ s,b });
     }
 
-    recur(0, 1, 0, 0);
+    recur(0, 1, 0, 0); //초기 상태에서 재귀 함수 실행
 
-    cout << answer;
+    cout << answer; //최종 갱신된 answer 출력
 
     return 0;
 }
