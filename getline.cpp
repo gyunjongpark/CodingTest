3487c3d92c58618adb02ac9e40e42a6a21605895
gyunjongpark <idjkda4034@gmail.com>
2024-07-20 15:22:07 +0900

    Update getline.cpp

--- getline.cpp
+++ getline.cpp
@@ -6,13 +6,14 @@ int T;
 string s;
 
 int main() {
-	cin >> T; // cin은 입력 버퍼에서 \n 직전까지 읽어들이기 때문에 개행이 남아있다
+	// cin은 입력 버퍼에서 \n 직전까지 읽어들이기 때문에 버퍼에는 개행이 남아있다
+	cin >> T;
   
 	string bufferflush;
-	getline(cin, bufferflush); // 버퍼에 있는 값을 전부 읽어서 처리
+	getline(cin, bufferflush); // getline : 버퍼에 있는 값을 전부 읽어서 처리
 
 	for (int i = 0; i < T; i++) {
-		getline(cin, s); // 버퍼에 있는 값을 전부 읽어서 처리
+		getline(cin, s); // getline : 버퍼에 있는 값을 전부 읽어서 처리
 		cout << s << "\n";
 	}
 	// 최종적인 버퍼는 비어있게 된다
