44c6d0514a21cdb0b7466e906518636b3dd3f013
gyunjongpark <idjkda4034@gmail.com>
2024-07-21 02:11:35 +0900

    Update and rename 007_17611.cpp to 007_17611.cpp

--- BOJ/2WEEKS/03/007_17611.cpp
+++ BOJ/2WEEKS/03/007_17611.cpp
@@ -1,11 +1,9 @@
 #include<iostream>
 #include<vector>
-#define ALPHA 500000
-#define MAX 1000004
 using namespace std;
 
+int cnt_x[1000001], cnt_y[1000001], s_x[1000001], s_y[1000001];
 int n, xpos, ypos;
-int cnt_x[MAX], cnt_y[MAX], s_x[MAX], s_y[MAX];
 vector<pair<int, int>> pos;
 
 int main() {
@@ -16,26 +14,28 @@ int main() {
 
 	for (int i = 0; i < n; i++) {
 		cin >> xpos >> ypos;
-		xpos += ALPHA;
-		ypos += ALPHA;
+		xpos += 500000;
+		ypos += 500000;
 		pos.push_back({ xpos,ypos });
 	}
 
-	for (int i = 0; i < n; i++) { //pos[0] ~ pos[n - 1]
+	for (int i = 0; i < n; i++) { // pos[0] ~ pos[n - 1]
 		int x = pos[i].first;
 		int y = pos[i].second;
-		int nx = pos[(i + 1) % n].first; //마지막 좌표는 첫 좌표로 돌아온다
+		int nx = pos[(i + 1) % n].first; // 마지막 좌표에 대한 nx, ny는 첫 좌표로 돌아온다
 		int ny = pos[(i + 1) % n].second;
 
 		if (x == nx) {
 			int max_y = max(y, ny);
 			int min_y = min(y, ny);
+
 			cnt_y[min_y]++;
 			cnt_y[max_y]--;
 		}
 		else {
 			int max_x = max(x, nx);
 			int min_x = min(x, nx);
+
 			cnt_x[min_x]++;
 			cnt_x[max_x]--;
 		}
