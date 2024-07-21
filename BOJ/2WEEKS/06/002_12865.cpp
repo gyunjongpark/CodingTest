ab7c421b3315510e30c617560c6cdd19fa3876eb
gyunjongpark <idjkda4034@gmail.com>
2024-07-21 09:59:30 +0900

    Update 002_12865.cpp

--- BOJ/2WEEKS/06/002_12865.cpp
+++ BOJ/2WEEKS/06/002_12865.cpp
@@ -10,7 +10,7 @@ int recur(int idx, int weight) {
     if (idx > n || weight > k) return -1e9;
     if (idx == n) return 0;
 
-    if (dp[idx][weight]) return dp[idx][weight];
+    if (dp[idx][weight]!=-1) return dp[idx][weight];
 
     dp[idx][weight] = max(recur(idx + 1, weight), recur(idx + 1, weight + table[idx].first) + table[idx].second);
 
@@ -23,7 +23,7 @@ int main() {
 
     cin >> n >> k;
 
-    dp.resize(n + 1, vector<int>(k + 1, 0)); //dp 입력은 push_back이 아니기 때문에 배열 초기화 필요
+    dp.resize(n + 1, vector<int>(k + 1, -1)); //dp 입력은 push_back이 아니기 때문에 배열 초기화 필요
 
     for (int i = 0; i < n; i++) {
         cin >> m >> v;
