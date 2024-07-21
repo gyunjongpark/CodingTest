31c799c5d9b14ae6b0b6a510d74cc8edd79ed959
gyunjongpark <idjkda4034@gmail.com>
2024-07-21 09:22:10 +0900

    Create 003_14501.cpp

---
+++ BOJ/2WEEKS/05/003_14501.cpp
@@ -0,0 +1,36 @@
+#include<iostream>
+#include<vector>
+using namespace std;
+
+int n, t, p, table[15][2];
+int answer = -1;
+
+void recur(int t, int p) {
+    if (t > n) return;
+    if (t == n) {
+        answer = max(answer, p);
+        return;
+    }
+
+    recur(t + 1, p);
+    recur(t + table[t][0], p + table[t][1]);
+
+    return;
+}
+
+int main() {
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL); cout.tie(NULL);
+
+    cin >> n;
+
+    for (int i = 0; i < n; i++) {
+        cin >> table[i][0] >> table[i][1];
+    }
+
+    recur(0,0);
+
+    cout << answer;
+
+    return 0;
+}
