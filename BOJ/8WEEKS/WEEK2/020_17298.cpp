7a3558c214a456620a3ee07b86c0634555ebb7ac
gyunjongpark <idjkda4034@gmail.com>
2024-07-22 23:54:27 +0900

    Update 020_17298.cpp

--- BOJ/8WEEKS/WEEK2/020_17298.cpp
+++ BOJ/8WEEKS/WEEK2/020_17298.cpp
@@ -2,28 +2,32 @@
 #include<stack>
 using namespace std;
 
+stack<int> stk;
 int n, a[1000001], ret[1000001];
-stack<int> s;
 
 int main() {
 	ios_base::sync_with_stdio(false);
 	cin.tie(NULL); cout.tie(NULL);
 
 	cin >> n;
-	fill(&ret[0], &ret[0] + n, -1); // init
 
 	for (int i = 0; i < n; i++) {
 		cin >> a[i];
 	}
 
+	fill(ret, ret + n, -1); // init
+
 	for (int i = 0; i < n; i++) {
-		while (s.size() && a[s.top()] < a[i]) { // top부터 역으로 채우기
-			ret[s.top()] = a[i];
-			s.pop();
+
+		// stack은 갱신할 때마다 size check 필수
+		while (stk.size() && a[stk.top()] < a[i]) {
+			ret[stk.top()] = a[i];
+			stk.pop();
 		}
-		s.push(i);
+		stk.push(i);
 	}
 
+	// n-1까지만 출력
 	for (int i = 0; i < n; i++) {
 		cout << ret[i] << ' ';
 	}
