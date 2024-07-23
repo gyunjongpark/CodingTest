5f2cf7521329a71115d27582511c00822af09ed0
gyunjongpark <idjkda4034@gmail.com>
2024-07-23 10:51:42 +0900

    Update 008.cpp

--- Programmers/Introduction/008.cpp
+++ Programmers/Introduction/008.cpp
@@ -2,10 +2,10 @@
 #include <vector>
 using namespace std;
 
-vector<int> v;
-
 vector<int> solution(vector<int> numbers) {
-    for(int i : numbers) v.push_back(2*i);
+    vector<int> answer;
+    
+    for(int i : numbers) answer.push_back(2*i);
   
-    return v;
+    return answer;
 }
