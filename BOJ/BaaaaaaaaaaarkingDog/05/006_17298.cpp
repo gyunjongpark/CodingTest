0fdeccf3521195f46ced6c3d779536097d90d18c
gyunjongpark <idjkda4034@gmail.com>
2024-07-24 13:50:47 +0900

    Create 006_17298.cpp

---
+++ BOJ/BaaaaaaaaaaarkingDog/05/006_17298.cpp
@@ -0,0 +1,34 @@
+#include<iostream>
+#include<stack>
+using namespace std;
+
+stack<int> stk;
+int n, a[1000001], ret[1000001];
+
+int main(void) {
+	ios_base::sync_with_stdio(false);
+	cin.tie(NULL); cout.tie(NULL);
+
+	cin >> n;
+
+	for (int i = 0; i < n; i++) {
+		cin >> a[i];
+	}
+
+	fill(&ret[0], &ret[0] + n, -1);
+
+	for (int i = 0; i < n; i++) {
+		while(stk.size() && a[stk.top()] < a[i]) {
+			ret[stk.top()] = a[i];
+			stk.pop();
+		}
+
+		stk.push(i);
+	}
+
+	for (int i = 0; i < n; i++) {
+		cout << ret[i] << ' ';
+	}
+
+	return 0;
+}
