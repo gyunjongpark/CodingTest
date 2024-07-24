9915ae2b3555aa04f761b21ac17498748f8988bd
gyunjongpark <idjkda4034@gmail.com>
2024-07-24 13:34:44 +0900

    Update 003_1874.cpp

--- BOJ/BaaaaaaaaaaarkingDog/05/003_1874.cpp
+++ BOJ/BaaaaaaaaaaarkingDog/05/003_1874.cpp
@@ -21,29 +21,29 @@ int main(void) {
     reverse(arr, arr + n);
 
     for (int i = 0; i < n; i++) {
-        stk.push(arr[i]); //예시 구현
+        stk.push(arr[i]);
     }
     
     int cnt = 1;
     while (!stk.empty()) {
-        if (ret_stk.empty()) { //ret_stk이 비어있을 경우에는 무조건 채운다
+        if (ret_stk.empty()) { // 비어있을 경우 무조건 채운다
             ret.push_back('+');
             ret_stk.push(cnt);
             cnt++;
         }
 
         if (!ret_stk.empty()) { 
-            if (stk.top() < ret_stk.top()) { //순서가 불가능한 경우
+            if (stk.top() < ret_stk.top()) { // 순서가 불가능한 경우
                 cout << "NO" << '\n';
                 return 0;
             }
 
-            if (stk.top() == ret_stk.top()) { //같으면 소거
+            if (stk.top() == ret_stk.top()) { // 값이 같으면 소거
                 ret.push_back('-');
                 stk.pop();
                 ret_stk.pop();
             }
-            else { //같지 않은 경우 stack 채우기
+            else { // 같지 않은 경우 stack 채우기
                 ret.push_back('+');
                 ret_stk.push(cnt);
                 cnt++;
