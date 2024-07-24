2eeb42a96304088c8f013be32a9428a0aa6b9b8d
gyunjongpark <idjkda4034@gmail.com>
2024-07-24 13:37:08 +0900

    Update 005_6198.cpp

--- BOJ/BaaaaaaaaaaarkingDog/05/005_6198.cpp
+++ BOJ/BaaaaaaaaaaarkingDog/05/005_6198.cpp
@@ -13,14 +13,14 @@ int main(void) {
 
     cin >> n;
 
-    //monotone stack 방법
+    // monotone stack 방법
     while(n--){
         cin >> h;
         
         while (!stk.empty() && h >= stk.top()) {
             stk.pop();
         }
-        ret += stk.size(); //본인을 내려다 볼 수 있는 경우의 수 합산
+        ret += stk.size(); // 본인을 내려다 볼 수 있는 경우의 수 합산
 
         stk.push(h);
     }
