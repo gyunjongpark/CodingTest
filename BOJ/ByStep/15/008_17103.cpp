d9efe94f3949b7dee69a344abd14a5dcbf595b7d
gyunjongpark <idjkda4034@gmail.com>
2024-07-24 13:16:18 +0900

    Update 008_17103.cpp

--- BOJ/ByStep/15/008_17103.cpp
+++ BOJ/ByStep/15/008_17103.cpp
@@ -1,20 +1,21 @@
 #include<iostream>
-#define MAX 1000004
 using namespace std;
 
 int t, n;
-bool chae[MAX];
+bool chae[1000001];
 
+
+// n의 범위 100만까지 소수 구하기
 void era() {
-	for (int i = 2; i <= MAX; i++) {
+	for (int i = 2; i <= 1000000; i++) {
 		chae[i] = true;
 	}
 
-	for (int i = 2; i <= MAX; i++) {
-		if (!chae[i]) continue; //소수가 아니면 continue
+	for (int i = 2; i <= 1000000; i++) {
+		if (!chae[i]) continue; // 소수가 아니면 continue
 
-		for (int j = i + i; j <= MAX; j += i) {
-			chae[j] = false; //본인을 제외한 배수 false
+		for (int j = i + i; j <= 1000000; j += i) {
+			chae[j] = false; // 본인을 제외한 배수 false
 		}
 	}
 
@@ -25,7 +26,7 @@ int main() {
 	ios_base::sync_with_stdio(false);
 	cin.tie(NULL); cout.tie(NULL);
 
-	era(); //에라토스테네스의 체로 최대 범위 내 소수 구하기
+	era(); // 에라토스테네스의 체로 최대 범위 내 소수 구하기
 	
 	cin >> t;
 
