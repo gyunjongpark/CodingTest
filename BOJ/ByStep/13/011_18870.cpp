be7d908f11b7d617490c86a437d8de9247b9d782
gyunjongpark <idjkda4034@gmail.com>
2024-07-24 13:08:46 +0900

    Update 011_18870.cpp

--- BOJ/ByStep/13/011_18870.cpp
+++ BOJ/ByStep/13/011_18870.cpp
@@ -14,10 +14,13 @@ int main() {
 
 	for (int i = 0; i < n; i++) {
 		cin >> temp;
+		
 		v.push_back(temp);
 		ret.push_back(temp);
 	}
+	
 	sort(v.begin(), v.end());
+	
 	v.erase(unique(v.begin(), v.end()), v.end());
 
 	for (int i = 0; i < n; i++) {
