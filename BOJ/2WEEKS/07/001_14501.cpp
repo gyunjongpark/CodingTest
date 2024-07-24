fbcd0c5433b6b0cc5bb930c80736f8176f959566
gyunjongpark <idjkda4034@gmail.com>
2024-07-24 10:33:34 +0900

    Update 001_14501.cpp

--- BOJ/2WEEKS/07/001_14501.cpp
+++ BOJ/2WEEKS/07/001_14501.cpp
@@ -21,7 +21,7 @@ int main() {
     }
 
     for (int idx = 0; idx < n; idx++) {
-        dp[idx + 1] = max(dp[idx + 1], dp[idx]); // dp의 길이는 n + 1까지 필요하다
+        dp[idx + 1] = max(dp[idx + 1], dp[idx]); // dp의 길이는 n까지 필요하다
 
         if (idx + table[idx].first <= n) {
             dp[idx + table[idx].first] = max(dp[idx + table[idx].first], dp[idx] + table[idx].second);
