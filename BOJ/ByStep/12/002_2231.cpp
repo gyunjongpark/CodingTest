26831e6e6b819d29ebd7be7876c432eadb3ad067
gyunjongpark <idjkda4034@gmail.com>
2024-07-25 22:18:36 +0900

    Update 002_2231.cpp

--- BOJ/ByStep/12/002_2231.cpp
+++ BOJ/ByStep/12/002_2231.cpp
@@ -7,18 +7,18 @@ int main() {
     ios_base::sync_with_stdio(false);
     cin.tie(NULL); cout.tie(NULL);
 
-    cin >> n; // 256을 예로 들면
+    cin >> n; //256을 예로 들면
 
     for (int i = 1; i < n; i++) {
         int sum = 0;
-        int num = i; // i는 1부터 시작해서 245가 되었을 때
+        int num = i; //i는 1부터 시작해서 245가 되었을 때
 
         while (num > 0) {
             sum += num % 10; //5 + 4 + 2
             num /= 10;
         }
 
-        if (sum + i == n) { // 조건을 만족한다
+        if (sum + i == n) { //조건을 만족한다
             ret = i;
             break;
         }
