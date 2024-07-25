2ff859b8ecdedda452a0a45b7f565e2befa0271a
gyunjongpark <idjkda4034@gmail.com>
2024-07-25 22:16:14 +0900

    Update 007_1316.cpp

--- BOJ/ByStep/06/007_1316.cpp
+++ BOJ/ByStep/06/007_1316.cpp
@@ -14,13 +14,13 @@ int main() {
 	for (int i = 0; i < t; i++) {
 		cin >> s;
 
-		if (s.size() <= 2) continue; // 무조건 그룹 단어
-		else { // 그룹 단어가 아닌 것을 찾는다
+		if (s.size() <= 2) continue; //무조건 그룹 단어
+		else {
 			for (int j = 0; j < s.size(); j++) {
 				if (s[j] != s[j + 1]) {
 
-					// find(찾는 요소,시작 위치) : 해당 요소의 인덱스(배열 번호) 반환
-					if (s.find(s[j], j + 2) != string::npos) { // 같은 단어를 찾았다면 그룹 단어가 아니다
+					//find(찾는 요소,시작 위치) : 해당 요소의 인덱스(배열 번호) 반환
+					if (s.find(s[j], j + 2) != string::npos) { //같은 단어를 찾았다면 그룹 단어가 아니다
 						cnt++; //그룹 단어가 아니라면 cnt++
 						break;
 					}
@@ -29,7 +29,7 @@ int main() {
 		}
 	}
 
-	// 전체 단어 - 그룹 단어가 아닌 단어
+	//전체 단어 - 그룹 단어가 아닌 단어
 	cout << t - cnt;
 
 	return 0;
