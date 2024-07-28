c904313001ab4274207e0eadcebea84e49373df8
gyunjongpark <idjkda4034@gmail.com>
2024-07-28 18:52:45 +0900

    Create 002_2178.cpp

---
+++ BOJ/BaaaaaaaaaaarkingDog/09/002_2178.cpp
@@ -0,0 +1,56 @@
+#include<iostream>
+#include<tuple> //tie
+#include<queue>
+using namespace std;
+
+const int dy[] = { -1,0,1,0 };
+const int dx[] = { 0,1,0,-1 };
+int n, m, a[101][101], visited[101][101];
+string s;
+
+void bfs(int y, int x) {
+    visited[y][x] = 1;
+
+    queue<pair<int, int>> q;
+    q.push({ y,x });
+
+    while (q.size()) {
+        int y, x;
+        tie(y, x) = q.front(); q.pop();
+
+        for (int i = 0; i < 4; i++) {
+            int ny = y + dy[i];
+            int nx = x + dx[i];
+
+            if (ny < 0 || ny >= n || nx < 0 || nx >= m) continue;
+
+            if (a[ny][nx] == 1 && !visited[ny][nx]) {
+                visited[ny][nx] = visited[y][x] + 1;
+                q.push({ ny,nx });
+            }
+        }
+    }
+
+    return;
+}
+
+int main() {
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL); cout.tie(NULL);
+
+    cin >> n >> m;
+
+    for (int i = 0; i < n; i++) {
+        cin >> s;
+
+        for (int j = 0; j < m; j++) {
+            a[i][j] = s[j] - '0';
+        }
+    }
+
+    bfs(0, 0);
+
+    cout << visited[n - 1][m - 1];
+
+    return 0;
+}
