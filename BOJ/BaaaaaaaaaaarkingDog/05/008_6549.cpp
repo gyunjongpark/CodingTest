ec3c0522e68d181fd5deb418c205282928a92d2d
gyunjongpark <idjkda4034@gmail.com>
2024-08-01 22:55:04 +0900

    Create 008_6549.cpp

---
+++ BOJ/BaaaaaaaaaaarkingDog/05/008_6549.cpp
@@ -0,0 +1,46 @@
+#include<iostream>
+#include<stack>
+using namespace std;
+typedef long long ll;
+
+int n, h, idx; //init
+
+int main(void) {
+	ios_base::sync_with_stdio(false);
+	cin.tie(NULL); cout.tie(NULL);
+
+	while (true) {		
+		cin >> n;
+
+		if (n == 0) break;
+
+		stack<pair<ll, ll>> stk; //init
+		ll ret = 0; //init
+
+		for (int i = 0; i < n; i++) {
+			cin >> h;
+
+			idx = i;
+
+			while (stk.size() && stk.top().first >= h) {
+				ret = max(ret, (i - stk.top().second) * stk.top().first);
+
+				idx = stk.top().second;
+
+				stk.pop();
+			}
+
+			stk.push({ h,idx });
+		}
+
+		while (stk.size()) {
+			ret = max(ret, (n - stk.top().second) * stk.top().first);
+
+			stk.pop();
+		}
+
+		cout << ret << '\n';
+	}
+
+	return 0;
+}
