ce670a4198d858be95565c7ae81510f08c78fb78
gyunjongpark <idjkda4034@gmail.com>
2024-08-03 16:22:55 +0900

    Create dfs.cpp

---
+++ BOJ/BaaaaaaaaaaarkingDog/10/dfs.cpp
@@ -0,0 +1,18 @@
+//dfs 기본형
+
+void dfs(int y, int x) {
+    visited[y][x] = 1;
+
+    for (int i = 0; i < 4; i++) {
+        int ny = y + dy[i];
+        int nx = x + dx[i];
+
+        if (ny < 0 || ny >= n || nx < 0 || nx >= m) continue;
+
+        if (a[ny][nx] == 1 && !visited[ny][nx]) {
+            dfs(ny, nx);
+        }
+    }
+
+    return;
+}
