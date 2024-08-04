89284831d0793b81ad88853f2a747bdc5e14c530
gyunjongpark <idjkda4034@gmail.com>
2024-08-04 11:29:07 +0900

    Update 007_3015.cpp

--- BOJ/BaaaaaaaaaaarkingDog/05/007_3015.cpp
+++ BOJ/BaaaaaaaaaaarkingDog/05/007_3015.cpp
@@ -16,19 +16,19 @@ int main(void) {
 	while (n--) {
 		cin >> h;
 
-		int cnt = 1;
+		int cnt = 1; //본인 포함 같은 키인 사람의 수(초기값은 본인 혼자이므로 1)
 
-		while (stk.size() && stk.top().first <= h) {
-			ret += stk.top().second;
+		while (stk.size() && stk.top().first <= h) { //현재 키가 이전보다 같거나 크다면
+			ret += stk.top().second; //현재 키로 볼 수 있는 사람의 수 ret에 추가
 
-			if (stk.top().first == h) {
+			if (stk.top().first == h) { //현재 키가 이전 키와 같다면 이전까지 같은 키의 사람을 더해서 cnt 갱신
 				cnt += stk.top().second;
 			}
 
-			stk.pop();
+			stk.pop(); //현재 키보다 작은 사람들은 더 이상 계산에 쓰이지 않는다
 		}
 
-		if (stk.size()) { //붙어있는 h가 존재한다면 무조건 1 추가
+		if (stk.size()) { //현재 키보다 큰 사람이 있다면(키 큰 사람 이전은 pop 연산으로 이미 삭제되었기 때문에 유일하다. 따라서 1 추가)
 			ret++;
 		}
 
