e9b6d7d27cfb9cf6f377ecb23156b96b105dd226
gyunjongpark <idjkda4034@gmail.com>
2024-08-06 15:12:16 +0900

    Update 008_17103.cpp

--- BOJ/ByStep/15/008_17103.cpp
+++ BOJ/ByStep/15/008_17103.cpp
@@ -5,7 +5,7 @@ int t, n;
 bool chae[1000001];
 
 
-//n의 범위 100만까지 소수 구하기
+//n의 범위 100만까지 배열 chae 완성
 void era() {
 	for (int i = 2; i <= 1000000; i++) {
 		chae[i] = true;
@@ -26,22 +26,21 @@ int main() {
 	ios_base::sync_with_stdio(false);
 	cin.tie(NULL); cout.tie(NULL);
 
-	era(); //에라토스테네스의 체로 최대 범위 내 소수 구하기
-	
+	era(); //에라토스테네스의 체로 최대 범위 내 소수 배열 chae 완성
+
 	cin >> t;
 
 	while (t--) {
 		cin >> n;
 
-		if (n == 0) break;
-
 		int cnt = 0;
+
 		for (int i = 2; i <= n / 2; i++) {
-			if (chae[i] && chae[n - i]) {
+			if (chae[i] && chae[n - i]) { //골드바흐의 추측: 2보다 큰 짝수는 두 소수의 합으로 나타낼 수 있다.
 				cnt++;
 			}
 		}
-    
+
 		cout << cnt << '\n';
 	}
 
