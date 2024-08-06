e44c3856985ed9f0abcae5bec7756a8312133e4a
gyunjongpark <idjkda4034@gmail.com>
2024-08-06 16:09:27 +0900

    Update 007_2164.cpp

--- BOJ/ByStep/16/007_2164.cpp
+++ BOJ/ByStep/16/007_2164.cpp
@@ -14,11 +14,13 @@ int main() {
 	for (int i = 1; i <= n; i++) {
 		q.push(i);
 	}
+	
 	while (q.size() != 1) {
 		q.pop();
 		q.push(q.front());
 		q.pop();
 	}
+	
 	cout << q.front();
 
 	return 0;
