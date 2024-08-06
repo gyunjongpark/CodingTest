d8719be55de1d389482b322ef29e9e8b61532b50
gyunjongpark <idjkda4034@gmail.com>
2024-08-07 08:41:10 +0900

    Create split.cpp

---
+++ split.cpp
@@ -0,0 +1,33 @@
+#include<iostream>
+#include<vector>
+using namespace std;
+
+vector<string> split(string input, string delimiter) {
+    vector<string> result;
+
+    int start = 0;
+    int end = input.find(delimiter); //배열번호(인덱스 반환)
+
+    while (end != string::npos) {
+        result.push_back(input.substr(start, end - start));
+        start = end + delimiter.size();
+        end = input.find(delimiter, start);
+    }
+
+    result.push_back(input.substr(start));
+
+    return result;
+}
+
+int main() {
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL); cout.tie(NULL);
+    
+    string s = "hello world! my name is park!";
+
+    vector<string> v = split(s, " ");
+
+    for (string s : v) cout << s << '\n';
+
+    return 0;
+}
