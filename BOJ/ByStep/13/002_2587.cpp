6f048438785687f9539045ca2009cf28f7d2d5bb
gyunjongpark <idjkda4034@gmail.com>
2024-08-06 14:17:23 +0900

    Update 002_2587.cpp

--- BOJ/ByStep/13/002_2587.cpp
+++ BOJ/ByStep/13/002_2587.cpp
@@ -1,9 +1,8 @@
 #include<iostream>
-#include<algorithm>
+#include<algorithm> //sort
 using namespace std;
 
-int a[5];
-int sum;
+int a[5], sum;
 
 int main() {
 	ios_base::sync_with_stdio(false);
@@ -12,9 +11,13 @@ int main() {
 	for (int i = 0; i < 5; i++) {
 		cin >> a[i];
 	}
+
 	sort(a, a + 5);
 
-	for (int i : a) sum += i;
+	for (int i : a) {
+		sum += i;
+	}
+
 	cout << sum / 5 << '\n' << a[2];
 
 	return 0;
