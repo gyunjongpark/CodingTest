d43412e443fb7d7144c8f6bc4ef17a8d52940b71
gyunjongpark <idjkda4034@gmail.com>
2024-08-07 13:54:03 +0900

    Create 004_10799_02.cpp

---
+++ BOJ/BaaaaaaaaaaarkingDog/08/004_10799_02.cpp
@@ -0,0 +1,35 @@
+#include<iostream>
+#include<stack>
+using namespace std;
+
+string s;
+stack<char> stk;
+int ret;
+
+int main() {
+	ios_base::sync_with_stdio(false);
+	cin.tie(NULL); cout.tie(NULL);
+
+    cin >> s;
+    
+    for (int i = 0; i < s.size(); i++) {
+        if (s[i] == '(') {
+            stk.push(s[i]);
+        }
+
+        if (s[i] == ')') {
+            stk.pop();
+
+            if (s[i - 1] == '(') {
+                ret += stk.size();
+            }
+            else {
+                ret++;
+            }
+        }
+    }
+
+	cout << ret;
+
+	return 0;
+}
