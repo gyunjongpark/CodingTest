36070943fe797ac34218b1d49e062193feccbfab
gyunjongpark <idjkda4034@gmail.com>
2024-08-07 13:59:52 +0900

    Update 001_1926.cpp

--- BOJ/BaaaaaaaaaaarkingDog/09/001_1926.cpp
+++ BOJ/BaaaaaaaaaaarkingDog/09/001_1926.cpp
@@ -26,6 +26,7 @@ int dfs(int y, int x) {
 
 	return ret;
 }
+
 int main() {
 	ios_base::sync_with_stdio(false);
 	cin.tie(NULL); cout.tie(NULL);
@@ -52,7 +53,6 @@ int main() {
 		maxVal = *(max_element(v.begin(), v.end()));
 	}
 	
-
 	cout << ret << '\n';
 	cout << maxVal;
 
