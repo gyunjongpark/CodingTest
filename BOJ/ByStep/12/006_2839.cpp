9b41f37444a5be25b8956b35119d0ebc74b704f4
gyunjongpark <idjkda4034@gmail.com>
2024-08-10 08:52:40 +0900

    Update 006_2839.cpp

--- BOJ/ByStep/12/006_2839.cpp
+++ BOJ/ByStep/12/006_2839.cpp
@@ -13,15 +13,19 @@ int main() {
 	while (true) {
 		if (a < 0) {
 			cout << -1;
-			return 0; //즉시 종료
+			
+			return 0;
 		}
 
 		if ((n - a * 5) % 3 == 0) {
 			b = (n - a * 5) / 3;
+			
 			break; //n이 5와 3으로 정확하게 분할된다면 break
 		}
-		a--; //정확하게 나눌 수 없는 경우 a를 줄이고 b로 다시 분할해보기
+		
+		a--; //정확하게 나눌 수 없는 경우 a를 줄이고 b로 다시 분할
 	}
+	
 	cout << a + b;
 
 	return 0;
