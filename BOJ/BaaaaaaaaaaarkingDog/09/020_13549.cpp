9820599db2b65d9f707abe42552feb3bb4d8e103
gyunjongpark <idjkda4034@gmail.com>
2024-08-10 23:00:12 +0900

    Create 020_13549.cpp

---
+++ BOJ/BaaaaaaaaaaarkingDog/09/020_13549.cpp
@@ -0,0 +1,49 @@
+#include<iostream>
+#include<queue>
+using namespace std;
+
+int n, k, visited[100001];
+
+void bfs(int n) {
+    visited[n] = 1;
+
+    queue<int> q;
+    q.push(n);
+
+    while (!q.empty()) {
+        int now = q.front(); q.pop();
+
+        if (now == k) break;
+
+        //가중치가 다른 경우를 먼저 push한다
+        if (now * 2 <= 100000 && !visited[now * 2]) {
+            visited[now * 2] = visited[now];
+            q.push(now * 2);
+        }
+
+        if (now - 1 >= 0 && !visited[now - 1]) {
+            visited[now - 1] = visited[now] + 1;
+            q.push(now - 1);
+        }
+
+        if (now + 1 <= 100000 && !visited[now + 1]) {
+            visited[now + 1] = visited[now] + 1;
+            q.push(now + 1);
+        }
+    }
+
+    return;
+}
+
+int main() {
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL); cout.tie(NULL);
+
+    cin >> n >> k;
+
+    bfs(n);
+
+    cout << visited[k] - 1;
+
+    return 0;
+}
