d6875ab5c06371a9b76ae5f3f8580415254e5dc1
gyunjongpark <idjkda4034@gmail.com>
2024-08-10 23:12:43 +0900

    Update 020_13549.cpp

--- BOJ/BaaaaaaaaaaarkingDog/09/020_13549.cpp
+++ BOJ/BaaaaaaaaaaarkingDog/09/020_13549.cpp
@@ -15,7 +15,7 @@ void bfs(int n) {
 
         if (now == k) break;
 
-        //가중치가 다른 경우를 먼저 push한다
+        //가중치가 다른 경우를 우선적으로 push
         if (now * 2 <= 100000 && !visited[now * 2]) {
             visited[now * 2] = visited[now];
             q.push(now * 2);
