f98b0f1567fd17d28a7ee2cde4ca85d042cd1959
gyunjongpark <idjkda4034@gmail.com>
2024-08-13 09:40:44 +0900

    Update 002.sql

--- Programmers/SQL_Kit/SELECT/002.sql
+++ Programmers/SQL_Kit/SELECT/002.sql
@@ -1,8 +1,15 @@
 SELECT DR_NAME, DR_ID, MCDP_CD, DATE_FORMAT(HIRE_YMD, '%Y-%m-%d') AS HIRE_YMD
 FROM DOCTOR
-WHERE MCDP_CD='CS' OR MCDP_CD='GS' -- WHERE MCDP_CD in ('CS', 'GS')
+WHERE MCDP_CD='CS' OR MCDP_CD='GS'
 ORDER BY HIRE_YMD DESC, DR_NAME ASC;
 
 -- 형식에 맞게 레이블 출력하기 : DATE_FORMAT(속성, '포멧 형식') AS 대신 나타낼 속성 명
 -- %Y-%M-%D, %Y-%m-%d ... 대소문자 구분하기
 -- 행 데이터 = 튜플 = 레코드 = row / 속성 = 레이블 / 필드 = 속성(레이블) + 데이터 = 열 데이터 = column
+
+/* 다른 풀이
+SELECT DR_NAME, DR_ID, MCDP_CD, DATE_FORMAT(HIRE_YMD, '%Y-%m-%d') AS HIRE_YMD
+FROM DOCTOR
+WHERE MCDP_CD in ('CS', 'GS')
+ORDER BY HIRE_YMD DESC, DR_NAME ASC;
+*/
