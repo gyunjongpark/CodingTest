3793158ba5f4d42e90e470923335c83f408571c8
gyunjongpark <idjkda4034@gmail.com>
2024-08-16 23:45:23 +0900

    Update 005_2292.cpp

--- BOJ/ByStep/08/005_2292.cpp
+++ BOJ/ByStep/08/005_2292.cpp
@@ -10,7 +10,7 @@ int main() {
     cin >> n;
 
     if (n == 1) cout << 1;
-    else {
+    else { //너무 신박해서 출제될 확률은 희박할 듯...
         for (int sum = 2; sum <= n; i++) {
             sum += 6 * i;
         }
