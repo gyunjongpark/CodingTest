cbf555de13a113cf09d764399c082729482be766
gyunjongpark <idjkda4034@gmail.com>
2024-08-16 21:57:01 +0900

    Update 021_01.sql

--- Programmers/SQL_Kit/SELECT/021_01.sql
+++ Programmers/SQL_Kit/SELECT/021_01.sql
@@ -2,7 +2,8 @@ SELECT ITEM_INFO.ITEM_ID, ITEM_INFO.ITEM_NAME, ITEM_INFO.RARITY
 FROM ITEM_INFO
 WHERE ITEM_INFO.ITEM_ID IN (
     SELECT ITEM_TREE.ITEM_ID
-    FROM ITEM_TREE WHERE ITEM_TREE.PARENT_ITEM_ID IN (
+    FROM ITEM_TREE
+    WHERE ITEM_TREE.PARENT_ITEM_ID IN (
                             SELECT ITEM_ID
                             FROM ITEM_INFO
                             WHERE RARITY = 'RARE'
@@ -13,8 +14,7 @@ ORDER BY ITEM_INFO.ITEM_ID DESC;
 /*
 부속질의부터 천천히 접근하는 문제
 
-1. rare인 아이템 id를 ITEM 테이블에서 찾는다
-2. 해당 id가 TREE 테이블의 부모 id가 되고, 업그레이드 할 수 있는 id를 TREE에서 찾는다
-3. 찾은 id를 다시 ITEM 테이블에서 조회한 후 최종 결과를 얻는다
-
+1. rare 등급 아이템의 ITEM_ID를 ITEM_INFO 테이블에서 찾는다
+2. INFO에서 찾은 ITEM_ID 중 TREE 테이블의 PARENT_ITEM_ID에 포함되어 업그레이드 할 수 있는 ITEM_ID를 TREE 테이블에서 찾는다
+3. TREE에서 찾은 ITEM_ID를 다시 ITEM 테이블에서 조회한 후 최종 결과를 얻는다
 */
