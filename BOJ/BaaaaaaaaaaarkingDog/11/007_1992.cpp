c90fe61863c482b6c1cd4cc7fb646c3a1b523543
gyunjongpark <idjkda4034@gmail.com>
2024-08-17 11:24:14 +0900

    Update 007_1992.cpp

--- BOJ/BaaaaaaaaaaarkingDog/11/007_1992.cpp
+++ BOJ/BaaaaaaaaaaarkingDog/11/007_1992.cpp
@@ -4,7 +4,7 @@ using namespace std;
 string a[65][65], s;
 int n;
 
-string quad(int y, int x, int size) {
+string recur(int y, int x, int size) {
 	if (size == 1) return a[y][x];
 
 	string ret = "";
@@ -15,10 +15,10 @@ string quad(int y, int x, int size) {
 
 				// divide and conquer
 				ret += "(";
-				ret += quad(y, x, size / 2);
-				ret += quad(y, x + size / 2, size / 2);
-				ret += quad(y + size / 2, x, size / 2);
-				ret += quad(y + size / 2, x + size / 2, size / 2);
+				ret += recur(y, x, size / 2);
+				ret += recur(y, x + size / 2, size / 2);
+				ret += recur(y + size / 2, x, size / 2);
+				ret += recur(y + size / 2, x + size / 2, size / 2);
 				ret += ")";
 
 				return ret;
@@ -44,7 +44,7 @@ int main() {
 		}
 	}
 
-	cout << quad(0, 0, n);
+	cout << recur(0, 0, n);
 
 	return 0;
 }
