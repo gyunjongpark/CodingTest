c59868ed3b516683f3a0914cc1f3a4abc437d20f
gyunjongpark <idjkda4034@gmail.com>
2024-08-18 15:41:15 +0900

    Create 008_15655.cpp

---
+++ BOJ/BaaaaaaaaaaarkingDog/12/008_15655.cpp
@@ -0,0 +1,58 @@
+#include<iostream>
+#include<vector>
+#include<algorithm>
+using namespace std;
+
+int n, m; //n개 중에서 m개를 뽑는다
+int a[10];
+
+void print(vector<int>& v) {
+    for (int i : v) {
+        cout << i << ' ';
+    }
+    cout << '\n';
+
+    return;
+}
+
+void combi(int start, vector<int>& v, vector<bool>& visited) {
+    if (v.size() == m) {
+        print(v);
+
+        return;
+    }
+
+    for (int i = start + 1; i < n; i++) {
+        //if (visited[a[i]]) continue;
+
+        v.push_back(a[i]);
+        //visited[a[i]] = true;
+
+        combi(i, v, visited); //다음 수는 현재 수보다 큰 수부터 순회하므로 visited가 필요없다
+
+        v.pop_back();
+        //visited[a[i]] = false;
+    }
+
+    return;
+}
+
+int main() {
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL); cout.tie(NULL);
+
+    cin >> n >> m; //1 ~ n까지의 수 중에서 m개를 뽑는다
+
+    for (int i = 0; i < n; i++) {
+        cin >> a[i];
+    }
+
+    sort(a, a + n);
+
+    vector<int> v;
+    vector<bool> visited(10001, false); //중복 방지를 위한 visited
+
+    combi(-1, v, visited); //idx를 다루어야 하므로 start = -1
+
+    return 0;
+}
