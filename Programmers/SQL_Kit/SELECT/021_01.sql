4e0a8dd963c8b86bf64e0b3c556a251af3da94be
gyunjongpark <idjkda4034@gmail.com>
2024-08-18 11:04:43 +0900

    Update 021_01.sql

--- Programmers/SQL_Kit/SELECT/021_01.sql
+++ Programmers/SQL_Kit/SELECT/021_01.sql
@@ -13,7 +13,6 @@ ORDER BY ITEM_INFO.ITEM_ID DESC;
 
 /*
 부속질의부터 천천히 접근하는 문제
-
 1. rare 등급 아이템의 ITEM_ID를 ITEM_INFO 테이블에서 찾는다
 2. INFO에서 찾은 ITEM_ID 중 TREE 테이블의 PARENT_ITEM_ID에 포함되어 업그레이드 할 수 있는 ITEM_ID를 TREE 테이블에서 찾는다
 3. TREE에서 찾은 ITEM_ID를 다시 ITEM 테이블에서 조회한 후 최종 결과를 얻는다
