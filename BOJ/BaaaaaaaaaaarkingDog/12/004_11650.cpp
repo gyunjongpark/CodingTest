04deb1170300d4ef04cd05f873653487da3b2049
gyunjongpark <idjkda4034@gmail.com>
2024-08-18 15:35:40 +0900

    Update 004_11650.cpp

--- BOJ/BaaaaaaaaaaarkingDog/12/004_11650.cpp
+++ BOJ/BaaaaaaaaaaarkingDog/12/004_11650.cpp
@@ -26,7 +26,7 @@ void combi(int start, vector<int>& v, vector<bool>& visited) {
         v.push_back(i);
         visited[i] = true;
 
-        combi(i, v, visited); //다음 수는 현재 i보다 큰 수부터 순회
+        combi(i, v, visited); //다음 i는 현재 i보다 큰 수부터 순회
 
         v.pop_back();
         visited[i] = false;
