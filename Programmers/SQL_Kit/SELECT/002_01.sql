74b8b11b18a2cb6546ae472b11eb5eaa4f79f3cf
gyunjongpark <idjkda4034@gmail.com>
2024-08-19 10:59:11 +0900

    Update 002_01.sql

--- Programmers/SQL_Kit/SELECT/002_01.sql
+++ Programmers/SQL_Kit/SELECT/002_01.sql
@@ -3,6 +3,6 @@ FROM DOCTOR
 WHERE MCDP_CD='CS' OR MCDP_CD='GS'
 ORDER BY HIRE_YMD DESC, DR_NAME ASC;
 
--- 형식에 맞게 레이블 출력하기 : DATE_FORMAT(속성, '포멧 형식') AS 대신 나타낼 속성 명
+-- 형식에 맞게 레이블 출력하기 : DATE_FORMAT(속성, '포멧 형식') AS 대신 나타낼 레이블 명
 -- %Y-%M-%D, %Y-%m-%d ... 대소문자 구분하기
 -- 행 데이터 = 튜플 = 레코드 = row / 속성 = 레이블 / 필드 = 속성(레이블) + 데이터 = 열 데이터 = column
