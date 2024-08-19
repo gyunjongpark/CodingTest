87d76e743feeeac760f801b55556eba4f7740563
gyunjongpark <idjkda4034@gmail.com>
2024-08-19 15:54:03 +0900

    Update 002_9663.cpp

--- BOJ/BaaaaaaaaaaarkingDog/12/002_9663.cpp
+++ BOJ/BaaaaaaaaaaarkingDog/12/002_9663.cpp
@@ -4,11 +4,11 @@ using namespace std;
 int n, ret, col[16];
 
 bool check(int level) {
-    for (int i = 0; i < level; i++) { //level 이전까지 열들에 대하여
-        //같은 행에 queen이 있거나
-        //행 차이 = 열 차이일 경우(대각선일 경우) queen을 놓을 수 없다
-        //level이 i보다 큰 열이므로 level - i로 연산 진행
-        if (col[level] == col[i] || abs(col[level] - col[i]) == level - i) {
+    for (int i = 0; i < level; i++) { //level 이전까지 행들에 대하여
+        //같은 열에 queen이 있거나
+        //행 차이와 열 차이가 같을 경우(대각선) queen을 놓을 수 없다
+        //level이 반복되는 i보다 무조건 큰 행이므로 level - i로 연산 진행
+        if (col[level] == col[i] || (level - i) == abs(col[level] - col[i])) {
             return false;
         }
     }
@@ -16,17 +16,18 @@ bool check(int level) {
     return true;
 }
 
-void nQueen(int x) { //x열에 queen을 배치하는 경우의 수 계산
-    if (x == n) { //n열에 문제없이 도달했다면
+void nQueen(int x) { //x행에 queen을 배치하는 경우의 수 계산
+    if (x == n) { //x가 n까지 문제없이 도달했다면
         ret++;
+
         return;
     }
 
     for (int i = 0; i < n; i++) {
-        col[x] = i; //x열 i행에 queen 저장
+        col[x] = i; //x행 i열에 queen 저장
 
-        if (check(x)) { //x열 i행에 queen을 놓을 수 있는지 check
-            nQueen(x + 1); //x열 임의의 행에 queen을 놓았다면 x + 1열 조사(같은 열에서는 queen이 중복되었는지 검사할 필요가 없다)
+        if (check(x)) { //x행에 queen을 놓을 수 있는지 check
+            nQueen(x + 1); //x행 임의의 열에 queen을 놓았다면 x + 1행으로 바로 넘어간다(같은 행에서 queen은 중복될 수 없다)
         }
     }
 
