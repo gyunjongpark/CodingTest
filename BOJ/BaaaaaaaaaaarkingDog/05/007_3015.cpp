a2214111c4c87202871bad46d04a359d9b1aa8e6
gyunjongpark <idjkda4034@gmail.com>
2024-08-19 13:50:26 +0900

    Update 007_3015.cpp

--- BOJ/BaaaaaaaaaaarkingDog/05/007_3015.cpp
+++ BOJ/BaaaaaaaaaaarkingDog/05/007_3015.cpp
@@ -21,18 +21,18 @@ int main(void) {
 		while (stk.size() && stk.top().first <= h) { //현재 입력된 키가 이전의 값들보다 같거나 크다면
 			ret += stk.top().second; //현재 키로 볼 수 있는 사람들의 수를 ret에 추가
 
-			if (stk.top().first == h) { //현재 키가 이전 키와 정확히 같다면, 이전 키의 사람이 볼 수 있었던 사람들(cnt)을 현재 키의 cnt로 갱신
+			if (stk.top().first == h) { //현재 키가 이전 키와 정확히 같다면, 이전 키의 사람이 볼 수 있었던 사람들(stk.top().second)을 현재 키의 cnt에 추가
 				cnt += stk.top().second;
 			}
 
 			stk.pop(); //계산 완료 후 pop
 		}
 
-		if (stk.size()) { //현재 키보다 큰 사람이 남아있다면
-			ret++; //키 큰 사람 이전의 사람은 키 큰 사람에 의해서 가려지기 때문에 무조건 1만 추가된다
+		if (stk.size()) { //현재 입력된 키보다 큰 사람이 있다면
+			ret++; //현재보다 키가 큰 사람 이전의 사람들은 키 큰 사람에 의해서 가려지기 때문에 ret은 무조건 1씩 추가된다
 		}
 
-		stk.push({ h, cnt });
+		stk.push({ h, cnt }); //(현재 키, 현재 키로 볼 수 있는 사람들의 수)
 	}
 
 	cout << ret;
