59f80dcc0820cf766f6a2dc891213edd708b2c42
gyunjongpark <idjkda4034@gmail.com>
2024-08-25 14:47:49 +0900

    Update 031*.sql

--- Programmers/SQL_Kit/SELECT/031*.sql
+++ Programmers/SQL_Kit/SELECT/031*.sql
@@ -10,5 +10,5 @@ FROM (
 ) AS A
 ORDER BY A.ID
 
--- PERCENT_RANK() OVER 퍼센트 비율로 나눌 속성(입력 시 정렬식 자체를 넣는다) AS 대신 나타낼 레이블(속성) 명
--- 테이블에 속한 필드의 수에 맞춰 0 ~ 1 사이의 비율값을 할당
+-- PERCENT_RANK() OVER (ORDER BY 퍼센트 비율로 나눌 속성 ASC OR DESC) AS 대신 나타낼 레이블(속성) 명
+-- 필드 데이터 길이에 따른 0 ~ 1 사이의 비율값을 할당
