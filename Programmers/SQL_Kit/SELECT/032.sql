9438123966fe1704e06cb5338d1124d14c5ce6e6
gyunjongpark <idjkda4034@gmail.com>
2024-08-25 14:49:15 +0900

    Update 032.sql

--- Programmers/SQL_Kit/SELECT/032.sql
+++ Programmers/SQL_Kit/SELECT/032.sql
@@ -1,10 +1,10 @@
-SELECT ID
+SELECT ID -- 3세대
 FROM ECOLI_DATA 
 WHERE PARENT_ID IN (
-                    SELECT ID
+                    SELECT ID -- 2세대
                     FROM ECOLI_DATA 
                     WHERE PARENT_ID IN (
-                                        SELECT ID
+                                        SELECT ID -- 1세대
                                         FROM ECOLI_DATA 
                                         WHERE PARENT_ID IS NULL
                                        )
