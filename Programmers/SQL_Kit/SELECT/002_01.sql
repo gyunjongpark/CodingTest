5a1a178e9894b048ffebd5d2146e67c806a4b650
gyunjongpark <idjkda4034@gmail.com>
2024-08-25 13:52:30 +0900

    Update 002_01.sql

--- Programmers/SQL_Kit/SELECT/002_01.sql
+++ Programmers/SQL_Kit/SELECT/002_01.sql
@@ -3,6 +3,11 @@ FROM DOCTOR
 WHERE MCDP_CD = 'CS' OR MCDP_CD = 'GS'
 ORDER BY HIRE_YMD DESC, DR_NAME ASC;
 
--- 형식에 맞게 레이블 출력하기 : DATE_FORMAT(속성, '포멧 형식') AS 대신 나타낼 레이블 명
--- %Y-%M-%D, %Y-%m-%d ... 대소문자 구분하기
--- 행 데이터 = 튜플 = 레코드 = row / 속성 = 레이블 / 필드 = 속성(레이블) + 속성에 해당하는 값들 = 열 데이터 = column
+/*
+형식에 맞게 레이블 출력하기 : DATE_FORMAT(DATE 타입의 속성, '포맷 형식') AS 대신 나타낼 레이블 명
+행 데이터 = 투플 = 레코드 = row / 속성 = 레이블 / 필드 = 속성(레이블) + 속성에 해당하는 값들 = 열 데이터 = column
+
+%Y-%M-%D, %Y-%m-%d ... 대소문자 구분하기
+select date_format(now(),'%Y-%M-%D') AS date; -- 2024-August-25th
+select date_format(now(),'%y-%m-%d') AS date; -- 24-08-25
+*/
