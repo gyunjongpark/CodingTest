bc9daeb46a95b4d9cb757e9513906c8215df054e
gyunjongpark <idjkda4034@gmail.com>
2024-08-25 14:04:15 +0900

    Update 009.sql

--- Programmers/SQL_Kit/SELECT/009.sql
+++ Programmers/SQL_Kit/SELECT/009.sql
@@ -3,4 +3,4 @@ FROM PATIENT
 WHERE AGE <= 12 AND GEND_CD = 'W'
 ORDER BY AGE DESC, PT_NAME ASC;
 
--- IFNULL(해당 속성, ' NULL일 경우 대체할 값') as 대신 나타낼 속성
+-- IFNULL(속성, '해당 속성값이 NULL일 경우 대체할 값') as 대신 나타낼 속성
