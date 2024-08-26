a9818e8bf65c0c46d17f72ef6b225f7f7c251eef
gyunjongpark <idjkda4034@gmail.com>
2024-08-26 19:39:09 +0900

    Create 10026_01.cpp

---
+++ BOJ/BaaaaaaaaaaarkingDog/09/10026_01.cpp
@@ -0,0 +1,161 @@
+#include<iostream>
+#include<queue>
+#include<tuple> //tie
+using namespace std;
+
+const int dy[] = { -1,0,1,0 };
+const int dx[] = { 0,1,0,-1 };
+char a[101][101];
+bool visited[101][101];
+int n, ret, ret_dis;
+string s;
+
+void bfs_R(int y, int x) {
+    visited[y][x] = 1;
+
+    queue<pair<int, int>> q;
+    q.push({ y,x });
+
+    while (!q.empty()) {
+        int y, x;
+        tie(y, x) = q.front(); q.pop();
+
+        for (int i = 0; i < 4; i++) {
+            int ny = y + dy[i];
+            int nx = x + dx[i];
+
+            if (ny < 0 || ny >= n || nx < 0 || nx >= n) continue;
+
+            if (a[ny][nx] == 'R' && !visited[ny][nx]) {
+                visited[ny][nx] = 1;
+                q.push({ ny,nx });
+            }
+        }
+    }
+
+    return;
+}
+
+void bfs_G(int y, int x) {
+    visited[y][x] = 1;
+
+    queue<pair<int, int>> q;
+    q.push({ y,x });
+
+    while (!q.empty()) {
+        int y, x;
+        tie(y, x) = q.front(); q.pop();
+
+        for (int i = 0; i < 4; i++) {
+            int ny = y + dy[i];
+            int nx = x + dx[i];
+
+            if (ny < 0 || ny >= n || nx < 0 || nx >= n) continue;
+
+            if (a[ny][nx] == 'G' && !visited[ny][nx]) {
+                visited[ny][nx] = 1;
+                q.push({ ny,nx });
+            }
+        }
+    }
+
+    return;
+}
+
+void bfs_B(int y, int x) {
+    visited[y][x] = 1;
+
+    queue<pair<int, int>> q;
+    q.push({ y,x });
+
+    while (!q.empty()) {
+        int y, x;
+        tie(y, x) = q.front(); q.pop();
+
+        for (int i = 0; i < 4; i++) {
+            int ny = y + dy[i];
+            int nx = x + dx[i];
+
+            if (ny < 0 || ny >= n || nx < 0 || nx >= n) continue;
+
+            if (a[ny][nx] == 'B' && !visited[ny][nx]) {
+                visited[ny][nx] = 1;
+                q.push({ ny,nx });
+            }
+        }
+    }
+
+    return;
+}
+
+void bfs_R_G(int y, int x) {
+    visited[y][x] = 1;
+
+    queue<pair<int, int>> q;
+    q.push({ y,x });
+
+    while (!q.empty()) {
+        int y, x;
+        tie(y, x) = q.front(); q.pop();
+
+        for (int i = 0; i < 4; i++) {
+            int ny = y + dy[i];
+            int nx = x + dx[i];
+
+            if (ny < 0 || ny >= n || nx < 0 || nx >= n) continue;
+
+            if ((a[ny][nx] == 'R' || a[ny][nx] == 'G') && !visited[ny][nx]) {
+                visited[ny][nx] = 1;
+                q.push({ ny,nx });
+            }
+        }
+    }
+
+    return;
+}
+
+int main() {
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL); cout.tie(NULL);
+
+    cin >> n;
+
+    for (int i = 0; i < n; i++) {
+        cin >> s;
+
+        for (int j = 0; j < n; j++) {
+            a[i][j] = s[j];
+        }
+    }
+
+    for (int i = 0; i < n; i++) {
+        for (int j = 0; j < n; j++) {
+            if (a[i][j] == 'R' && !visited[i][j]) {
+                bfs_R(i, j); ret++;
+            }
+            else if (a[i][j] == 'G' && !visited[i][j]) {
+                bfs_G(i, j); ret++;
+            }
+            else if (a[i][j] == 'B' && !visited[i][j]) {
+                bfs_B(i, j); ret++;
+            }
+        }
+    }
+
+    fill(&visited[0][0], &visited[0][0] + 101 * 101, 0); //init
+
+    for (int i = 0; i < n; i++) {
+        for (int j = 0; j < n; j++) {
+            if ((a[i][j] == 'R' || a[i][j] == 'G') && !visited[i][j]) {
+                bfs_R_G(i, j); ret_dis++;
+            }
+            else if (a[i][j] == 'B' && !visited[i][j]) {
+                bfs_B(i, j); ret_dis++;
+            }
+        }
+    }
+
+    cout << ret << ' ' << ret_dis;
+
+    return 0;
+}
