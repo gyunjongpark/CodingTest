2b7c92ccba94abae206202b16d86a7d046cb1563
gyunjongpark <idjkda4034@gmail.com>
2024-08-27 08:46:43 +0900

    Update 002_01.sql

--- Programmers/SQL_Kit/SELECT/002_01.sql
+++ Programmers/SQL_Kit/SELECT/002_01.sql
@@ -4,7 +4,7 @@ WHERE MCDP_CD = 'CS' OR MCDP_CD = 'GS'
 ORDER BY HIRE_YMD DESC, DR_NAME ASC;
 
 /*
-형식에 맞게 레이블 출력하기 : DATE_FORMAT(DATE 타입의 속성, '포맷 형식') AS 대신 나타낼 레이블 명
+형식에 맞게 날짜 레이블 출력하기 : DATE_FORMAT(DATE 타입의 속성, '포맷 형식') AS 대신 나타낼 레이블 명
 행 데이터 = 투플 = 레코드 = row / 속성 = 레이블 / 필드 = 속성(레이블) + 속성에 해당하는 값들 = 열 데이터 = column
 
 %Y-%M-%D, %Y-%m-%d ... 대소문자 구분하기
