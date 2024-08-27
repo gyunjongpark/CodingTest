d58c697e694e15e903e208a45342cad36fb59ca3
gyunjongpark <idjkda4034@gmail.com>
2024-08-27 17:36:54 +0900

    Create GCD_LCM.cpp

---
+++ mathematics/N_i/GCD_LCM.cpp
@@ -0,0 +1,24 @@
+#include<iostream>
+using namespace std;
+
+int a, b;
+
+int getGCD(int a, int b) {
+	if (a == 0) return b;
+	return getGCD(b % a, a);
+}
+int getLCM(int a, int b) {
+	return (a * b) / getGCD(a, b);
+}
+
+int main() {
+	ios_base::sync_with_stdio(false);
+	cin.tie(NULL); cout.tie(NULL);
+
+	cin >> a >> b;
+
+	cout << getGCD(a, b);
+	cout << getLCM(a, b);
+
+	return 0;
+}
