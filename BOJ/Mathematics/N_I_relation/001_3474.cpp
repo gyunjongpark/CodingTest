43db0f0d27561a4823fca529e6862c6c576858b0
gyunjongpark <idjkda4034@gmail.com>
2024-08-27 21:31:29 +0900

    Update and rename mathematics/N_i/001_3474.cpp to BOJ/Mathematics/N_I_relation/001_3474.cpp

--- BOJ/Mathematics/N_I_relation/001_3474.cpp
+++ BOJ/Mathematics/N_I_relation/001_3474.cpp
@@ -10,10 +10,9 @@ int main() {
 	cin >> t;
 
 	while (t--) {
-		cin >> n;
-
-		int ret2 = 0;
-		int ret5 = 0;
+		cin >> n; //init
+		int ret2 = 0; //init
+		int ret5 = 0; //init
 
 		for (int i = 2; i <= n; i *= 2) {
 			ret2 += n / i; //n!에서 2가 몇 번 곱해져 있는가?
@@ -23,7 +22,7 @@ int main() {
 			ret5 += n / i; //n!에서 5가 몇 번 곱해져 있는가?
 		}
 
-    //최솟값을 구하면 n!에서 10이 몇 번 곱해져 있는지 = 0이 몇 개 존재하는지 구할 수 있다
+    		//최솟값을 구하면 n!에서 10이 몇 번 곱해져 있는지(0이 몇 개 존재하는지) 구할 수 있다
 		cout << min(ret2, ret5) << '\n';
 	}
 
