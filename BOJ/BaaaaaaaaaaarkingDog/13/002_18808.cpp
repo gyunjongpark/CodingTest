71480697ae59a3fa3c736978af9745b138549460
gyunjongpark <idjkda4034@gmail.com>
2024-08-28 10:23:57 +0900

    Update 002_18808.cpp

--- BOJ/BaaaaaaaaaaarkingDog/13/002_18808.cpp
+++ BOJ/BaaaaaaaaaaarkingDog/13/002_18808.cpp
@@ -8,7 +8,7 @@ int note[42][42], paper[12][12];
 void rotate() {
 	int temp[12][12];
 
-	for (int i = 0; i < c; i++) { //temp를 기준으로 r, c 반복문 진행
+	for (int i = 0; i < c; i++) { //temp의 행, 열을 기준으로 반복문 진행
 		for (int j = 0; j < r; j++) {
 			temp[i][j] = paper[r - j - 1][i];
 		}
@@ -16,17 +16,18 @@ void rotate() {
 
 	memcpy(paper, temp, sizeof(temp));
 
-	//열과 행이 바뀐 temp가 새로운 paper가 되므로 다음 rotate는 r과 c가 바뀐다
+	//열과 행이 바뀐 temp가 새로운 paper가 되므로 다음 papar는 r과 c가 바뀐다
 	swap(r, c);
 
 	return;
 }
 
-//R x C 사이즈의 스티커가 (y,x)를 좌상단 기준으로 온전히 붙여지는가?
+//R x C 사이즈의 스티커가 좌상단 (y,x) 기준으로 온전히 붙여지는가?
 bool postable(int y, int x) {
 	for (int i = 0; i < r; i++) {
 		for (int j = 0; j < c; j++) {
-			//이미 note가 1이라면(하나라도 겹친다면) 온전히 못 붙인다
+			
+			//paper가 1인데 이미 note가 1이라면(하나라도 겹친다면) 온전히 못 붙인다
 			if (note[y + i][x + j] == 1 && paper[i][j] == 1) {
 				return false;
 			}
@@ -53,13 +54,13 @@ int main() {
 		}
 
 		for (int dir = 0; dir < 4; dir++) {
-			bool is_pasted = false;
+			bool is_posted = false;
 
 			for (int y = 0; y <= n - r; y++) {
-				if (is_pasted) break;
+				if (is_posted) break;
 
 				for (int x = 0; x <= m - c; x++) {
-					if (postable(y, x)) { //붙일 수 있는지 검사
+					if (postable(y, x)) { //온전히 붙일 수 있는지 검사
 
 						//검사 통과! R x C 사이즈의 스티커 붙이기
 						for (int i = 0; i < r; i++) {
@@ -70,13 +71,14 @@ int main() {
 							}
 						}
 
-						is_pasted = true;
+						is_posted = true;
 
 						break;
 					}
 				}
 			}
-			if (is_pasted) break;
+
+			if (is_posted) break;
 
 			rotate();
 		}
@@ -87,7 +89,7 @@ int main() {
 			ret += note[i][j];
 		}
 	}
-
+	
 	cout << ret;
 
 	return 0;
