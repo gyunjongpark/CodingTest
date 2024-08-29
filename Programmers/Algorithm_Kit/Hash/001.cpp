cf206c2b43109527984bd2f993fc3691a6a396b3
gyunjongpark <idjkda4034@gmail.com>
2024-08-29 23:39:23 +0900

    Update 001.cpp

--- Programmers/Algorithm_Kit/Hash/001.cpp
+++ Programmers/Algorithm_Kit/Hash/001.cpp
@@ -14,7 +14,7 @@ int solution(vector<int> nums)
 
     for (int num : nums) st.insert(num);
 
-    answer = min(answer, (int)st.size()); //size 연산은 unsigned를 반환
+    answer = min(answer, (int)st.size()); //size 연산은 unsigned 반환
 
     return answer;
 }
