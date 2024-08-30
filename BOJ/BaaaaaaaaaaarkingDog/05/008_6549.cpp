3de259bf84c5fd8ad1fc93c25d6c75cfba84c6a7
gyunjongpark <idjkda4034@gmail.com>
2024-08-30 15:36:18 +0900

    Update 008_6549.cpp

--- BOJ/BaaaaaaaaaaarkingDog/05/008_6549.cpp
+++ BOJ/BaaaaaaaaaaarkingDog/05/008_6549.cpp
@@ -19,22 +19,21 @@ int main() {
 
 		for (int i = 0; i < n; i++) {
 			cin >> h;
-
 			idx = i;
 
 			while (!stk.empty() && stk.top().first >= h) { //입력된 높이가 현재 저장된 높이보다 작다면 중간 검사
-				//현재 저장된 위치부터 역으로 만들 수 있는 직사각형 전부 조사 후 ret 갱신
+				//현재 저장된 위치부터 역으로 만들 수 있는 직사각형 전부 조사해서 최댓값 갱신
 				ret = max(ret, (i - stk.top().second) * stk.top().first);
 
-				idx = stk.top().second; //현재까지 나온 높이 중 최저점을 높이로 삼는 가로로 긴 직사각형을 계산하기 위해
+				idx = stk.top().second; //stk에 저장된 높이들 중에서 h보다 작은 높이 && 최소 인덱스가 되도록 갱신
 
 				stk.pop();
 			}
 
-			stk.push({ h,idx });
+			stk.push({ h,idx }); //현재 h를 최소 높이로 하면서 가로로 가장 긴 직사각형을 계산하기 위해 (h, idx) push
 		}
 
-		while (!stk.empty()) { //마지막 기둥에 대해서 검사
+		while (!stk.empty()) { //마지막 기둥에 대하여 검사
 			ret = max(ret, (n - stk.top().second) * stk.top().first);
 
 			stk.pop();
