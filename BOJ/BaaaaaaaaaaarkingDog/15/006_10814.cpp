8a84db9079fef4d70c8321a652ae3186fdae91e6
gyunjongpark <idjkda4034@gmail.com>
2024-08-30 11:16:55 +0900

    Create 006_10814.cpp

---
+++ BOJ/BaaaaaaaaaaarkingDog/15/006_10814.cpp
@@ -0,0 +1,33 @@
+#include<iostream>
+#include<algorithm> //sort
+#include<vector>
+using namespace std;
+
+int n, a;
+string b;
+vector<pair<int, string>> v;
+
+bool cmp(pair<int, string> a, pair<int, string> b) {
+	return a.first < b.first;
+}
+
+int main() {
+	ios_base::sync_with_stdio(false);
+	cin.tie(NULL); cout.tie(NULL);
+
+	cin >> n;
+
+	while (n--) {
+		cin >> a >> b;
+		v.push_back({ a,b });
+	}
+
+	stable_sort(v.begin(), v.end(), cmp); //stable_sort
+
+	for (pair<int, string> a : v) {
+		cout << a.first << ' ' << a.second << '\n';
+	}
+
+
+	return 0;
+}
