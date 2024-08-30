eb5e6fbec2b972d6555436c2015648f0a7002a10
gyunjongpark <idjkda4034@gmail.com>
2024-08-30 15:07:28 +0900

    Update 005_6198.cpp

--- BOJ/BaaaaaaaaaaarkingDog/05/005_6198.cpp
+++ BOJ/BaaaaaaaaaaarkingDog/05/005_6198.cpp
@@ -5,7 +5,7 @@ typedef long long ll;
 
 stack<int> stk;
 int n, height;
-ll ret; //최악의 경우 n = 8만, (n * n - 1) / 2 -> 64억
+ll ret; //최악의 경우 n = 8만, (n * n - 1) / 2 -> O(N^2) -> 64억!?
 
 int main() {
 	ios_base::sync_with_stdio(false);
@@ -21,10 +21,8 @@ int main() {
 			stk.pop();
 		}
 
-		//오른쪽 방향 기준
-		//본인이 내려다볼 수 있는 빌딩 수의 총합 = 본인을 내려다보는 빌딩 수의 총합
-		//발상의 전환이 살짝 필요하다
-		ret += stk.size(); //본인을 내려다보는 빌딩의 수 합산
+		//본인이 내려다볼 수 있는 빌딩 수의 총합 = 본인을 내려다볼 수 빌딩 수의 총합
+		ret += stk.size(); //본인을 내려다볼 수 있는 빌딩의 수 계산하기
 		
 		stk.push(height);
 	}
