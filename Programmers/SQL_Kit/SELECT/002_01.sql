6e2c5c9b6b7bea23b0078df503fc99741f4a39ec
gyunjongpark <idjkda4034@gmail.com>
2024-09-05 10:31:15 +0900

    Update 002_01.sql

--- Programmers/SQL_Kit/SELECT/002_01.sql
+++ Programmers/SQL_Kit/SELECT/002_01.sql
@@ -5,7 +5,7 @@ ORDER BY HIRE_YMD DESC, DR_NAME ASC;
 
 /*
 형식에 맞게 날짜 레이블 출력하기 : DATE_FORMAT(DATE 타입의 속성, '포맷 형식') AS 대신 나타낼 레이블 명
-행 데이터 = 투플 = 레코드 = row / 속성 = 레이블 / 필드 = 속성(레이블) + 속성에 해당하는 값들 = 열 데이터 = column
+행 데이터 = 투플 = 레코드 = row / 속성 = 레이블 / 속성(레이블) + 속성에 해당하는 값들 = 열 데이터 = column
 
 %Y-%M-%D, %Y-%m-%d ... 대소문자 구분하기
 select date_format(now(),'%Y-%M-%D') AS date; -- 2024-August-25th
