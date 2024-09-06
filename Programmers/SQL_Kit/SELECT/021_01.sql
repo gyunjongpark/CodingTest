7016dfe3a334832ff4ce899138bcce716b392052
gyunjongpark <idjkda4034@gmail.com>
2024-09-06 13:32:58 +0900

    Update 021_01.sql

--- Programmers/SQL_Kit/SELECT/021_01.sql
+++ Programmers/SQL_Kit/SELECT/021_01.sql
@@ -14,6 +14,6 @@ ORDER BY ITEM_INFO.ITEM_ID DESC;
 /*
 부속질의부터 천천히 접근하는 문제
 1. rare 등급 아이템의 ITEM_ID를 ITEM_INFO 테이블에서 찾는다
-2. INFO에서 찾은 ITEM_ID가 TREE 테이블의 PARENT_ITEM_ID인 ITEM_ID를 TREE 테이블에서 찾는다
-3. TREE에서 찾은 ITEM_ID를 다시 ITEM 테이블에서 조회한 후 최종 결과를 얻는다
+2. 찾은 ITEM_ID가 TREE 테이블의 PARENT_ITEM_ID인 경우에 해당하는 ITEM_ID를 TREE 테이블에서 찾는다
+3. 찾은 ITEM_ID를 다시 ITEM 테이블에서 조회한 후 최종 결과를 얻는다
 */
