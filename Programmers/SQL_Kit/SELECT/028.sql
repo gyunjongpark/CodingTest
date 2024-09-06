e803a259629565129086a7487b202a41c469a569
gyunjongpark <idjkda4034@gmail.com>
2024-09-06 13:37:57 +0900

    Update 028.sql

--- Programmers/SQL_Kit/SELECT/028.sql
+++ Programmers/SQL_Kit/SELECT/028.sql
@@ -12,8 +12,8 @@ CASE
     WHEN 값을 비교하는 구문 THEN '대신 나타낼 값'
     .
     .
-    ELSE 대신 나타낼 값
-END AS 대신 나타낼 속성(레이블) 명
+    ELSE '대신 나타낼 값'
+END AS 대신 나타낼 속성명(이름)
 
 C++에서의 switch - case문과 동일한 기능을 가진다
 CASE - END 문이 하나의 속성이 된다
