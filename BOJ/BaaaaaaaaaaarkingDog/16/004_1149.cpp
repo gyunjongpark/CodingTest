449a1b4089c5c6e8bbc2d8d69f227453d3416f8a
gyunjongpark <idjkda4034@gmail.com>
2024-09-12 23:37:02 +0900

    Create 004_1149.cpp

---
+++ BOJ/BaaaaaaaaaaarkingDog/16/004_1149.cpp
@@ -0,0 +1,37 @@
+#include<iostream>
+#include<vector>
+using namespace std;
+
+vector<vector<int>> house, dp;
+int n, r, g, b;
+int answer = 1e9;
+
+int main() {
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL); cout.tie(NULL);
+
+    cin >> n;
+
+    dp.resize(n, vector<int>(3, 0));
+
+    for (int i = 0; i < n; i++) {
+        cin >> r >> g >> b;
+        house.push_back({ r,g,b });
+    }
+
+    // dp 첫 번째 줄 초기화
+    for (int i = 0; i < 3; i++) {
+        dp[0][i] = house[0][i];
+    }
+
+    for (int i = 1; i < n; i++) {
+        dp[i][0] = min(dp[i - 1][1], dp[i - 1][2]) + house[i][0];
+        dp[i][1] = min(dp[i - 1][0], dp[i - 1][2]) + house[i][1];
+        dp[i][2] = min(dp[i - 1][0], dp[i - 1][1]) + house[i][2];
+    }
+    answer = min(min(dp[n - 1][0], dp[n - 1][1]), dp[n - 1][2]);
+
+    cout << answer;
+
+    return 0;
+}
