dde62f9d173c042438dea3b0e7cde0e64972ac03
gyunjongpark <idjkda4034@gmail.com>
2024-09-18 18:43:00 +0900

    Update 003_2579.cpp

--- BOJ/BaaaaaaaaaaarkingDog/16/003_2579.cpp
+++ BOJ/BaaaaaaaaaaarkingDog/16/003_2579.cpp
@@ -9,19 +9,20 @@ int main() {
 
     cin >> n;
 
-    for (int i = 0; i < n; i++) {
+    for (int i = 1; i <= n; i++) {
         cin >> a[i];
     }
 
-    dp[0] = a[0];
-    dp[1] = a[0] + a[1];
-    dp[2] = max(a[0], a[1]) + a[2];
+    //init
+    dp[1] = a[1];
+    dp[2] = a[1] + a[2];
+    dp[3] = max(a[1], a[2]) + a[3];
 
-    for (int i = 3; i < n; i++) {
+    for (int i = 4; i <= n; i++) {
         dp[i] = max(dp[i - 2], dp[i - 3] + a[i - 1]) + a[i];
     }
 
-    cout << dp[n - 1];
+    cout << dp[n];
 
     return 0;
 }
