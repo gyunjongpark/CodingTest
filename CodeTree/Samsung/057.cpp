8db236bdd767898aa5711a2b63de247f8e9a5769
gyunjongpark <idjkda4034@gmail.com>
2024-09-18 22:28:50 +0900

    Update 057.cpp

--- CodeTree/Samsung/057.cpp
+++ CodeTree/Samsung/057.cpp
@@ -15,26 +15,26 @@ void drive(int y, int x, int dir) {
     }
     
     for (int i = 0; i < 4; i++) { //상하좌우 반복
-        dir = (dir + 3) % 4;
+        dir = (dir + 3) % 4; //왼쪽으로 방향 전환
 
         int ny = y + dy[dir];
         int nx = x + dx[dir];
 
         if (ny >= 0 && ny < n && nx >= 0 && nx < m && a[ny][nx] == 0 && !visited[ny][nx]) {
             drive(ny, nx, dir); //1번 과정 시도
-            return; //조건을 만족하여 좌표가 옮겨졌다면 현재 y, x에서의 drive의 역할은 종료된다(후진 과정을 거치지 않는다).
+            return; //조건을 만족하여 좌표가 옮겨졌다면 현재 y, x에서의 drive의 역할은 종료(후진 과정을 거치지 않는다)
         }
     }
 
-    //y, x에서 네 방향 모두 조건을 만족하지 않을 시(if 문의 조건 불만족) 후진 방향 설정
-    int back_dir = (dir + 2) % 4;
+    //y, x에서 네 방향 모두 조건을 만족하지 않을 시(더 이상의 전진이 불가능하다면) 후진 방향 설정
+    int back_dir = (dir + 2) % 4; //후진 방향으로 전환
 
     //후진
     int ny = y + dy[back_dir];
     int nx = x + dx[back_dir];
 
-    if (ny >= 0 && ny < n && nx >= 0 && nx < m && a[ny][nx] == 0) {
-        drive(ny, nx, dir); //방향은 유지한 채로 후진
+    if (ny >= 0 && ny < n && nx >= 0 && nx < m && a[ny][nx] == 0) { //후진은 visited와는 관계없다
+        drive(ny, nx, dir); //후진 이후 해당 좌표에서 다시 drive 실행
     }
     else return; //더 이상 후진할 수 없다면 종료
 }
