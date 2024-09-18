f708be4628e70b466538ad17e2e73ac017a7c9b6
gyunjongpark <idjkda4034@gmail.com>
2024-09-19 08:34:23 +0900

    Update 01.sql

--- Programmers/SQL_Kit/01_SELECT/01.sql
+++ Programmers/SQL_Kit/01_SELECT/01.sql
@@ -1,6 +1,6 @@
 SELECT ROUND(AVG(DAILY_FEE),0) AS AVERAGE_FEE
 FROM CAR_RENTAL_COMPANY_CAR
-WHERE CAR_TYPE='SUV';
+WHERE CAR_TYPE = 'SUV';
 
 -- ROUND(속성의 집계함수, 자릿수 n)  소수 n 번째 자릿수까지 소수점 표현. 반올림 적용
 -- ex) SELECT ROUND(42.195, 2) -> 42.20
