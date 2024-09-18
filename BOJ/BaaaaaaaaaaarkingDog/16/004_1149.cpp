3e0d656398e1d8e7be80b851dbe920faa15a7d48
gyunjongpark <idjkda4034@gmail.com>
2024-09-18 18:47:52 +0900

    Update 004_1149.cpp

--- BOJ/BaaaaaaaaaaarkingDog/16/004_1149.cpp
+++ BOJ/BaaaaaaaaaaarkingDog/16/004_1149.cpp
@@ -1,10 +1,11 @@
 #include<iostream>
 #include<vector>
+#include<climits> //INT_MAX
 using namespace std;
 
 vector<vector<int>> house, dp;
 int n, r, g, b;
-int answer = 1e9;
+int answer = INT_MAX;
 
 int main() {
     ios_base::sync_with_stdio(false);
@@ -19,7 +20,7 @@ int main() {
         house.push_back({ r,g,b });
     }
 
-    // dp 첫 번째 줄 초기화
+    //dp 첫 번째 줄 init
     for (int i = 0; i < 3; i++) {
         dp[0][i] = house[0][i];
     }
@@ -29,6 +30,7 @@ int main() {
         dp[i][1] = min(dp[i - 1][0], dp[i - 1][2]) + house[i][1];
         dp[i][2] = min(dp[i - 1][0], dp[i - 1][1]) + house[i][2];
     }
+
     answer = min(min(dp[n - 1][0], dp[n - 1][1]), dp[n - 1][2]);
 
     cout << answer;
