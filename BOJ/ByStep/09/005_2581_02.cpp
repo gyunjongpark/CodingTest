e61a75fe93fe3540bc8508228e48c1e2a4fdbdda
gyunjongpark <idjkda4034@gmail.com>
2024-09-29 22:07:15 +0900

    Update 005_2581_02.cpp

--- BOJ/ByStep/09/005_2581_02.cpp
+++ BOJ/ByStep/09/005_2581_02.cpp
@@ -2,23 +2,31 @@
 #include<vector>
 using namespace std;
 
-int n, m, sum, chae[10004]; //범위는 넉넉히 잡는다
+int n, m, sum;
 int minVal = 10001;
+bool chae[10001]; //범위는 넉넉히 잡는다
 
-vector<int> era(int n) { //에라토스테네스의 체
+vector<int> era(int num) { //에라토스테네스의 체
 	vector<int> v;
 
+	chae[0] = false;
+	chae[1] = false;
+
+	for (int i = 2; i <= num; i++) {
+		chae[i] = true;
+	}
+
 	//O(Nlog(logN))
-	for (int i = 2; i <= n; i++) {
-		if (chae[i]) continue; //이미 지워졌다면 continue
-		
-		for (int j = i + i; j <= n; j += i) {
-			chae[j] = 1; //i를 제외한 i의 배수 지우기
+	for (int i = 2; i <= num; i++) {
+		if (!chae[i]) continue; //이미 지워졌다면 continue
+
+		for (int j = i + i; j <= num; j += i) {
+			chae[j] = false; //i를 제외한 i의 배수 지우기
 		}
 	}
 
-	for (int i = 2; i <= n; i++) {
-		if (!chae[i]) v.push_back(i); //소수만 골라서 push back
+	for (int i = 0; i <= num; i++) {
+		if (chae[i]) v.push_back(i); //소수만 골라서 push back
 	}
 
 	return v;
@@ -38,7 +46,8 @@ int main() {
 			minVal = min(minVal, i);
 		}
 	}
-	if (sum) cout << sum<<'\n'<<minVal;
+
+	if (sum) cout << sum << '\n' << minVal;
 	else cout << -1;
 
 	return 0;
