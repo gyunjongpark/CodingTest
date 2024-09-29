c6f656b4ed8cdb315c8ad3aa72a6086cc896ef6b
gyunjongpark <idjkda4034@gmail.com>
2024-09-29 21:29:08 +0900

    Update 003_10818.cpp

--- BOJ/ByStep/04/003_10818.cpp
+++ BOJ/ByStep/04/003_10818.cpp
@@ -1,7 +1,9 @@
 #include<iostream>
+#include<climits> //INT_MAX, INT_MIN
 using namespace std;
 
-int n, temp, minVal = 1000004, maxVal = -1000004;
+int n, temp;
+int minVal = INT_MAX, maxVal = INT_MIN;
 
 int main() {
 	ios_base::sync_with_stdio(false);
@@ -11,9 +13,11 @@ int main() {
 
 	for (int i = 0; i < n; i++) {
 		cin >> temp;
+		
 		if (temp > maxVal) maxVal = temp;
 		if (temp < minVal) minVal = temp;
 	}
+	
 	cout << minVal << ' ' << maxVal;
 
 	return 0;
