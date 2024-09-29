e31d12dbff61e7389b44918a4c2d9ad588b130a1
gyunjongpark <idjkda4034@gmail.com>
2024-09-30 08:59:51 +0900

    Update 001_28278.cpp

--- BOJ/ByStep/16/001_28278.cpp
+++ BOJ/ByStep/16/001_28278.cpp
@@ -2,8 +2,8 @@
 #include<stack>
 using namespace std;
 
-stack<int> stk;
 int n, c, x;
+stack<int> stk;
 
 int main() {
 	ios_base::sync_with_stdio(false);
@@ -19,7 +19,7 @@ int main() {
 			stk.push(x);
 		}
 		else if (c == 2) {
-			if (stk.size()) {
+			if (!stk.empty()) {
 				cout << stk.top() << '\n';
 				stk.pop();
 			}
@@ -29,11 +29,11 @@ int main() {
 			cout << stk.size() << '\n';
 		}
 		else if (c == 4) {
-			if (stk.size()) cout << 0 << '\n';
+			if (!stk.empty()) cout << 0 << '\n';
 			else cout << 1 << '\n';
 		}
 		else if (c == 5) {
-			if (stk.size()) cout << stk.top() << '\n';
+			if (!stk.empty()) cout << stk.top() << '\n';
 			else cout << -1 << '\n';
 		}
 	}
