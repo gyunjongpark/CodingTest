e42cbdf428f3e761cdfac36232387d269d654080
gyunjongpark <idjkda4034@gmail.com>
2024-09-29 21:58:29 +0900

    Update 004_1978_01.cpp

--- BOJ/ByStep/09/004_1978_01.cpp
+++ BOJ/ByStep/09/004_1978_01.cpp
@@ -6,6 +6,7 @@ int a, n, ret;
 
 bool check(int n) {
 	if (n <= 1) return 0;
+	
 	for (int i = 2; i <= (int)sqrt(n); i++) {
 		if (n % i == 0) return 0;
 	}
@@ -21,6 +22,7 @@ int main() {
 
 	for (int i = 0; i < n; i++) {
 		cin >> a;
+		
 		if (check(a)) ret++;
 	}
   
