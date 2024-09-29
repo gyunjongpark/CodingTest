5bdff8f6b41abb50a6334dca75f366f630a0d471
gyunjongpark <idjkda4034@gmail.com>
2024-09-29 21:31:57 +0900

    Update 004_2562.cpp

--- BOJ/ByStep/04/004_2562.cpp
+++ BOJ/ByStep/04/004_2562.cpp
@@ -1,7 +1,7 @@
 #include<iostream>
 using namespace std;
 
-int a[9], temp, maxVal;
+int a[9], temp, maxVal; //배열 a의 원소는 자연수이므로 maxVal = 0으로 초기화해도 무방하다
 
 int main() {
 	ios_base::sync_with_stdio(false);
@@ -9,11 +9,13 @@ int main() {
 
 	for (int i = 0; i < 9; i++) {
 		cin >> a[i];
+		
 		if (a[i] > maxVal) {
 			maxVal = a[i];
 			temp = i;
 		}
 	}
+	
 	cout << maxVal << '\n' << temp + 1;
 
 	return 0;
