a3637f1851b61f9ccfbb66095ccf4f54d5f0e179
gyunjongpark <idjkda4034@gmail.com>
2024-09-29 21:54:21 +0900

    Update 006_2941.cpp

--- BOJ/ByStep/06/006_2941.cpp
+++ BOJ/ByStep/06/006_2941.cpp
@@ -15,11 +15,14 @@ int main() {
 	for (int i = 0; i < str.size(); i++) {
 		while (true) {
 			idx = s.find(str[i]);
+			
 			if (idx == string::npos) break;
 
+			//replace(시작 위치, 바꿀 문자열의 범위, 대신 들어갈 문자열)
 			s.replace(idx, str[i].size(), "#");
 		}
 	}
+	
 	cout << s.size();
 
 	return 0;
