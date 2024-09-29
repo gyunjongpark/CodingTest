41e24c1f03983b4e0e342d229b9229d9f1ddae36
gyunjongpark <idjkda4034@gmail.com>
2024-09-29 22:11:14 +0900

    Update 004_2751.cpp

--- BOJ/ByStep/13/004_2751.cpp
+++ BOJ/ByStep/13/004_2751.cpp
@@ -1,16 +1,17 @@
 #include<iostream>
-#include<algorithm>
+#include<algorithm> //sort
 #include<vector>
 using namespace std;
 
 vector<int> v;
-int n,temp;
+int n, temp;
 
 int main() {
 	cin >> n;
 
 	for (int i = 0; i < n; i++) {
 		cin >> temp;
+		
 		v.push_back(temp);
 	}
 	sort(v.begin(), v.end());
