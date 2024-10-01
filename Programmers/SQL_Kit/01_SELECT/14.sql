70b5050c949e3dca6b43102ee2655cbdcc1f8cb9
gyunjongpark <idjkda4034@gmail.com>
2024-10-01 10:29:06 +0900

    Update 14.sql

--- Programmers/SQL_Kit/01_SELECT/14.sql
+++ Programmers/SQL_Kit/01_SELECT/14.sql
@@ -15,5 +15,5 @@ ORDER BY SALES_DATE, PRODUCT_ID, USER_ID
 
 -- NULL 속성 : 해당 속성의 모든 값을 NULL로 표시
 
--- NULL, IS NULL, IS NOT NULL, IFNULL 구분하기!
--- NULL 속성 / IFNULL(속성,'대체할 값') / IS NULL, IS NOT NULL : WHERE 절(조건)에서 사용
+-- NULL, IS NULL, IS NOT NULL, IFNULL 구분하기
+-- NULL 속성 / IFNULL(속성,'속성의 값이 NULL일때 대체할 값') / IS NULL, IS NOT NULL : NULL인 것, NULL이 아닌 것 구분. WHERE 절(조건)에서 사용
