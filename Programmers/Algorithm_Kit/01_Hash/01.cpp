c318c08c57236f9aa4cb14aaf1330d87e32cf5fb
gyunjongpark <idjkda4034@gmail.com>
2024-10-01 15:43:01 +0900

    Update 01.cpp

--- Programmers/Algorithm_Kit/01_Hash/01.cpp
+++ Programmers/Algorithm_Kit/01_Hash/01.cpp
@@ -15,7 +15,7 @@ int solution(vector<int> nums)
 
     for (int num : nums) st.insert(num);
 
-    //size() 연산은 size_t 타입을 반환하므로 엄밀한 검사가 필요
+    //size() 연산은 size_t 타입을 반환하므로 비교 시 주의할 것!
     answer = min(answer, (st.size() <= INT_MAX) ? (int)st.size() : INT_MAX);
 
     return answer;
