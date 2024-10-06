c3f5710f3969b244f99cb0a8fd56e5c5be3a0112
gyunjongpark <idjkda4034@gmail.com>
2024-10-06 21:37:16 +0900

    Update 006_14500_02.cpp

--- BOJ/Samsung/006_14500_02.cpp
+++ BOJ/Samsung/006_14500_02.cpp
@@ -1,9 +1,8 @@
 #include<iostream>
-#include<vector>
 using namespace std;
 
 //5373 큐빙과 비슷한 접근법. 경우의 수로 나올 좌표를 미리 생성
-vector<vector<pair<int, int>>> tet = {
+int tet[19][3][2] = {
     {{0,1},{0,2},{0,3}}, {{1,0},{2,0},{3,0}}, //ㅣ 회전
     {{0,1},{1,0},{1,1}}, //ㅁ
     {{1,0},{2,0},{2,1}}, {{1,0},{0,1},{0,2}},{{0,1},{1,1},{2,1}},{{0,1},{0,2},{-1,2}}, //ㄴ 회전
@@ -19,8 +18,8 @@ int get_score(int i, int j, int k) {
     int sum = a[i][j]; //기준점 먼저 더하기
 
     for (int t = 0; t < 3; t++) {
-        int y = i + tet[k][t].first;
-        int x = j + tet[k][t].second;
+        int y = i + tet[k][t][0];
+        int x = j + tet[k][t][1];
 
         if (y < 0 || y >= n || x < 0 || x >= m) continue;
 
@@ -44,7 +43,7 @@ int main() {
 
     for (int i = 0; i < n; i++) {
         for (int j = 0; j < m; j++) {
-            for (int k = 0; k < tet.size(); k++) {
+            for (int k = 0; k < 19; k++) {
                 sum = get_score(i, j, k);
                 ret = max(ret, sum);
             }
