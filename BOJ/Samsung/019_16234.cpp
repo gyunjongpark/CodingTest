bc7ddf7b52b66951e38323239fd7f33001e6d99f
gyunjongpark <idjkda4034@gmail.com>
2024-10-07 09:06:45 +0900

    Update 019_16234.cpp

--- BOJ/Samsung/019_16234.cpp
+++ BOJ/Samsung/019_16234.cpp
@@ -6,11 +6,12 @@ const int dy[] = { -1,0,1,0 };
 const int dx[] = { 0,1,0,-1 };
 int n, ret, l, r, sum, a[51][51];
 bool visited[51][51];
+vector<pair<int, int>> v;
 
-void dfs(int y, int x, int& sum, vector<pair<int,int>>& v) {
+void dfs(int y, int x) {
 	visited[y][x] = true;
-	v.push_back({ y,x });
 
+	v.push_back({ y,x });
 	sum += a[y][x];
 
 	for (int i = 0; i < 4; i++) {
@@ -22,7 +23,7 @@ void dfs(int y, int x, int& sum, vector<pair<int,int>>& v) {
 		int diff = abs(a[y][x] - a[ny][nx]);
 
 		if (!visited[ny][nx] && l <= diff && diff <= r) {
-			dfs(ny, nx, sum, v);
+			dfs(ny, nx);
 		}
 	}
 
@@ -42,21 +43,17 @@ int main() {
 	}
 
 	while (true) {
-		fill(&visited[0][0], &visited[0][0] + 51 * 51, 0); //인구이동이 끝나면 reset
+		fill(&visited[0][0], &visited[0][0] + 51 * 51, 0);
 
 		bool flag = false; //인구이동 발생 시 true
-		
+
 		for (int i = 0; i < n; i++) {
 			for (int j = 0; j < n; j++) {
-				vector<pair<int, int>> v;
-				int sum = 0; //연합에 포함되는 인구 수의 합
+				v.clear();
+				sum = 0;
 
 				if (!visited[i][j]) {
-
-					//dfs를 진행하면서 공유 가능한 좌표를 v에 넣는다(참조자 필수)
-					//dfs를 진행하면서 해당 좌표의 값을 sum에 추가한다(참조자 필수)
-					dfs(i, j, sum, v);
-
+					dfs(i, j);
 
 					if (v.size() == 1) continue; //본인만 포함되었다면(국경선이 공유되지 않음)
 					else {
