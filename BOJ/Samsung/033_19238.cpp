bcf04debed6751cb93ace4841e19877cb9155c95
gyunjongpark <idjkda4034@gmail.com>
2024-10-11 11:40:15 +0900

    Update 033_19238.cpp

--- BOJ/Samsung/033_19238.cpp
+++ BOJ/Samsung/033_19238.cpp
@@ -27,25 +27,25 @@ int find_shortest_customer() {
 	queue<Taxi> q;
 	q.push({ taxi_y * 100 + taxi_x, 0 });
 
-	int candi[400] = { 0, };
-	int candi_idx = 0; //택시로부터 가까운 거리에 있는 손님 발견 시, 해당 인덱스를 손님 번호에 부여
+	int candi[400] = { 0, }; //택시의 현 좌표에서 가장 가까운 거리에 있는 승객의 번호만을 저장
+	int candi_idx = 0;
 	int candi_distance = -1;
 
 	while (!q.empty()) { //bfs를 통해 최단거리의 승객을 찾아낸다
 		Taxi cur = q.front(); q.pop();
 
-		//갱신되었는데 기존의 거리보다 멀다면 break
-		if (candi_distance != -1 && cur.distance > candi_distance) break;
+		//갱신되었는데 기존의 거리보다 멀다면 break(feat. 16236 아기상어)
+		if (candi_distance != -1 && candi_distance < cur.distance) break;
 
-		//택시 좌표부터 탐색(택시인 동시에 승객이 있을 가능성)
+		//택시 좌표부터 탐색(택시인 동시에 승객이 있을 가능성. distance는 0이 되겠다)
 		int y = cur.pos / 100;
 		int x = cur.pos % 100;
 
 		if (board[y][x] >= 0) { //승객이라면
-			candi[candi_idx] = board[y][x]; //승객의 번호를 candi 목록에 저장
+			candi[candi_idx] = board[y][x]; //해당 승객의 번호를 candi 목록에 저장
 			candi_idx++; //인덱스 증가(승객의 번호와 인덱스는 다른 개념이다)
 
-			candi_distance = cur.distance; //최소 거리 갱신(bfs의 특성)
+			candi_distance = cur.distance;
 		}
 
 		for (int d = 0; d < 4; ++d) {
@@ -60,17 +60,17 @@ int find_shortest_customer() {
 		}
 	}
 
-	//손님을 태울 연료가 부족하다면
+	//가장 가까운 승객을 태울 연료가 부족하다면
 	if (candi_distance > fuel) return -1;
 
-	//우선순위를 적용해 손님을 선택한다
+	//우선순위(행번호 * 100 + 열번호)를 적용해 승객 선택
 	int ret = -1; //승객 인덱스
-	int candi_val = 10000; //행번호 * 100 + 열번호 연산과 비교. 배열은 최대 2020
+	int candi_val = 10000; //pos(행번호 * 100 + 열번호)와 비교
 
 	for (int i = 0; i < candi_idx; i++) {
 		if (candi_val > customer[candi[i]].start) {
-			candi_val = customer[candi[i]].start; //행번호, 열번호가 작은 손님 선택
-			ret = candi[i]; //가장 우선순위의 승객 번호 저장
+			candi_val = customer[candi[i]].start; //행번호, 열번호가 작은 승객 선택
+			ret = candi[i]; //가장 작은 승객의 번호 저장
 		}
 	}
 
@@ -78,14 +78,16 @@ int find_shortest_customer() {
 	taxi_y = customer[ret].start / 100;
 	taxi_x = customer[ret].start % 100;
 
+	//위치로 이동 후 승객을 태운다
 	board[taxi_y][taxi_x] = EMPTY;
 
+	//이동하는 과정에서 연료 소모
 	fuel -= candi_distance;
 
 	return ret; //승객의 번호 반환
 }
 
-bool move_customer(int target) {
+bool move_customer(int selected_customer_idx) {
 	bool visited[20][20] = { false, };
 	visited[taxi_y][taxi_x] = true;
 
@@ -99,12 +101,13 @@ bool move_customer(int target) {
 		if (cur.distance > fuel) return false;
 
 		//목적지까지 도달했다면
-		if (cur.pos == customer[target].end) {
-			taxi_y = customer[target].end / 100;
-			taxi_x = customer[target].end % 100;
+		if (cur.pos == customer[selected_customer_idx].end) {
+			taxi_y = customer[selected_customer_idx].end / 100;
+			taxi_x = customer[selected_customer_idx].end % 100;
 
 			fuel += cur.distance;
 
+			//승객을 목적지로 이동시킨 동시에 연료가 바닥나는 경우는 실패한 것으로 간주하지 않는다
 			return true;
 		}
 
@@ -127,7 +130,7 @@ bool move_customer(int target) {
 }
 
 void go() {
-	for (int i = 0; i < m; ++i) {
+	for (int i = 0; i < m; ++i) { //승객 전체 검사
 		int target = find_shortest_customer(); //가장 가까운 승객의 번호를 target으로 저장
 
 		if (target == -1) {
@@ -155,14 +158,15 @@ int main() {
 	for (int i = 0; i < n; i++) {
 		for (int j = 0; j < n; j++) {
 			cin >> board[i][j];
-			board[i][j] -= 2; //WALL = -1, EMPTY = -2
+
+			board[i][j] -= 2; //WALL = -1, EMPTY = -2, 승객 번호 >= 0
 		}
 	}
 
 	cin >> taxi_y >> taxi_x;
 	taxi_y--; taxi_x--; //인덱싱
 
-	for (int i = 0; i < m; i++) {
+	for (int i = 0; i < m; i++) { //0번째 승객부터 입력받는다
 		int sy, sx, ey, ex;
 
 		cin >> sy >> sx >> ey >> ex;
@@ -173,7 +177,7 @@ int main() {
 		customer[i].start = (sy * 100 + sx);
 		customer[i].end = (ey * 100 + ex);
 
-		board[sy][sx] = i; //격자에 승객 번호를 남긴다
+		board[sy][sx] = i; //격자에 승객의 번호(인덱스)를 남긴다
 	}
 
 	go();
