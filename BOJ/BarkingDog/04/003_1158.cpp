3f0d4f292b68eb4877f65bfd161eb77f0df6679b
gyunjongpark <idjkda4034@gmail.com>
2024-10-16 23:02:51 +0900

    Create 003_1158.cpp

---
+++ BOJ/BarkingDog/04/003_1158.cpp
@@ -0,0 +1,40 @@
+#include<iostream>
+#include<list>
+using namespace std;
+
+list<int> L;
+int n, k;
+
+int main(void) {
+	ios_base::sync_with_stdio(false);
+	cin.tie(NULL); cout.tie(NULL);
+
+	cin >> n >> k;
+
+	for (int i = 0; i < n; i++) {
+		L.push_back(i + 1);
+	}
+
+	int cur = k;
+
+	cout << "<";
+
+	while (true) {
+		if (L.size() == 1) {
+			cout << L.front();
+			break;
+		}
+
+		for (int i = 0; i < cur - 1; i++) {
+			L.push_back(L.front());
+			L.pop_front();
+		}
+
+		cout << L.front() << ", ";
+		L.erase(L.begin());
+	}
+
+	cout << ">";
+
+	return 0;
+}
