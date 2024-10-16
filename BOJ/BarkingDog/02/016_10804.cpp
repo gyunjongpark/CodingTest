6d91890816b26e2af693d10851edeb9b7c71ad7d
gyunjongpark <idjkda4034@gmail.com>
2024-10-16 15:06:03 +0900

    Create 016_10804.cpp

---
+++ BOJ/BarkingDog/02/016_10804.cpp
@@ -0,0 +1,26 @@
+#include<iostream>
+#include<algorithm> //reverse
+using namespace std;
+
+int arr[20], s, e;
+
+int main() {
+	ios_base::sync_with_stdio(false);
+	cin.tie(NULL), cout.tie(NULL);
+
+	for (int i = 0; i < 20; i++) {
+		arr[i] = i + 1;
+	}
+
+	for (int i = 0; i < 10; i++) {
+		cin >> s >> e;
+
+		reverse(arr + s - 1, arr + e); //이터레이터 연산 주의!
+	}
+
+	for (int i : arr) {
+		cout << i << ' ';
+	}
+
+	return 0;
+}
