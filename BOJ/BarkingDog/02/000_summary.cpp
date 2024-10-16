e0d02a5f5ddc4720b27ca9b1c7d19d55b97d4300
gyunjongpark <idjkda4034@gmail.com>
2024-10-16 22:38:34 +0900

    Update 000_summary.cpp

--- BOJ/BarkingDog/02/000_summary.cpp
+++ BOJ/BarkingDog/02/000_summary.cpp
@@ -1,37 +1,38 @@
 #include<iostream>
 using namespace std;
 
+int a[10] = { 1, };
+
 struct Shark {
-	int y, x, dir;
+	int y, x;
 };
-Shark shark;
+Shark shark = { 1,1 };
+
+//배열
+void changearr(int arr[]) {
+	arr[0] = 10;
+
+	return;
+}
 
 //단순한 자료형 변수, 구조체, 벡터(call by value)를 인자로 사용해 메모리 내 값을 변경하는 함수는 참조자(&)나 포인터 연산이 필요하다
-//배열을 인자로 넘기는 경우는 call by reference이므로 참조자는 필요 없다
-
-//call by value. 구조체의 값이 '복사'되는 것 뿐이므로 실제 값은 변하지 않는다
-void changeShark1(Shark shark) {
-	shark.y = 0;
-	shark.x = 0;
-	shark.dir = 0;
+void changestruct1(Shark shark) {
+	shark.y = 10;
+	shark.x = 10;
 
 	return;
 }
 
-//call by reference. 값이 변한다(참조자 연산)
-void changeShark2(Shark& shark) {
-	shark.y = 0;
-	shark.x = 0;
-	shark.dir = 0;
+void changestruct2(Shark& shark) {
+	shark.y = 10;
+	shark.x = 10;
 
 	return;
 }
 
-//call by reference. 값이 변한다(포인터 연산)
-void changeShark3(Shark* shark) {
-	(*shark).y = 0;
-	(*shark).x = 0;
-	(*shark).dir = 0;
+void changestruct3(Shark* shark) {
+	(*shark).y = 20;
+	(*shark).x = 20;
 
 	return;
 }
@@ -40,25 +41,17 @@ int main() {
 	ios_base::sync_with_stdio(false);
 	cin.tie(NULL); cout.tie(NULL);
 
-	shark.y = 1;
-	shark.x = 1;
-	shark.dir = 1;
+	changearr(a); //call by reference. a[0]을 10으로 변경
+	cout << a[0] << '\n'; //10
 
-	changeShark1(shark);
-	changeShark2(shark);
-	changeShark3(&shark);
+	changestruct1(shark); //call by value. 값이 변경되지 않는다.
+	cout << shark.y << ' ' << shark.x << '\n'; //1 1
 
-	cout << shark.y << '\n';
-	cout << shark.x << '\n';
-	cout << shark.dir << '\n';
+	changestruct2(shark); //call by reference. 참조자 연산을 통한 값의 변경
+	cout << shark.y << ' ' << shark.x << '\n'; //10 10
 
-	shark.y = 2;
-	shark.x = 2;
-	shark.dir = 2;
-
-	cout << shark.y << '\n';
-	cout << shark.x << '\n';
-	cout << shark.dir << '\n';
+	changestruct3(&shark); //call by reference. 포인터 연산을 통한 값의 변경
+	cout << shark.y << ' ' << shark.x << '\n'; //20 20
 
 	return 0;
 }
