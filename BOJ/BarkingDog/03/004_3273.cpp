a1a6b80c804dac8717095eef9a6bf7ed0515d833
gyunjongpark <idjkda4034@gmail.com>
2024-10-16 15:53:23 +0900

    Create 004_3273.cpp

---
+++ BOJ/BarkingDog/03/004_3273.cpp
@@ -0,0 +1,28 @@
+#include<iostream>
+using namespace std;
+
+int n, a[1000001], x, ret;
+int cnt[2000001];
+
+int main() {
+	ios_base::sync_with_stdio(false);
+	cin.tie(NULL); cout.tie(NULL);
+
+	cin >> n;
+
+	for (int i = 1; i <= n; i++) {
+		cin >> a[i];
+	}
+
+	cin >> x;
+
+	for (int i = 1; i <= n; i++) {
+		if (x > a[i] && cnt[x - a[i]]) ret++;
+
+		cnt[a[i]] = 1;
+	}
+
+	cout << ret;
+
+	return 0;
+}
