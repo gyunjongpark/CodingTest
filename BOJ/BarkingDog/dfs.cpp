37e2faa5a0aeb4a12e71858baedb32dc5fa26ee3
gyunjongpark <idjkda4034@gmail.com>
2024-10-17 16:58:59 +0900

    Create dfs.cpp

---
+++ BOJ/BarkingDog/dfs.cpp
@@ -0,0 +1,17 @@
+void dfs(int y, int x){
+    visited[y][x] = 1;
+    ret++;
+
+    for (int i = 0; i < 4; i++) {
+        int ny = y + dy[i];
+        int nx = x + dx[i];
+
+        if (ny < 0 || ny >= n || nx < 0 || nx >= m) continue;
+
+        if (board[ny][nx] && !visited[ny][nx]) {
+            dfs(ny, nx);
+        }
+    }
+
+    return;
+}
