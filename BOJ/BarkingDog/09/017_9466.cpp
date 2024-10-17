c1d1675e3ba0b48b9935b738ad7b591273d13920
gyunjongpark <idjkda4034@gmail.com>
2024-10-17 16:43:11 +0900

    Update 017_9466.cpp

--- BOJ/BarkingDog/09/017_9466.cpp
+++ BOJ/BarkingDog/09/017_9466.cpp
@@ -28,6 +28,11 @@ void run(int here) {
         //돌고돌아 cur를 재방문했지만 cur != here인 경우
         //here는 사이클에 속해있지 않다!
         if (state[cur] == VISITED && cur != here) {
+            while (state[cur] != IN_CYCLE) { //사이클을 전부 돌 때까지
+                state[cur] = IN_CYCLE; //사이클에 속하는 state를 visited에서 IN_CYCLE으로 변경
+                cur = a[cur];
+            }
+
             cur = here; //here에서 다시 시작
 
             while (state[cur] != IN_CYCLE) { //IN_CYCLE인 cur를 만나기 전까지(사이클에 속하지 않는 state 동안)
@@ -42,8 +47,8 @@ void run(int here) {
             cur = here; //here부터 다시 시작
 
             //here가 사이클이라면 첫번째 if문에서 이미 state[here]가 VISITED가 아닌 IN_CYCLE이 되어야 하므로 NOT_IN_CYCLE. 즉, 끼어들 수 없다는 의미가 된다
-            //사이클이 아니라면 당연히 NOT_IN_CYCLE 처리
-            while (state[cur] == VISITED) { //사이클 소속 여부에 관계없이 방문만 되어있는 state라면 전부 NOT_IN_CYCLE으로 변경
+            //사이클 소속 여부에 관계없이 방문만 되어있는 state라면 전부 NOT_IN_CYCLE으로 변경
+            while (state[cur] == VISITED) {
                 state[cur] = NOT_IN_CYCLE;
                 cur = a[cur];
             }
