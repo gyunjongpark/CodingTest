4b9216bf6be52cea55100c3e02883f99d089812c
gyunjongpark <idjkda4034@gmail.com>
2024-10-18 09:52:33 +0900

    Update 010_14956.cpp

--- BOJ/BarkingDog/11/010_14956.cpp
+++ BOJ/BarkingDog/11/010_14956.cpp
@@ -1,5 +1,6 @@
 #include<iostream>
 using namespace std;
+
 int side, walk;
 
 pair<int, int> philo(int side, int walk) {
@@ -17,7 +18,7 @@ pair<int, int> philo(int side, int walk) {
 	}
 
 	int half = side / 2;
-  int section = half * half;
+  	int section = half * half;
 
 	if (walk <= section) {
 		pair<int, int> rv = philo(half, walk);
