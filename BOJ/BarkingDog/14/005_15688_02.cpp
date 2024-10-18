17ca29314970d07b51ba9f568add04710dcc885c
gyunjongpark <idjkda4034@gmail.com>
2024-10-18 15:32:30 +0900

    Create 005_15688_02.cpp

---
+++ BOJ/BarkingDog/14/005_15688_02.cpp
@@ -0,0 +1,30 @@
+#include<iostream>
+#include<vector>
+#include<algorithm> //sort
+using namespace std;
+
+int n, temp;
+vector<int> v;
+
+bool cmp(int a, int b) {
+	return a < b;
+}
+
+int main() {
+	ios_base::sync_with_stdio(false);
+	cin.tie(NULL); cout.tie(NULL);
+
+	cin >> n;
+
+	for (int i = 0; i < n; i++) {
+		cin >> temp;
+		v.push_back(temp);
+	}
+
+	//N은 10^6이므로 O(NlogN) 내의 정렬 방식을 사용해야 한다
+	sort(v.begin(), v.end(), cmp);
+
+	for (int i : v) cout << i << '\n';
+
+	return 0;
+}
