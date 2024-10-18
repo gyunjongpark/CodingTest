938666aa099bd32fbbc20e40ec75ea4d59b4d7db
gyunjongpark <idjkda4034@gmail.com>
2024-10-18 15:50:31 +0900

    Create 007_11656.cpp

---
+++ BOJ/BarkingDog/15/007_11656.cpp
@@ -0,0 +1,25 @@
+#include<iostream>
+#include<vector>
+#include<algorithm> //sort
+using namespace std;
+
+string s;
+vector<string> v;
+
+int main() {
+	ios_base::sync_with_stdio(false);
+	cin.tie(NULL); cout.tie(NULL);
+
+	cin >> s;
+
+	while (!s.empty()) {
+		v.push_back(s);
+		s.erase(0, 1);
+	}
+
+	sort(v.begin(), v.end());
+
+	for (string s : v) cout << s << '\n';
+
+	return 0;
+}
