8f26feb58e4651d0c36c1a0490c0ad186b83a178
gyunjongpark <idjkda4034@gmail.com>
2024-10-18 13:56:07 +0900

    Create 011_3190.cpp

---
+++ BOJ/BarkingDog/13/011_3190.cpp
@@ -0,0 +1,76 @@
+#include<iostream>
+#include<vector>
+#include<deque>
+using namespace std;
+
+const int dy[] = { -1,0,1,0 };
+const int dx[] = { 0,1,0,-1 };
+int n, k, y, x, l, t, dir, ret;
+bool a[101][101], visited[101][101];
+char c;
+deque<pair<int, int>> v;
+
+void go() {
+	visited[0][0] = 1;
+
+	deque<pair<int, int>> dq;
+	dq.push_back({ 0,0 }); //게임이 시작할 때 뱀은 맨 위, 좌측에 위치
+	
+	dir = 1; //초기 방향은 오른쪽
+
+	while (!dq.empty()) {
+		ret++;
+
+		int ny = dq.front().first + dy[dir];
+		int nx = dq.front().second + dx[dir];
+
+		if (ny < 0 || ny >= n || nx < 0 || nx >= n) break; //벽에 부딪히는 경우
+		if (visited[ny][nx]) break; //자기 자신의 몸과 부딪히는 경우
+
+		dq.push_front({ ny,nx });
+		visited[ny][nx] = 1;
+
+		if (a[ny][nx] == 1) {
+			a[ny][nx] = 0;
+		}
+		else {
+			visited[dq.back().first][dq.back().second] = 0;
+			dq.pop_back(); //몸길이를 줄인다
+		}
+
+		if (!v.empty() && ret == v.front().first) { //입력된 시간이 되면 방향을 바꾼다
+			dir = (dir + v.front().second) % 4;
+			v.pop_front();
+		}
+	}
+
+	return;
+}
+
+int main() {
+	ios_base::sync_with_stdio(false);
+	cin.tie(NULL); cout.tie(NULL);
+
+	cin >> n >> k;
+
+	for (int i = 0; i < k; i++) {
+		cin >> y >> x;
+
+		a[y - 1][x - 1] = 1;
+	}
+
+	cin >> l;
+
+	for (int i = 0; i < l; i++) {
+		cin >> t >> c;
+
+		if (c == 'D') v.push_back({ t,1 });
+		else v.push_back({ t,3 });
+	}
+
+	go();
+
+	cout << ret;
+
+	return 0;
+}
