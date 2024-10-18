0728dbf5efa951198900f3e1d7ad0cf7979eb26b
gyunjongpark <idjkda4034@gmail.com>
2024-10-18 10:20:02 +0900

    Create 003_1182_02.cpp

---
+++ BOJ/BarkingDog/12/003_1182_02.cpp
@@ -0,0 +1,35 @@
+#include<iostream>
+using namespace std;
+
+int n, s, ret, a[21];
+
+void recur(int cur, int sum) {
+    if (cur == n) {
+        if (sum == s) ret++;
+        return;
+    }
+
+    recur(cur + 1, sum);
+    recur(cur + 1, sum + a[cur]);
+
+    return;
+}
+
+int main() {
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL); cout.tie(NULL);
+
+    cin >> n >> s;
+
+    for (int i = 0; i < n; i++) {
+        cin >> a[i];
+    }
+
+    recur(0, 0);
+
+    if (s == 0) ret--; //부분 수열이므로 공집합은 제외
+
+    cout << ret;
+
+    return 0;
+}
