3718b773d9918e55ed2a4ed4c7ae34f9a06d8c52
gyunjongpark <idjkda4034@gmail.com>
2024-10-22 09:21:52 +0900

    Update 013_10172.cpp

--- BOJ/ByStep/01/013_10172.cpp
+++ BOJ/ByStep/01/013_10172.cpp
@@ -5,6 +5,7 @@ int main() {
     ios_base::sync_with_stdio(false);
     cin.tie(NULL); cout.tie(NULL);
 
+	//"와 \는 \를 붙여서 표현
 	cout << "|\\_/|\n";
 	cout << "|q p|   /}\n";
 	cout << "( 0 )\"\"\"\\\n";
