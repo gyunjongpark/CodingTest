2a81636a17eb97e9332d00d0cd8820234f2df417
gyunjongpark <idjkda4034@gmail.com>
2024-10-22 10:30:51 +0900

    Update 008_1003.cpp

--- BOJ/BarkingDog/16/008_1003.cpp
+++ BOJ/BarkingDog/16/008_1003.cpp
@@ -6,7 +6,7 @@ int main() {
 	ios_base::sync_with_stdio(false);
 	cin.tie(NULL); cout.tie(NULL);
 
-  //init
+	//init
 	dp[0] = 0;
 	dp[1] = 1;
 
@@ -22,7 +22,7 @@ int main() {
 
 		if (n == 0) cout << 1 << ' ' << 0 << '\n';
 		else if (n == 1) cout << 0 << ' ' << 1 << '\n';
-		else cout << dp[n - 1] << ' ' << dp[n] << '\n'; //도식화 후 규칙성 찾고 완성된 dp와 연관 짓는다
+		else cout << dp[n - 1] << ' ' << dp[n] << '\n'; //도식화 후 규칙성을 찾고 dp와 연결
 	}
 
 	return 0;
