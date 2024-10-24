daeb1645c73da76ad22e6824325e5c30808e15bd
gyunjongpark <idjkda4034@gmail.com>
2024-10-24 21:47:54 +0900

    Update 001_1463_01.cpp

--- BOJ/BarkingDog/16/001_1463_01.cpp
+++ BOJ/BarkingDog/16/001_1463_01.cpp
@@ -7,12 +7,12 @@ int main() {
     ios_base::sync_with_stdio(false);
     cin.tie(NULL); cout.tie(NULL);
 
-    cin >> n;
-
     dp[1] = 0;
 
+    cin >> n;
+
     for (int i = 2; i <= n; i++) {
-        dp[i] = dp[i - 1] + 1; //3번 방법(1, 2번 방법이 안될 때 사용)
+        dp[i] = dp[i - 1] + 1; //3번 방법(default)
 
         if (i % 2 == 0) dp[i] = min(dp[i], dp[i / 2] + 1); //2번 방법
         if (i % 3 == 0) dp[i] = min(dp[i], dp[i / 3] + 1); //1번 방법
