c27a7d7ad7217754c140a3ceaeb8e546694c04e2
gyunjongpark <idjkda4034@gmail.com>
2024-10-24 21:49:43 +0900

    Update 002_9095.cpp

--- BOJ/BarkingDog/16/002_9095.cpp
+++ BOJ/BarkingDog/16/002_9095.cpp
@@ -9,11 +9,11 @@ int main() {
 
     dp[1] = 1; dp[2] = 2; dp[3] = 4; //init
 
-    cin >> t;
-
     for (int i = 4; i < 11; i++) {
         dp[i] = dp[i - 1] + dp[i - 2] + dp[i - 3];
     }
+
+    cin >> t;
 
     while (t--) {
         cin >> n;
