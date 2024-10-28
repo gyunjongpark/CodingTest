55ecb3da5a84b1d0996441735fd66e09bb9f25c9
gyunjongpark <idjkda4034@gmail.com>
2024-10-29 07:46:40 +0900

    Update 02_02.cpp

--- Programmers/PCCE/02_02.cpp
+++ Programmers/PCCE/02_02.cpp
@@ -5,9 +5,9 @@ int main(void) {
     int a;
     int c;
     cin >> a >> c;
-    
-    int b_square = c*c - a*a;
+
+    int b_square = c * c - a * a;
     cout << b_square << endl;
-  
+
     return 0;
 }
