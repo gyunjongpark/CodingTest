48a68c7adad7817e734d8027f299e704cae0415f
gyunjongpark <idjkda4034@gmail.com>
2024-10-29 07:47:14 +0900

    Update 02_05.cpp

--- Programmers/PCCE/02_05.cpp
+++ Programmers/PCCE/02_05.cpp
@@ -6,26 +6,26 @@ vector<int> solution(string route) {
     int east = 0;
     int north = 0;
     vector<int> answer(2);
-  
-    for(int i=0; i<route.length(); i++){
-        switch(route[i]){
-            case 'N':
-                north++;
-                break;
-            case 'S':
-                north--;
-                break;
-            case 'E':
-                east++;
-                break;
-            case 'W':
-                east--;
-                break;
+
+    for (int i = 0; i < route.length(); i++) {
+        switch (route[i]) {
+        case 'N':
+            north++;
+            break;
+        case 'S':
+            north--;
+            break;
+        case 'E':
+            east++;
+            break;
+        case 'W':
+            east--;
+            break;
         }
     }
-  
+
     answer[0] = east;
     answer[1] = north;
-  
+
     return answer;
 }
