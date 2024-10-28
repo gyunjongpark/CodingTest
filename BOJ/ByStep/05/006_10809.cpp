9d4d1a782e816b3c2bbf61db453aa004f8047726
gyunjongpark <idjkda4034@gmail.com>
2024-10-29 08:54:03 +0900

    Update 006_10809.cpp

--- BOJ/ByStep/05/006_10809.cpp
+++ BOJ/ByStep/05/006_10809.cpp
@@ -10,8 +10,8 @@ int main() {
 	cin >> s;
 
 	for (int i = 0; i < 26; i++) {
-		//find(문자열, 찾는 위치) : 해당 위치의 인덱스 반환, 못찾으면 string::npos 반환
-		//string::npos는 size_t 타입(64비트 OS에서 8바이트)의 최대값 = unsigned long long의 최대값을 반환하므로 (int)로 형변환 시 -1을 반환한다
+		//find(문자열, 시작 위치) : 해당 위치의 인덱스 반환, 못 찾으면 string::npos 반환
+		//string::npos는 size_t 타입(64비트 OS에서 8바이트)의 최대값 = unsigned long long의 최대값을 반환하므로 (int)로 형 변환 시 -1을 반환한다
 		cout << (int)s.find(alph[i]) << ' ';
 	}
 
