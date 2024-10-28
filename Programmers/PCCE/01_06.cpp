a5553f1c8969110206a2db92d1a8a573c316f33c
gyunjongpark <idjkda4034@gmail.com>
2024-10-29 07:45:37 +0900

    Update 01_06.cpp

--- Programmers/PCCE/01_06.cpp
+++ Programmers/PCCE/01_06.cpp
@@ -4,12 +4,12 @@ using namespace std;
 
 int solution(int storage, int usage, vector<int> change) {
     int total_usage = 0;
-  
-    for(int i=0; i<change.size(); i++){
+
+    for (int i = 0; i < change.size(); i++) {
         usage += usage * change[i] / 100;
         total_usage += usage;
-      
-        if(total_usage > storage){
+
+        if (total_usage > storage) {
             return i;
         }
     }
