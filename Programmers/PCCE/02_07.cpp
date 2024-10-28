22b0fe073d7da8dc21a726bfb22d0686d3c34b98
gyunjongpark <idjkda4034@gmail.com>
2024-10-29 07:47:45 +0900

    Update 02_07.cpp

--- Programmers/PCCE/02_07.cpp
+++ Programmers/PCCE/02_07.cpp
@@ -2,14 +2,14 @@
 #include <vector>
 using namespace std;
 
-int func1(int humidity, int val_set){
-    if(humidity < val_set)
+int func1(int humidity, int val_set) {
+    if (humidity < val_set)
         return 3;
     return 1;
 }
 
-int func2(int humidity){
-    if(humidity >= 50)
+int func2(int humidity) {
+    if (humidity >= 50)
         return 0;
     else if (humidity >= 40)
         return 1;
@@ -23,24 +23,24 @@ int func2(int humidity){
         return 5;
 }
 
-int func3(int humidity, int val_set){
-    if(humidity < val_set)
+int func3(int humidity, int val_set) {
+    if (humidity < val_set)
         return 1;
     return 0;
 }
 
 int solution(string mode_type, int humidity, int val_set) {
     int answer = 0;
-  
-    if(mode_type == "auto"){
+
+    if (mode_type == "auto") {
         answer = func2(humidity);
     }
-    else if(mode_type == "target"){
+    else if (mode_type == "target") {
         answer = func1(humidity, val_set);
     }
-    else if(mode_type == "minimum"){
+    else if (mode_type == "minimum") {
         answer = func3(humidity, val_set);
     }
-  
+
     return answer;
 }
