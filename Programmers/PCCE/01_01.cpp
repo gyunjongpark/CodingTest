852300643d8b2d551f6b783434ab019ec634c555
gyunjongpark <idjkda4034@gmail.com>
2024-10-29 07:44:24 +0900

    Update 01_01.cpp

--- Programmers/PCCE/01_01.cpp
+++ Programmers/PCCE/01_01.cpp
@@ -4,8 +4,8 @@ using namespace std;
 int main(void) {
     string message = "Let's go!";
 
-    cout << "3"<<'\n'<<"2"<<'\n'<<"1" << endl;
+    cout << "3" << '\n' << "2" << '\n' << "1" << endl;
     cout << message << endl;
-    
+
     return 0;
 }
