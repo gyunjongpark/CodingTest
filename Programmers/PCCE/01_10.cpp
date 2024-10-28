ed7832f2a62c2cd193a80794041cd19b5476c437
gyunjongpark <idjkda4034@gmail.com>
2024-10-28 23:33:55 +0900

    Create 01_10.cpp

---
+++ Programmers/PCCE/01_10.cpp
@@ -0,0 +1,47 @@
+#include <string>
+#include <vector>
+
+using namespace std;
+
+bool postable(int mat_size, vector<vector<string>> park) {
+    bool flag = true;
+    int park_row, park_col;
+    park_row = park.size();
+    park_col = park[0].size();
+
+    for (int y = 0; y <= park_row - mat_size; y++) {
+        for (int x = 0; x <= park_col - mat_size; x++) {
+            flag = true;
+
+            for (int i = 0; i < mat_size; i++) {
+                for (int j = 0; j < mat_size; j++) {
+                    if (park[i + y][j + x] != "-1") {
+                        flag = false;
+                        break;
+                    }
+                }
+                if (!flag) {
+                    break;
+                }
+            }
+            if (flag) {
+                return true;
+            }
+        }
+    }
+    return false;
+}
+
+int solution(vector<int> mats, vector<vector<string>> park) {
+    int answer = 0;
+
+    for (int i : mats) {
+        if (postable(i, park)) {
+            if (i > answer) answer = i;
+        }
+    }
+
+    if (answer == 0) return -1;
+
+    return answer;
+}
