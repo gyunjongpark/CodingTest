fe6c9363d018a4d797cd115304572460df886b2f
gyunjongpark <idjkda4034@gmail.com>
2024-10-29 07:45:50 +0900

    Update 01_07.cpp

--- Programmers/PCCE/01_07.cpp
+++ Programmers/PCCE/01_07.cpp
@@ -3,38 +3,38 @@
 
 using namespace std;
 
-int func1(int num){
-    if(0 > num){
+int func1(int num) {
+    if (0 > num) {
         return 0;
     }
-    else{
+    else {
         return num;
     }
 }
 
-int func2(int num){
-    if(num > 0){
+int func2(int num) {
+    if (num > 0) {
         return 0;
     }
-    else{
+    else {
         return num;
     }
 }
 
-int func3(vector<string> station){
+int func3(vector<string> station) {
     int num = 0;
-    for(int i=0; i<station.size(); i++){
-        if(station[i] == "Off"){
+    for (int i = 0; i < station.size(); i++) {
+        if (station[i] == "Off") {
             num += 1;
         }
     }
     return num;
 }
 
-int func4(vector<string> station){
+int func4(vector<string> station) {
     int num = 0;
-    for(int i=0; i<station.size(); i++){
-        if(station[i] == "On"){
+    for (int i = 0; i < station.size(); i++) {
+        if (station[i] == "On") {
             num += 1;
         }
     }
@@ -43,7 +43,7 @@ int func4(vector<string> station){
 
 int solution(int seat, vector<vector<string>> passengers) {
     int num_passenger = 0;
-    for(int i=0; i<passengers.size(); i++){
+    for (int i = 0; i < passengers.size(); i++) {
         num_passenger += func4(passengers[i]);
         num_passenger -= func3(passengers[i]);
     }
