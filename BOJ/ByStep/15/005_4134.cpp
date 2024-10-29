30805bc24951d1b3ebb812ddc7cd9ff064a18a61
gyunjongpark <idjkda4034@gmail.com>
2024-10-29 09:57:39 +0900

    Update 005_4134.cpp

--- BOJ/ByStep/15/005_4134.cpp
+++ BOJ/ByStep/15/005_4134.cpp
@@ -1,14 +1,13 @@
 #include<iostream>
 #include<vector>
-#include<cmath> //sqrt
 using namespace std;
 
 unsigned int n, t; //정수 n(0 ≤ n ≤ 4 * 10^9)가 주어졌을 때
 
 bool checkPrime(unsigned int num) { // 4 * 10^9
 	if (num <= 1) return 0;
-	
-	for (unsigned int i = 2; i <= (unsigned int)sqrt(num); i++) {
+
+	for (unsigned int i = 2; i * i <= num; i++) {
 		if (num % i == 0) return 0;
 	}
 
@@ -25,10 +24,10 @@ int main() {
 		cin >> n;
 
 		while (true) {
-			if (checkPrime(n)) break;	
+			if (checkPrime(n)) break;
 			n++;
 		}
-		
+
 		cout << n << '\n';
 	}
 
