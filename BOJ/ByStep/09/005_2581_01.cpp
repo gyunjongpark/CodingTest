0afeae193ed48ecdda6a5887c4837ab43ae01735
gyunjongpark <idjkda4034@gmail.com>
2024-10-29 09:44:37 +0900

    Update 005_2581_01.cpp

--- BOJ/ByStep/09/005_2581_01.cpp
+++ BOJ/ByStep/09/005_2581_01.cpp
@@ -1,5 +1,4 @@
 #include<iostream>
-#include<cmath> //sqrt
 using namespace std;
 
 int n, m, ret;
@@ -7,8 +6,8 @@ int minVal = 10001;
 
 bool check(int n) {
 	if (n <= 1) return 0;
-	
-	for (int i = 2; i <= (int)sqrt(n); i++) {
+
+	for (int i = 2; i * i <= n; i++) {
 		if (n % i == 0) return 0;
 	}
 
@@ -27,7 +26,7 @@ int main() {
 			minVal = i;
 		}
 	}
-	
+
 	if (ret == 0) cout << -1;
 	else cout << ret << '\n' << minVal;
 
