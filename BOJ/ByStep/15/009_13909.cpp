52a95a1d935490ad5059d134c7da92976312dbb8
gyunjongpark <idjkda4034@gmail.com>
2024-10-29 10:14:31 +0900

    Update 009_13909.cpp

--- BOJ/ByStep/15/009_13909.cpp
+++ BOJ/ByStep/15/009_13909.cpp
@@ -1,15 +1,19 @@
 #include<iostream>
-#include<cmath> //sqrt
 using namespace std;
 
-int n;
+int n, ret;
 
 int main() {
 	ios_base::sync_with_stdio(false);
 	cin.tie(NULL); cout.tie(NULL);
 
 	cin >> n;
-	cout << (int)sqrt(n);
+
+	for (int i = 1; i * i <= n; i++) {
+		ret++;
+	}
+
+	cout << ret;
 
 	return 0;
 }
