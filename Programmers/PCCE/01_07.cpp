f5db9e1a34093416d2cbcaf1f58377ef5af3e731
gyunjongpark <idjkda4034@gmail.com>
2024-10-31 09:34:59 +0900

    Update 01_07.cpp

--- Programmers/PCCE/01_07.cpp
+++ Programmers/PCCE/01_07.cpp
@@ -22,30 +22,37 @@ int func2(int num) {
 
 int func3(vector<string> station) {
     int num = 0;
+    
     for (int i = 0; i < station.size(); i++) {
         if (station[i] == "Off") {
             num += 1;
         }
     }
+    
     return num;
 }
 
 int func4(vector<string> station) {
     int num = 0;
+    
     for (int i = 0; i < station.size(); i++) {
         if (station[i] == "On") {
             num += 1;
         }
     }
+    
     return num;
 }
 
 int solution(int seat, vector<vector<string>> passengers) {
     int num_passenger = 0;
+    
     for (int i = 0; i < passengers.size(); i++) {
         num_passenger += func4(passengers[i]);
         num_passenger -= func3(passengers[i]);
     }
+    
     int answer = func1(seat - num_passenger);
+    
     return answer;
 }
