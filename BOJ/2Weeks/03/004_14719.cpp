41a19fe04ffae19931accd88e04b51d9f727bb04
gyunjongpark <idjkda4034@gmail.com>
2024-10-31 10:32:41 +0900

    Create 004_14719.cpp

---
+++ BOJ/2Weeks/03/004_14719.cpp
@@ -0,0 +1,44 @@
+#include<iostream>
+using namespace std;
+
+int n, m, temp, a[502], ret;
+int maxVal, maxPos, maxVal_front, maxVal_back;
+
+int main() {
+	ios_base::sync_with_stdio(false);
+	cin.tie(NULL); cout.tie(NULL);
+
+	cin >> n >> m;
+
+	for (int i = 1; i <= m; i++) {
+		cin >> temp;
+		a[i] = temp;
+
+		if (temp >= maxVal) {
+			maxVal = temp;
+			maxPos = i;
+		}
+	}
+
+	for (int i = 1; i < maxPos; i++) {
+		if (a[i - 1] >= a[i]) {
+			maxVal_front = a[i - 1];
+			ret += maxVal_front - a[i];
+			a[i] = maxVal_front;
+		}
+		else maxVal_front = a[i];
+	}
+
+	for (int i = 500; i > maxPos; i--) {
+		if (a[i + 1] >= a[i]) {
+			maxVal_back = a[i + 1];
+			ret += maxVal_back - a[i];
+			a[i] = maxVal_back;
+		}
+		else maxVal_back = a[i];
+	}
+
+	cout << ret;
+
+	return 0;
+}
