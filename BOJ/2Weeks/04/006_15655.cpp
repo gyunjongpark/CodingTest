7390ca9b374bcb4cf89f82232d4e82ed8fbea57c
gyunjongpark <idjkda4034@gmail.com>
2024-11-01 08:54:10 +0900

    Create 006_15655.cpp

---
+++ BOJ/2Weeks/04/006_15655.cpp
@@ -0,0 +1,49 @@
+#include<iostream>
+#include<vector>
+#include<algorithm> //sort
+using namespace std;
+
+int n, m, a[10];
+vector<int> v;
+vector<bool> visited(10001, false); //입력으로 주어지는 수는 10,000보다 작거나 같은 자연수이다
+
+void print(vector<int> v) {
+	for (int i : v) cout << i << ' '; cout << '\n';
+	return;
+}
+
+void combi(int start, vector<int>& v, vector<bool>& visited) {
+	if (v.size() == m) {
+		print(v);
+		return;
+	}
+
+	for (int i = start + 1; i < n; i++) {
+		if (visited[a[i]]) continue;
+
+		v.push_back(a[i]);
+		visited[a[i]] = true;
+
+		combi(i, v, visited);
+
+		v.pop_back();
+		visited[a[i]] = false;
+	}
+}
+
+int main() {
+	ios_base::sync_with_stdio(false);
+	cin.tie(NULL); cout.tie(NULL);
+
+	cin >> n >> m;
+
+	for (int i = 0; i < n; i++) {
+		cin >> a[i];
+	}
+
+	sort(a, a + n);
+
+	combi(-1, v, visited);
+
+	return 0;
+}
