066b40137b1af2b55e87e2984e6717ccea6a75eb
gyunjongpark <idjkda4034@gmail.com>
2024-10-31 10:33:36 +0900

    Update 005_11660.cpp

--- BOJ/2Weeks/03/005_11660.cpp
+++ BOJ/2Weeks/03/005_11660.cpp
@@ -1,7 +1,7 @@
 #include<iostream>
 using namespace std;
 
-int n, m, temp, dp[1025][1025];
+int n, m, temp, s[1025][1025];
 int a1, a2, b1, b2;
 
 int main() {
@@ -13,13 +13,13 @@ int main() {
     for (int i = 1; i <= n; i++) {
         for (int j = 1; j <= n; j++) {
             cin >> temp;
-            dp[i][j] = dp[i - 1][j] + dp[i][j - 1] - dp[i - 1][j - 1] + temp;
+            s[i][j] = s[i - 1][j] + s[i][j - 1] - s[i - 1][j - 1] + temp;
         }
     }
 
     for (int i = 0; i < m; i++) {
         cin >> a1 >> b1 >> a2 >> b2;
-        cout << dp[a2][b2] - dp[a2][b1 - 1] - dp[a1 - 1][b2] + dp[a1 - 1][b1 - 1] << '\n';
+        cout << s[a2][b2] - s[a2][b1 - 1] - s[a1 - 1][b2] + s[a1 - 1][b1 - 1] << '\n';
     }
 
     return 0;
