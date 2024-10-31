de2ff3e97ee218a0d8e403432dc5deb203d6f9ee
gyunjongpark <idjkda4034@gmail.com>
2024-11-01 07:59:34 +0900

    Update 003_2304.cpp

--- BOJ/2Weeks/03/003_2304.cpp
+++ BOJ/2Weeks/03/003_2304.cpp
@@ -1,7 +1,7 @@
 #include<iostream>
 using namespace std;
 
-int n, x, y, a[1002], s[1002];
+int n, x, y, a[1001], s[1001];
 int maxPos, maxVal;
 
 int main() {
@@ -22,16 +22,14 @@ int main() {
 	}
 
 	//왼쪽부터 maxPos 직전까지 검사
-	//a[0] = 0
-	for (int i = 1; i < maxPos; i++) {
+	for (int i = 2; i < maxPos; i++) {
 		if (a[i - 1] >= a[i]) {
 			a[i] = a[i - 1];
 		}
 	}
 
 	//오른쪽부터 maxPos 직전까지 검사
-	//a[1001] = 0 -> 배열은 1002까지 선언
-	for (int i = 1000; i > maxPos; i--) {
+	for (int i = 999; i > maxPos; i--) {
 		if (a[i + 1] >= a[i]) {
 			a[i] = a[i + 1];
 		}
