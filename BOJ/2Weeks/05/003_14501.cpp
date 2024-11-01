e4c7aa8b3128b7d0fbb5c804cd92620e74037c05
gyunjongpark <idjkda4034@gmail.com>
2024-11-01 11:00:56 +0900

    Create 003_14501.cpp

---
+++ BOJ/2Weeks/05/003_14501.cpp
@@ -0,0 +1,33 @@
+#include<iostream>
+using namespace std;
+
+int n, t[16], p[16];
+int ret = -1; //1 ≤ T[i] ≤ 5, 1 ≤ P[i] ≤ 1,000
+
+void recur(int day, int pay) {
+	if (day > n) return;
+	if (day == n) {
+		ret = max(ret, pay);
+		return;
+	}
+
+	recur(day + t[day], pay + p[day]);
+	recur(day + 1, pay);
+}
+
+int main() {
+	ios_base::sync_with_stdio(false);
+	cin.tie(NULL); cout.tie(NULL);
+
+	cin >> n;
+
+	for (int i = 0; i < n; i++) {
+		cin >> t[i] >> p[i];
+	}
+
+	recur(0, 0);
+
+	cout << ret;
+
+	return 0;
+}
