76491570963a8baec62de231105310d19dce1959
gyunjongpark <idjkda4034@gmail.com>
2024-11-04 11:07:31 +0900

    Create 001_1978_02.cpp

---
+++ BOJ/BarkingDog/18/001_1978_02.cpp
@@ -0,0 +1,43 @@
+#include<iostream>
+using namespace std;
+
+int n, temp, ret;
+bool chae[1001]; //수는 1,000 이하의 자연수이다
+
+void era(int num) { //에라토스테네스의 체
+	chae[0] = false;
+	chae[1] = false;
+
+	for (int i = 2; i <= num; i++) {
+		chae[i] = true;
+	}
+
+	for (int i = 2; i <= num; i++) {
+		if (!chae[i]) continue;
+
+		for (int j = i + i; j <= num; j += i) {
+			chae[j] = false;
+		}
+	}
+
+	return;
+}
+
+int main() {
+	ios_base::sync_with_stdio(false);
+	cin.tie(NULL); cout.tie(NULL);
+
+	cin >> n;
+
+	era(1000); //수는 1,000 이하의 자연수이다
+
+	for (int i = 0; i < n; i++) {
+		cin >> temp;
+
+		if (chae[temp]) ret++;
+	}
+
+	cout << ret;
+
+	return 0;
+}
