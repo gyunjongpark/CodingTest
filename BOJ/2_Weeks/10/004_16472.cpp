a02fbb4fb0d9abfc162981188be3a71e11f96aad
gyunjongpark <idjkda4034@gmail.com>
2024-11-12 10:17:26 +0900

    Update 004_16472.cpp

--- BOJ/2_Weeks/10/004_16472.cpp
+++ BOJ/2_Weeks/10/004_16472.cpp
@@ -2,9 +2,9 @@
 #include<unordered_set>
 using namespace std;
 
-int n, s, e, dist;
+int n;
 string arr;
-unordered_set<char> letters;
+unordered_set<char> letters; //중복 방지를 위한 set
 
 int main() {
     ios_base::sync_with_stdio(false);
@@ -12,25 +12,28 @@ int main() {
 
     cin >> n >> arr;
 
+    int l = 0, r = 0, dist = 0;
+
     letters.insert(arr[0]);
 
-    while (s < arr.size() && e < arr.size()) {
-        dist = max(dist, e - s + 1);
+    while (l < arr.size() && r < arr.size()) {
+        dist = max(dist, r - l + 1);
 
         if (letters.size() <= n) {
-            e++;
+            r++;
 
-            if (e < arr.size() && letters.find(arr[e]) == letters.end()) {
-                letters.insert(arr[e]);
+            if (r < arr.size() && letters.find(arr[r]) == letters.end()) {
+                letters.insert(arr[r]);
             }
         }
 
         if (letters.size() > n) {
-            letters.erase(arr[s]);
-            s++;
-            e = s;
+            letters.erase(arr[l]);
+
+            l++;
+            r = l; //새로운 시작 문자로 초기화 후 다시 탐색
             letters.clear();
-            letters.insert(arr[s]);
+            letters.insert(arr[l]);
         }
     }
 
