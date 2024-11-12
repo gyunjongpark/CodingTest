4ac0767f27e079d379867c109804aab054a76144
gyunjongpark <idjkda4034@gmail.com>
2024-11-12 14:51:23 +0900

    Create 011_1213.cpp

---
+++ BOJ/8_Weeks/01/011_1213.cpp
@@ -0,0 +1,45 @@
+#include<iostream>
+using namespace std;
+
+string s, ret;
+int flag, cnt['Z' + 1];
+char mid = -1;
+
+int main() {
+	ios_base::sync_with_stdio(false);
+	cin.tie(NULL); cout.tie(NULL);
+
+	cin >> s;
+
+	for (char c : s) {
+		cnt[c]++;
+	}
+
+	for (int i = 'Z'; i >= 'A'; i--) {
+		if (cnt[i] % 2) {
+			mid = (char)i;
+			flag++;
+			cnt[i]--;
+		}
+
+		if (flag == 2) break; //mid는 1개가 최대
+
+		for (int j = 0; j < cnt[i]; j += 2) { //양쪽으로 하나씩 덧붙인다
+			ret += (char)i;
+			ret = (char)i + ret;
+		}
+	}
+
+	if (mid != -1) {
+		ret.insert(ret.begin() + ret.size() / 2, mid); //iterator 연산
+	}
+
+	if (flag == 2) {
+		cout << "I'm Sorry Hansoo";
+	}
+	else {
+		cout << ret;
+	}
+
+	return 0;
+}
