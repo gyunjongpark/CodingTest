8ac9ee490e7043abd03fe7200f0be55b815c9b8d
gyunjongpark <idjkda4034@gmail.com>
2024-11-12 20:13:49 +0900

    Update 001_2309_02.cpp

--- BOJ/8_Weeks/01/001_2309_02.cpp
+++ BOJ/8_Weeks/01/001_2309_02.cpp
@@ -21,7 +21,7 @@ int main() {
 			sum += arr[i];
 		}
 
-		if (sum == 100) break; //반드시 탈출 조건을 명시해야 한다
+		if (sum == 100) break; //next_permutation 사용 시 탈출 조건을 반드시 명시해야 한다
 	} while (next_permutation(arr, arr + 9)); //next_permutation : 순열 무한 반복
 
 	for (int i = 0; i < 7; i++) {
