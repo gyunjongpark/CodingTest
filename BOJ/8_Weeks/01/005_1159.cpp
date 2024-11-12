4656c67108bb06dfb109f6ddf47dfe2c00cabb79
gyunjongpark <idjkda4034@gmail.com>
2024-11-12 14:29:47 +0900

    Create 005_1159.cpp

---
+++ BOJ/8_Weeks/01/005_1159.cpp
@@ -0,0 +1,31 @@
+#include<iostream>
+#include<algorithm> //reverse
+using namespace std;
+
+string s, ret;
+int n, cnt[26];
+bool flag;
+
+int main() {
+	ios_base::sync_with_stdio(false);
+	cin.tie(NULL); cout.tie(NULL);
+
+	cin >> n;
+
+	while (n--) {
+		cin >> s;
+		cnt[s[0] - 'a']++;
+	}
+
+	for (int i = 0; i < 26; i++) {
+		if (cnt[i] >= 5) {
+			flag = true;
+			ret += (char)(i + 97); //'a' = 97, 'A' = 65
+		}
+	}
+
+	if (flag) cout << ret;
+	else cout << "PREDAJA";
+
+	return 0;
+}
