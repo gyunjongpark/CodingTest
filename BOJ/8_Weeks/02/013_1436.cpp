03900e0aded869c3e6e07412e1dfd1d488c39f32
gyunjongpark <idjkda4034@gmail.com>
2024-11-13 10:48:54 +0900

    Create 013_1436.cpp

---
+++ BOJ/8_Weeks/02/013_1436.cpp
@@ -0,0 +1,25 @@
+#include<iostream>
+#include<string> //to_string
+using namespace std;
+
+int cnt, n;
+string ret;
+
+int main() {
+	ios_base::sync_with_stdio(false);
+	cin.tie(NULL); cout.tie(NULL);
+
+	cin >> n;
+
+	for (int i = 666; cnt < n; i++) {
+		ret = to_string(i);
+    
+		if (ret.find("666") != string::npos) {
+			cnt++;
+		}
+	}
+  
+	cout << ret;
+
+	return 0;
+}
