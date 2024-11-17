a739e5406359c1fd74481f14b737274199856343
gyunjongpark <idjkda4034@gmail.com>
2024-11-17 19:55:37 +0900

    Update 003_2468.cpp

--- BOJ/8_Weeks/02/003_2468.cpp
+++ BOJ/8_Weeks/02/003_2468.cpp
@@ -3,9 +3,9 @@ using namespace std;
 
 const int dy[] = { -1,0,1,0 };
 const int dx[] = { 0,1,0,-1 };
+bool visited[101][101];
 int n, ny, nx, a[101][101];
 int ret = 1;
-bool visited[101][101];
 
 void dfs(int y, int x, int k) {
 	visited[y][x] = true;
@@ -30,14 +30,16 @@ int main() {
 
 	cin >> n;
 
-	for (int i = 0; i < n; i++)
-		for (int j = 0; j < n; j++)
+	for (int i = 0; i < n; i++) {
+		for (int j = 0; j < n; j++) {
 			cin >> a[i][j];
+		}
+	}
 
 	for (int k = 1; k <= 100; k++) {
-		int cnt = 0;
-
 		fill(&visited[0][0], &visited[0][0] + 101 * 101, false);
+
+		int cnt = 0;
 
 		for (int i = 0; i < n; i++) {
 			for (int j = 0; j < n; j++) {
