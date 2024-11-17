2e362f7f2186b7bf95cd9ce39beba9b2c60a6036
gyunjongpark <idjkda4034@gmail.com>
2024-11-17 20:21:07 +0900

    Update 015_4949.cpp

--- BOJ/8_Weeks/02/015_4949.cpp
+++ BOJ/8_Weeks/02/015_4949.cpp
@@ -1,6 +1,6 @@
 #include<iostream>
-#include<stack>
 #include<string> //getline
+#include<stack>
 using namespace std;
 
 string s;
@@ -13,11 +13,11 @@ int main() {
 	while (getline(cin, s)) {
 		if (s == ".") break;
 
-		stack<char> stk; // init
+		stack<char> stk; //init
 
 		for (char a : s) {
-			if (a == '(' || a == ')' || a == '[' || a == ']') { // 4가지 반 괄호에 대해서만 생각
-				if (stk.empty()) { // stk이 비어있다면 무조건 push
+			if (a == '(' || a == ')' || a == '[' || a == ']') { //괄호에 대해서만 생각
+				if (stk.empty()) { //stk이 비어있다면 무조건 push
 					stk.push(a);
 				}
 				else {
@@ -30,7 +30,7 @@ int main() {
 			if (a == '.') stk.push(a);
 		}
 
-		if (stk.size() == 1 && stk.top() == '.') { // .만 남아있다면
+		if (stk.size() == 1 && stk.top() == '.') { //.만 남아있다면
 			cout << "yes"<<'\n';
 		}
 		else cout << "no"<<'\n';
