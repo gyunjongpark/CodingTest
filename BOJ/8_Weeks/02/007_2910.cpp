7d7f49dd74e1ad867d1b8ffe417ad5a5bc93af6e
gyunjongpark <idjkda4034@gmail.com>
2024-11-17 20:11:53 +0900

    Update 007_2910.cpp

--- BOJ/8_Weeks/02/007_2910.cpp
+++ BOJ/8_Weeks/02/007_2910.cpp
@@ -1,16 +1,16 @@
 #include<iostream>
-#include<map>
-#include<vector>
 #include<algorithm> //sort
+#include<vector>
+#include<map>
 using namespace std;
 
-int n, c, temp, a[1001];
+int n, c, temp;
 map<int, int> mp, mp2;
 vector<pair<int, int>> v;
 
 bool cmp(pair<int, int> a, pair<int, int> b) {
 	if (a.second == b.second) return mp2[a.first] < mp2[b.first];
-	else return a.second > b.second;
+	return a.second > b.second;
 }
 
 int main() {
@@ -21,15 +21,16 @@ int main() {
 
 	for (int i = 0; i < n; i++) {
 		cin >> temp;
-		mp[temp]++; //배열의 값 - 값이 나온 횟수
+
+		mp[temp]++; //값 - 값이 나온 횟수
 
 		if (!mp2[temp]) {
-			mp2[temp] = i + 1; //배열의 값 - 값이 나온 순서
+			mp2[temp] = i + 1; //값 - 값이 나온 순서
 		}
 	}
 
-	for (pair<int, int> p : mp) {
-		v.push_back(p);
+	for (pair<int, int> i : mp) {
+		v.push_back(i);
 	}
 
 	sort(v.begin(), v.end(), cmp);
