ea4a61d73f4befc45d00535d87dcfa08eb5e9c65
gyunjongpark <idjkda4034@gmail.com>
2024-11-18 22:06:32 +0900

    Update 020_17298.cpp

--- BOJ/8_Weeks/02/020_17298.cpp
+++ BOJ/8_Weeks/02/020_17298.cpp
@@ -1,23 +1,24 @@
 #include<iostream>
 #include<stack>
+#include<vector>
 using namespace std;
 
+int n, temp, arr[1000001];
 stack<int> stk;
-int n, a[1000001], ret[1000001];
 
-int main(void) {
+int main() {
 	ios_base::sync_with_stdio(false);
 	cin.tie(NULL); cout.tie(NULL);
-
+	
 	cin >> n;
 
-	fill(&ret[0], &ret[0] + n, -1);
+	vector<int> ret(n, -1);
 
 	for (int i = 0; i < n; i++) {
-		cin >> a[i];
+		cin >> arr[i];
 
-		while (!stk.empty() && a[stk.top()] < a[i]) {
-			ret[stk.top()] = a[i];
+		while (!stk.empty() && arr[stk.top()] < arr[i]) {
+			ret[stk.top()] = arr[i];
 			stk.pop();
 		}
 
