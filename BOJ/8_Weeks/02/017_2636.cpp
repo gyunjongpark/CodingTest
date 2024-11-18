2bfe082605f8b8908c22d4be563952cd3d51e7b3
gyunjongpark <idjkda4034@gmail.com>
2024-11-18 20:39:39 +0900

    Update 017_2636.cpp

--- BOJ/8_Weeks/02/017_2636.cpp
+++ BOJ/8_Weeks/02/017_2636.cpp
@@ -4,16 +4,17 @@ using namespace std;
 
 const int dy[] = { -1,0,1,0 };
 const int dx[] = { 0,1,0,-1 };
-bool visited[101][101], a[101][101];
-int n, m, cnt, ret;
+bool visited[101][101], board[101][101];
+int n, m, ret;
 vector<pair<int, int>> v;
 
 void dfs(int y, int x) {
 	visited[y][x] = true;
 
-	if (a[y][x] == 1) { //치즈라면
-		v.push_back({ y,x }); //녹일 치즈 목록에 추가
-		return; //추가적인 dfs 탐색 없이 return
+	//치즈인지 아닌지 판단
+	if (board[y][x] == 1) {
+		v.push_back({ y,x });
+		return; //더 이상 치즈를 녹이지 않고 return
 	}
 
 	for (int i = 0; i < 4; i++) {
@@ -22,6 +23,7 @@ void dfs(int y, int x) {
 
 		if (ny < 0 || ny >= n || nx < 0 || nx >= m) continue;
 
+		//치즈인지 여부와 관계없이 일단 dfs 탐색
 		if (!visited[ny][nx]) {
 			dfs(ny, nx);
 		}
@@ -38,34 +40,43 @@ int main() {
 
 	for (int i = 0; i < n; i++) {
 		for (int j = 0; j < m; j++) {
-			cin >> a[i][j];
+			cin >> board[i][j];
 		}
 	}
 
 	while (true) {
-		fill(&visited[0][0], &visited[0][0] + 101 * 101, false); //init
-		v.clear(); //녹일 치즈 초기화
+		//초기화
+		fill(&visited[0][0], &visited[0][0] + 101 * 101, false);
+		v.clear();
 
-		dfs(0, 0); //매번 첫 좌표부터 dfs 시작
+		//녹일 치즈 구하기. v에 치즈 좌표 저장
+		dfs(0, 0);
 
+		//치즈 녹이기
 		for (pair<int, int> i : v) {
-			a[i.first][i.second] = 0; //치즈 녹이기
+			board[i.first][i.second] = 0;
 		}
 
-		ret = v.size(); //녹인 치즈의 수
-		cnt++; //치즈를 녹인 횟수 count
+		int cnt = v.size(); //녹이기 직전 치즈의 양
+		ret++; //녹인 횟수 증가
 
 		bool flag = false;
 
 		for (int i = 0; i < n; i++) {
 			for (int j = 0; j < m; j++) {
-				if (a[i][j]) flag = true; //녹일 치즈가 남아있다면 true
+				if (board[i][j] == 1) {
+					flag = true;
+					break;
+				}
 			}
+			if (flag) break;
 		}
-		if (!flag) break; //더 이상 녹일 치즈가 없다면 break
-	}
 
-	cout << cnt << '\n' << ret;
+		if (!flag) {
+			cout << ret << '\n' << cnt;
+			break;
+		}
+	}
 
 	return 0;
 }
