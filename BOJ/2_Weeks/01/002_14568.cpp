b672cd7711a8f2f9ac085e5b2876def129ad3c82
gyunjongpark <idjkda4034@gmail.com>
2024-11-19 10:06:19 +0900

    Update 002_14568.cpp

--- BOJ/2_Weeks/01/002_14568.cpp
+++ BOJ/2_Weeks/01/002_14568.cpp
@@ -12,7 +12,9 @@ int main() {
 	for (int a = 1; a <= n; a++) { //택희
 		for (int b = 1; b <= n; b++) { //영훈이
 			for (int c = 1; c <= n; c++) { //남규
-				if (a + b + c == n && c >= b + 2 && a % 2 == 0) ret++;
+				if (a + b + c == n && c >= b + 2 && a % 2 == 0){
+					ret++;
+				}
 			}
 		}
 	}
