f9398d259652ff1b3cc9d5b9afbf283d9a33253c
gyunjongpark <idjkda4034@gmail.com>
2024-11-19 11:01:59 +0900

    Update 004_16472.cpp

--- BOJ/2_Weeks/10/004_16472.cpp
+++ BOJ/2_Weeks/10/004_16472.cpp
@@ -1,39 +1,45 @@
 #include<iostream>
-#include<unordered_set>
+#include<set>
 using namespace std;
 
 int n;
-string arr;
-unordered_set<char> letters; //중복 방지를 위한 set
+string s;
+set<char> st; //중복 방지를 위한 set
 
 int main() {
     ios_base::sync_with_stdio(false);
     cin.tie(NULL); cout.tie(NULL);
 
-    cin >> n >> arr;
+    cin >> n >> s;
 
-    int l = 0, r = 0, dist = 0;
+    int dist = 0;
 
-    letters.insert(arr[0]);
+    //init
+    int l = 0, r = 0; //two pointer
+    st.clear(); //set
+    st.insert(s[0]);
 
-    while (l < arr.size() && r < arr.size()) {
+    while (l < s.size() && r < s.size()) {
         dist = max(dist, r - l + 1);
 
-        if (letters.size() <= n) {
+        if (st.size() <= n) {
             r++;
 
-            if (r < arr.size() && letters.find(arr[r]) == letters.end()) {
-                letters.insert(arr[r]);
+            //오른쪽 포인터가 문자열 범위 내 + 집합에 해당 포인터의 문자가 없다면
+            if (r < s.size() && st.find(s[r]) == st.end()) {
+                st.insert(s[r]); //해당 포인터의 문자 추가
             }
         }
 
-        if (letters.size() > n) {
-            letters.erase(arr[l]);
+        if (st.size() > n) {
+            st.erase(s[l]); //가장 처음의 문자 제거
 
-            l++;
-            r = l; //새로운 시작 문자로 초기화 후 다시 탐색
-            letters.clear();
-            letters.insert(arr[l]);
+            l++; //왼쪽 포인터 옮기기
+
+            //init
+            r = l; //two pointer
+            st.clear(); //set
+            st.insert(s[l]);
         }
     }
 
