8119b15d44b2e0f9ab717db0fffb3e85277c01be
gyunjongpark <idjkda4034@gmail.com>
2024-11-19 23:49:14 +0900

    Update 001_1753.cpp

--- BOJ/2_Weeks/13/001_1753.cpp
+++ BOJ/2_Weeks/13/001_1753.cpp
@@ -1,48 +1,51 @@
 #include<iostream>
 #include<vector>
 #include<queue> //priority_queue
+#include<climits> //INT_MAX
 using namespace std;
 
-#define X first
-#define Y second
+vector<pair<int, int>> adj[20005]; //{거리 비용, 정점 번호}
+int v, e, st, d[20005]; //최단 거리 테이블
 
-int v, e, st;
+int main() {
+	ios_base::sync_with_stdio(false);
+	cin.tie(NULL); cout.tie(NULL);
 
-// {비용, 정점 번호}
-vector<pair<int, int>> adj[20005];
-const int INF = 0x3f3f3f3f;
-int d[20005]; // 최단 거리 테이블
-int main(void) {
-    ios::sync_with_stdio(0);
-    cin.tie(0);
+	cin >> v >> e >> st;
 
-    cin >> v >> e >> st;
-    fill(d, d + v + 1, INF);
-    while (e--) {
-        int u, v, w;
-        cin >> u >> v >> w;
-        adj[u].push_back({ w,v });
-    }
+	fill(d, d + v + 1, INT_MAX);
 
-    priority_queue<pair<int, int>, vector<pair<int, int>>, greater<pair<int, int>> > pq;
-    d[st] = 0;
-    // 우선순위 큐에 (0, 시작점) 추가
-    pq.push({ d[st],st });
-    while (!pq.empty()) {
-        auto cur = pq.top(); pq.pop(); // {비용, 정점 번호}    
-        // 거리가 d에 있는 값과 다를 경우 넘어감
-        if (d[cur.Y] != cur.X) continue;
-        for (auto nxt : adj[cur.Y]) {
-            if (d[nxt.Y] <= d[cur.Y] + nxt.X) continue;
-            // cur를 거쳐가는 것이 더 작은 값을 가질 경우
-            // d[nxt.Y]을 갱신하고 우선순위 큐에 (거리, nxt.Y)를 추가
-            d[nxt.Y] = d[cur.Y] + nxt.X;
-            pq.push({ d[nxt.Y],nxt.Y });
-        }
-    }
+	for (int i = 0; i < e; i++) {
+		int u, v, w;
 
-    for (int i = 1; i <= v; i++) {
-        if (d[i] == INF) cout << "INF\n";
-        else cout << d[i] << "\n";
-    }
+		cin >> u >> v >> w;
+		adj[u].push_back({ w,v });
+	}
+
+	priority_queue<pair<int, int>, vector<pair<int, int>>, greater<pair<int, int>>> pq;
+
+	d[st] = 0; //시작점의 최단 거리는 0으로 고정
+	pq.push({ d[st],st });
+
+	while (!pq.empty()) {
+		pair<int, int> cur = pq.top(); pq.pop(); //{거리 비용, 정점 번호}
+
+		//해당 번호의 최단 거리와 번호까지의 거리 비용이 다르다면
+		//cur.first가 d[cur.second]보다 크다면 d[번호]의 값을 바꿀 필요가 없음
+		if (d[cur.second] != cur.first) continue;
+
+		for (pair<int,int> next : adj[cur.second]) {
+			if (d[next.second] <= d[cur.second] + next.first) continue;
+
+			d[next.second] = d[cur.second] + next.first;
+			pq.push({ d[next.second], next.second });
+		}
+	}
+
+	for (int i = 1; i <= v; i++) {
+		if (d[i] == INT_MAX) cout << "INF\n";
+		else cout << d[i] << '\n';
+	}
+
+	return 0;
 }
