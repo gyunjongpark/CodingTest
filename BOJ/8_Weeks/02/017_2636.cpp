6dff48b94e4c550118e950d792caedb012a1c5c8
gyunjongpark <idjkda4034@gmail.com>
2024-11-22 14:13:27 +0900

    Update 017_2636.cpp

--- BOJ/8_Weeks/02/017_2636.cpp
+++ BOJ/8_Weeks/02/017_2636.cpp
@@ -1,31 +1,39 @@
 #include<iostream>
 #include<vector>
+#include<queue>
 using namespace std;
 
 const int dy[] = { -1,0,1,0 };
 const int dx[] = { 0,1,0,-1 };
 bool visited[101][101], board[101][101];
 int n, m, ret;
-vector<pair<int, int>> v;
+vector<pair<int, int>> v; //녹일 치즈들의 좌표 저장
 
-void dfs(int y, int x) {
+void bfs(int y, int x) {
 	visited[y][x] = true;
 
-	//치즈인지 아닌지 판단
-	if (board[y][x] == 1) {
-		v.push_back({ y,x });
-		return; //더 이상 치즈를 녹이지 않고 return
-	}
+	queue<pair<int, int>> q;
+	q.push({ y,x });
 
-	for (int i = 0; i < 4; i++) {
-		int ny = y + dy[i];
-		int nx = x + dx[i];
+	while (!q.empty()) {
+		pair<int, int> cur = q.front(); q.pop();
 
-		if (ny < 0 || ny >= n || nx < 0 || nx >= m) continue;
+		if (board[cur.first][cur.second] == 1) {
+			v.push_back({ cur.first,cur.second });
+			break;
+		}
 
-		//치즈인지 여부와 관계없이 일단 dfs 탐색
-		if (!visited[ny][nx]) {
-			dfs(ny, nx);
+		for (int i = 0; i < 4; i++) {
+			int ny = cur.first + dy[i];
+			int nx = cur.second + dx[i];
+
+			if (ny < 0 || ny >= n || nx < 0 || nx >= m) continue;
+
+			//치즈인지 여부와 관계없이 일단 탐색
+			if (!visited[ny][nx]) {
+				visited[ny][nx] = true;
+				bfs(ny, nx);
+			}
 		}
 	}
 
@@ -50,14 +58,15 @@ int main() {
 		v.clear();
 
 		//녹일 치즈 구하기. v에 치즈 좌표 저장
-		dfs(0, 0);
+		bfs(0, 0);
+
+		int cnt = v.size(); //녹이기 직전 치즈의 양
 
 		//치즈 녹이기
 		for (pair<int, int> i : v) {
 			board[i.first][i.second] = 0;
 		}
 
-		int cnt = v.size(); //녹이기 직전 치즈의 양
 		ret++; //녹인 횟수 증가
 
 		bool flag = false;
