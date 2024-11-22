1880d6c3194a8b2d2d947b1bc6f142b1a0984da3
gyunjongpark <idjkda4034@gmail.com>
2024-11-22 21:11:05 +0900

    Update 005_12869.cpp

--- BOJ/8_Weeks/03/005_12869.cpp
+++ BOJ/8_Weeks/03/005_12869.cpp
@@ -5,7 +5,6 @@ using namespace std;
 struct scv_hp {
     int a, b, c;
 };
-queue<scv_hp> q;
 
 int n, a[3], visited[61][61][61], ret;
 int atk[6][3]{ //공격할 수 있는 경우의 수
@@ -19,25 +18,26 @@ int atk[6][3]{ //공격할 수 있는 경우의 수
 
 void bfs(int a, int b, int c) {
     visited[a][b][c] = 1;
-    
+
+    queue<scv_hp> q;
     q.push({ a,b,c });
 
     while (!q.empty()) {
         scv_hp cur = q.front(); q.pop();
 
-        if (visited[0][0][0]) {
+        if (visited[0][0][0]) { //탈출 조건
             ret = visited[0][0][0] - 1;
             break;
         };
 
         for (int i = 0; i < 6; i++) {
-            int na = max(0, cur.a - atk[i][0]);
-            int nb = max(0, cur.b - atk[i][1]);
-            int nc = max(0, cur.c - atk[i][2]);
+            int nxt_a = max(0, cur.a - atk[i][0]);
+            int nxt_b = max(0, cur.b - atk[i][1]);
+            int nxt_c = max(0, cur.c - atk[i][2]);
 
-            if (!visited[na][nb][nc]) {
-                visited[na][nb][nc] = visited[cur.a][cur.b][cur.c] + 1;
-                q.push({ na,nb,nc });
+            if (!visited[nxt_a][nxt_b][nxt_c]) {
+                visited[nxt_a][nxt_b][nxt_c] = visited[cur.a][cur.b][cur.c] + 1;
+                q.push({ nxt_a,nxt_b,nxt_c });
             }
         }
     }
