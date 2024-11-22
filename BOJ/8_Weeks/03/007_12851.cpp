fba185efc88e608a7c9c22237d8139e81bc25c22
gyunjongpark <idjkda4034@gmail.com>
2024-11-22 21:35:30 +0900

    Update 007_12851.cpp

--- BOJ/8_Weeks/03/007_12851.cpp
+++ BOJ/8_Weeks/03/007_12851.cpp
@@ -7,26 +7,27 @@ int n, k, visited[200004];
 int cnt[200004];
 
 void bfs(int n) {
-    visited[n] = 1;
-    cnt[n] = 1;
+    visited[n] = 1; //방문 여부
+    cnt[n] = 1; //방문 횟수
 
     queue<int> q;
     q.push(n);
 
     while (!q.empty()) {
-        int now = q.front(); q.pop();
+        int cur = q.front(); q.pop();
 
-        if (now == k) break;
+        if (cur == k) break;
 
-        for (int next : {now - 1, now + 1, now * 2}) {
-            if (next >= 0 && next <= 100000) { //범위 체크                
-                if (!visited[next]) { //처음 방문하는 경우
-                    visited[next] = visited[now] + 1;
+        for (int next : {cur - 1, cur + 1, cur * 2}) {
+            if (0 <= next && next <= 100000) {            
+                if (!visited[next]) {
+                    visited[next] = visited[cur] + 1;
                     q.push(next);
-                    cnt[next] = cnt[now]; //경로 초기화
+
+                    cnt[next] = cnt[cur]; //방문 횟수 복사하기
                 }
-                else if (visited[next] == visited[now] + 1) { //최단 거리로 도달한 경우
-                    cnt[next] += cnt[now];
+                else if (visited[next] == visited[cur] + 1) { //최단 거리로 도달한 경우
+                    cnt[next] += cnt[cur]; //최단 거리로 방문한 횟수를 추가로 얹어서 갱신
                 }
             }
         }
