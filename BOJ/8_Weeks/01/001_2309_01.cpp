9e9311710d6174fd65de3983750fd122d6b63e33
gyunjongpark <idjkda4034@gmail.com>
2024-11-26 21:00:20 +0900

    Update 001_2309_01.cpp

--- BOJ/8_Weeks/01/001_2309_01.cpp
+++ BOJ/8_Weeks/01/001_2309_01.cpp
@@ -16,12 +16,18 @@ int main() {
 
     sort(a, a + 9); //먼저 정렬하지 않으면 7개의 원소를 담는 순서 벡터를 다시 정의해야 한다
 
+    bool flag = false;
+    
     for (int i = 0; i < 9; i++) {
         for (int j = 0; j < i; j++) {
-            if (sum - a[i] - a[j] == 100) {
+            if (sum - a[i] - a[j] == 100) {                
                 not_dwarf = { i,j };
+                flag = true;
+                
+                break;
             }
         }
+        if(flag) break;
     }
 
     for (int i = 0; i < 9; i++) {
