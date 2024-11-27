a19c86096b297116c0e405d548e8df010e59d3c4
gyunjongpark <idjkda4034@gmail.com>
2024-11-27 16:18:28 +0900

    Update 006_16637.cpp

--- BOJ/8_Weeks/03/006_16637.cpp
+++ BOJ/8_Weeks/03/006_16637.cpp
@@ -1,37 +1,31 @@
 #include<iostream>
 #include<vector>
-#include<climits> //INT_MIN
 using namespace std;
 
+int n, ret;
+string s;
 vector<int> num;
 vector<char> oper;
-string s;
-int n;
-int ret = INT_MIN;
 
-int cal(char a, int b, int c) {
-    if (a == '+') return b + c;
-    if (a == '-') return b - c;
-    if (a == '*') return b * c;
+int cal(char c, int a, int b) {
+    if (c == '+') return a + b;
+    if (c == '*') return a * b;
+    if (c == '-') return a - b;
 }
 
-//단, 괄호 안에는 연산자가 하나만 들어 있어야 한다. O(2 ^ n)
-void recur(int idx, int cur_num) {
+void recur(int idx, int cur) {
     if (idx == num.size() - 1) {
-        ret = max(ret, cur_num);
+        ret = max(ret, cur);
         return;
     }
 
     if (idx + 1 < num.size()) {
-        recur(idx + 1, cal(oper[idx], cur_num, num[idx + 1]));
+        recur(idx + 1, cal(oper[idx], cur, num[idx + 1]));
     }
 
     if (idx + 2 < num.size()) {
-        //뒷 연산을 괄호로 묶어서 먼저 계산
         int temp = cal(oper[idx + 1], num[idx + 1], num[idx + 2]);
-
-        //계산된 temp와 기존의 now를 나중에 연산
-        recur(idx + 2, cal(oper[idx], cur_num, temp));
+        recur(idx + 2, cal(oper[idx], cur, temp));
     }
 }
 
@@ -43,14 +37,14 @@ int main() {
 
     for (int i = 0; i < n; i++) {
         if (i % 2 == 0) {
-            num.push_back(s[i] - '0'); //숫자
+            num.push_back(s[i] - '0');
         }
         else {
-            oper.push_back(s[i]); //연산자
+            oper.push_back(s[i]);
         }
     }
 
-    recur(0, num[0]);
+    recur(0, num[0]); //반드시 첫번째 숫자부터 연산 시작
 
     cout << ret;
 
