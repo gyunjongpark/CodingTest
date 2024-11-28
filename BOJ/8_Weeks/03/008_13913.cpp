f93307814e5cb142b33a4f78f8ce4e226f1520c5
gyunjongpark <idjkda4034@gmail.com>
2024-11-28 20:19:04 +0900

    Update 008_13913.cpp

--- BOJ/8_Weeks/03/008_13913.cpp
+++ BOJ/8_Weeks/03/008_13913.cpp
@@ -4,8 +4,7 @@
 #include<algorithm> //reverse
 using namespace std;
 
-int n, k, visited[100001];
-int pre[100001];
+int n, k, visited[100001], pre[100001];
 vector<int> ret;
 
 void bfs(int n) {
@@ -20,7 +19,7 @@ void bfs(int n) {
         if (cur == k) break;
 
         for (int next : {cur - 1, cur + 1, cur * 2}) {
-            if (0 <= next && next <= 100000) {            
+            if (0 <= next && next <= 100000) {
                 if (!visited[next]) {
                     visited[next] = visited[cur] + 1;
                     q.push(next);
@@ -46,14 +45,16 @@ int main() {
 
     int cur = k;
 
-    while (true) { //cur가 출발점 n에 도달할 때까지
-        if (cur == n) break;
-        
+    while (true) {
+        if (cur == n) {
+            ret.push_back(cur);
+            break;
+        }
+
         ret.push_back(cur); //현재 노드 추가
         cur = pre[cur];     //이전 노드로 이동
     }
 
-    ret.push_back(n); //마지막 이전 노드 추가(첫 노드)
     reverse(ret.begin(), ret.end()); //역순으로 정렬
 
     for (int i : ret) cout << i << ' '; //경로 출력
