509c5cc1c807e78a13aaf0ae7848baeda82612e2
gyunjongpark <idjkda4034@gmail.com>
2024-12-01 22:25:37 +0900

    Create 001_19942.cpp

---
+++ BOJ/8_Weeks/04/001_19942.cpp
@@ -0,0 +1,59 @@
+#include<iostream>
+#include<vector>
+#include<climits> //INT_MAX
+using namespace std;
+
+struct info {
+	int p, f, s, v, c;
+};
+vector<info> ingre;
+
+int n;
+int mp, mf, ms, mv;
+int p, f, s, v, c;
+int ret = INT_MAX;
+vector<int> temp_combi, best_combi;
+
+void recur(int idx, int p, int f, int s, int v, int c) {
+	if (p >= mp && f >= mf && s >= ms && v >= mv) {
+		if (ret > c) {
+			ret = c;
+			best_combi = temp_combi;
+
+			return;
+		}
+	}
+
+	if (idx == n) return;
+
+	temp_combi.push_back(idx + 1);
+	recur(idx + 1, p + ingre[idx].p, f + ingre[idx].f, s + ingre[idx].s, v + ingre[idx].v, c + ingre[idx].c);
+	temp_combi.pop_back();
+
+	recur(idx + 1, p, f, s, v, c);
+}
+
+int main() {
+	ios_base::sync_with_stdio(false);
+	cin.tie(NULL); cout.tie(NULL);
+
+	cin >> n >> mp >> mf >> ms >> mv;
+
+	for (int i = 0; i < n; i++) {
+		cin >> p >> f >> s >> v >> c;
+		ingre.push_back({ p,f,s,v,c });
+	}
+
+	recur(0, 0, 0, 0, 0, 0); //idx, p, f, s, v, c
+
+	if (ret == INT_MAX) cout << -1;
+	else {
+		cout << ret << '\n';
+
+		for (int i : best_combi) {
+			cout << i << ' ';
+		}
+	}
+
+	return 0;
+}
