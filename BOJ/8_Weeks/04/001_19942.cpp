6c960212498ce63b8dbd48f9317c91c7919fcb48
gyunjongpark <idjkda4034@gmail.com>
2024-12-03 22:16:14 +0900

    Update 001_19942.cpp

--- BOJ/8_Weeks/04/001_19942.cpp
+++ BOJ/8_Weeks/04/001_19942.cpp
@@ -10,14 +10,14 @@ vector<info> ingre;
 
 int n;
 int mp, mf, ms, mv;
-int p, f, s, v, c;
+int p, f, s, v, cost;
 int ret = INT_MAX;
 vector<int> temp_combi, best_combi;
 
-void recur(int idx, int p, int f, int s, int v, int c) {
+void recur(int idx, int p, int f, int s, int v, int cost) {
 	if (p >= mp && f >= mf && s >= ms && v >= mv) {
-		if (ret > c) {
-			ret = c;
+		if (ret > cost) {
+			ret = cost;
 			best_combi = temp_combi;
 
 			return;
@@ -27,10 +27,11 @@ void recur(int idx, int p, int f, int s, int v, int c) {
 	if (idx == n) return;
 
 	temp_combi.push_back(idx + 1);
-	recur(idx + 1, p + ingre[idx].p, f + ingre[idx].f, s + ingre[idx].s, v + ingre[idx].v, c + ingre[idx].c);
-	temp_combi.pop_back();
+	recur(idx + 1, p + ingre[idx].p, f + ingre[idx].f, s + ingre[idx].s, v + ingre[idx].v, cost + ingre[idx].c);
 
-	recur(idx + 1, p, f, s, v, c);
+	temp_combi.pop_back(); //원상 복구
+
+	recur(idx + 1, p, f, s, v, cost); //변하지 않은 temp_combi인 채로 idx 증가
 }
 
 int main() {
@@ -40,11 +41,11 @@ int main() {
 	cin >> n >> mp >> mf >> ms >> mv;
 
 	for (int i = 0; i < n; i++) {
-		cin >> p >> f >> s >> v >> c;
-		ingre.push_back({ p,f,s,v,c });
+		cin >> p >> f >> s >> v >> cost;
+		ingre.push_back({ p,f,s,v,cost });
 	}
 
-	recur(0, 0, 0, 0, 0, 0); //idx, p, f, s, v, c
+	recur(0, 0, 0, 0, 0, 0); //idx, p, f, s, v, cost
 
 	if (ret == INT_MAX) cout << -1;
 	else {
