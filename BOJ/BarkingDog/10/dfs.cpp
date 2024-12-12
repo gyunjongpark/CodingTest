15e341a2e56b8867ff558906d07780f08b5c1b62
gyunjongpark <idjkda4034@gmail.com>
2024-12-12 22:05:14 +0900

    Update dfs.cpp

--- BOJ/BarkingDog/10/dfs.cpp
+++ BOJ/BarkingDog/10/dfs.cpp
@@ -1,15 +1,10 @@
-/*
-void dfs(int y, int x){
-    visited[y][x] = 1;
+/* pseudo code
+void dfs(int here) {
+    visited[here] = true;
 
-    for (int i = 0; i < 4; i++) {
-        int ny = y + dy[i];
-        int nx = x + dx[i];
-
-        if (ny < 0 || ny >= n || nx < 0 || nx >= m) continue;
-
-        if (board[ny][nx] && !visited[ny][nx]) {
-            dfs(ny, nx);
+    for (int there : adj[here]) {
+        if (!visited[there]) {
+            dfs(there);
         }
     }
 
