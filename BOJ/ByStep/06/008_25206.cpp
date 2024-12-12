5320ca1a3b16e2fbba7bc1b1fe9e950910ab4042
gyunjongpark <idjkda4034@gmail.com>
2024-12-12 09:24:35 +0900

    Update 008_25206.cpp

--- BOJ/ByStep/06/008_25206.cpp
+++ BOJ/ByStep/06/008_25206.cpp
@@ -5,9 +5,6 @@ string majorName, input;
 double sum, avg, grade[20], score[20];
 
 int main() {
-	ios_base::sync_with_stdio(false);
-	cin.tie(NULL), cout.tie(NULL);
-	
 	for (int i = 0; i < 20; ++i) {
 		cin >> majorName >> grade[i] >> input;
 
@@ -28,14 +25,10 @@ int main() {
 		avg += (score[i] * grade[i]);
 		sum += grade[i];
 	}
+
 	avg = avg / sum;
 
-	//정답과의 절대오차 또는 상대오차가 10^-4 이하이면 정답으로 인정한다
-	//평점은 최대 4.5점이므로 정수1자리 + 소수 4자리 = precision(5)
-	cout.precision(5);
-	cout << avg;
+	printf("%0.6f", avg);
 
-	//printf("%0.4f",avg);의 경우 cin, cout tie 사용 불가. 하지만 이 방법이 일반적이다
-	
 	return 0;
 }
