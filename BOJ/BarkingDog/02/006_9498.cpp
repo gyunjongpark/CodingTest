e3271fd66165c5bc6b92b7ec21404e32beda3c13
gyunjongpark <idjkda4034@gmail.com>
2024-12-13 13:37:59 +0900

    Create 006_9498.cpp

---
+++ BOJ/BarkingDog/02/006_9498.cpp
@@ -0,0 +1,19 @@
+#include<iostream>
+using namespace std;
+
+int a;
+
+int main() {
+	ios_base::sync_with_stdio(false);
+	cin.tie(NULL); cout.tie(NULL);
+
+	cin >> a;
+
+	if (a >= 90) cout << 'A';
+	else if (a >= 80) cout << 'B';
+	else if (a >= 70) cout << 'C';
+	else if (a >= 60) cout << 'D';
+	else cout << 'F';
+
+	return 0;
+}
