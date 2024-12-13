3eea578b52c8aacdb52c82f6978cf60c87ef51cf
gyunjongpark <idjkda4034@gmail.com>
2024-12-13 16:04:58 +0900

    Create 002_5397.cpp

---
+++ BOJ/BarkingDog/04/002_5397.cpp
@@ -0,0 +1,48 @@
+#include<iostream>
+#include<list>
+using namespace std;
+
+int t;
+string s;
+
+int main(void) {
+	ios_base::sync_with_stdio(false);
+	cin.tie(NULL); cout.tie(NULL);
+
+	cin >> t;
+
+	while (t--) {
+		list<char> L;
+		list<char>::iterator cur = L.end();
+
+		cin >> s;
+
+		for (char c : s) {
+			if (c == '<') {
+				if (cur != L.begin()) {
+					cur--;
+				}
+			}
+			else if (c == '>') {
+				if (cur != L.end()) {
+					cur++;
+				}
+			}
+			else if (c == '-') {
+				if (cur != L.begin()) {
+					cur--;
+					cur = L.erase(cur); //다음 이터레이터를 반환
+				}
+			}
+			else {
+				L.insert(cur, c);
+			}
+		}
+
+		for (char c : L) cout << c;
+
+		cout << '\n';
+	}
+
+	return 0;
+}
