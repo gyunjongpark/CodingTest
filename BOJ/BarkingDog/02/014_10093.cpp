638fd9de9ca6270b5d145f9b86e57f2b8c8406b3
gyunjongpark <idjkda4034@gmail.com>
2024-12-13 16:27:29 +0900

    Create 014_10093.cpp

---
+++ BOJ/BarkingDog/02/014_10093.cpp
@@ -0,0 +1,29 @@
+#include<iostream>
+using namespace std;
+typedef unsigned long long ll;
+
+ll a, b;
+
+int main() {
+	ios_base::sync_with_stdio(false);
+	cin.tie(NULL), cout.tie(NULL);
+
+	cin >> a >> b;
+
+	if (a > b) {
+		swap(a, b);
+	}
+
+	if (a == b || b - a == 1) {
+		cout << 0;
+	}
+	else {
+		cout << b - a - 1 << '\n';
+
+		for (ll i = a + 1; i < b; i++) {
+			cout << i << ' ';
+		}
+	}
+
+	return 0;
+}
