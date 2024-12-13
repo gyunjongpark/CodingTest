d9ee576bf9b553fd7f5238bc1d2b2520fcf10bd9
gyunjongpark <idjkda4034@gmail.com>
2024-12-13 16:28:32 +0900

    Create 015_1267.cpp

---
+++ BOJ/BarkingDog/02/015_1267.cpp
@@ -0,0 +1,30 @@
+#include<iostream>
+using namespace std;
+
+int n, y, m, temp;
+
+int main() {
+	ios_base::sync_with_stdio(false);
+	cin.tie(NULL), cout.tie(NULL);
+
+	cin >> n;
+
+	for (int i = 0; i < n; i++) {
+		cin >> temp;
+
+		y += (1 + (temp / 30)) * 10;
+		m += (1 + (temp / 60)) * 15;
+	}
+
+	if (y > m) {
+		cout << "M " << m;
+	}
+	else if (y < m) {
+		cout << "Y " << y;
+	}
+	else {
+		cout << "Y M " << y;
+	}
+
+	return 0;
+}
