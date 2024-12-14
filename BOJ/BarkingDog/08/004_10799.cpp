2b50f0708bf65ad64037f68848c90db8fee62960
gyunjongpark <idjkda4034@gmail.com>
2024-12-14 11:32:31 +0900

    Create 004_10799.cpp

---
+++ BOJ/BarkingDog/08/004_10799.cpp
@@ -0,0 +1,36 @@
+#include<iostream>
+#include<stack>
+using namespace std;
+
+string s;
+stack<char> stk;
+int ret;
+
+int main() {
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL); cout.tie(NULL);
+
+    cin >> s;
+
+    for (int i = 0; i < s.size(); i++) {
+        if (s[i] == '(') {
+            stk.push(s[i]);
+        }
+
+        if (s[i] == ')') {
+            stk.pop();
+
+            //기존의 stk.top()이 아닌 직전 문자열 s[i - 1]을 고려한다
+            if (s[i - 1] == '(') {
+                ret += stk.size();
+            }
+            else {
+                ret++;
+            }
+        }
+    }
+
+    cout << ret;
+
+    return 0;
+}
