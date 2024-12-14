764c281530f5ee44a305676033dff5d8c318539c
gyunjongpark <idjkda4034@gmail.com>
2024-12-14 10:37:27 +0900

    Update 001_1406.cpp

--- BOJ/BarkingDog/04/001_1406.cpp
+++ BOJ/BarkingDog/04/001_1406.cpp
@@ -2,49 +2,51 @@
 #include<list>
 using namespace std;
 
-list<char> lst;
-string input_str;
-char ch, add;
+list<char> L;
+string s;
 int n;
+char ch, add;
 
 int main(void) {
 	ios_base::sync_with_stdio(false);
 	cin.tie(NULL); cout.tie(NULL);
 
-	cin >> input_str >> n;
+	cin >> s >> n;
 
-	for (char c : input_str) {
-		lst.push_back(c);
+	for (char c : s) {
+		L.push_back(c);
 	}
 
-	list<char>::iterator cur = lst.end();
+	list<char>::iterator cur = L.end();
 
 	while (n--) {
 		cin >> ch;
 
 		if (ch == 'P') {
 			cin >> add;
-			lst.insert(cur, add); //작업 후 cur는 다음 이터레이터를 반환합니다
+			L.insert(cur, add); //작업 후 cur는 다음 이터레이터를 반환합니다
 		}
 		else if (ch == 'L') {
-			if (cur != lst.begin()) {
+			if (cur != L.begin()) {
 				cur--;
 			}
 		}
 		else if (ch == 'D') {
-			if (cur != lst.end()) {
+			if (cur != L.end()) {
 				cur++;
 			}
 		}
 		else if (ch == 'B') {
-			if (cur != lst.begin()) {
+			if (cur != L.begin()) {
 				cur--; //커서 '왼쪽'에 있는 문자를 삭제하기 위해
-				cur = lst.erase(cur); //작업 후 cur는 다음 이터레이터를 반환합니다
+				cur = L.erase(cur); //작업 후 cur는 다음 이터레이터를 반환합니다
 			}
 		}
 	}
 
-	for (char c : lst) cout << c;
+	for (char c : L) {
+		cout << c;
+	}
 
 	return 0;
 }
