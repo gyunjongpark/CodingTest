236a79cae0dadacb7e0928abc22354d3734353fb
gyunjongpark <idjkda4034@gmail.com>
2024-12-14 10:56:17 +0900

    Update 007_3015.cpp

--- BOJ/BarkingDog/05/007_3015.cpp
+++ BOJ/BarkingDog/05/007_3015.cpp
@@ -18,17 +18,17 @@ int main(void) {
 
 		int cnt = 1; //입력된 사람과 같은 키인 사람의 수(본인 포함)
 
-		while (!stk.empty() && stk.top().first <= h) { //입력된 키가 스택의 키보다 크거나 같다면
-			ret += stk.top().second; //스택에 있는 사람들을 위에서부터 차례대로 만날 수 있다
+		while (!stk.empty() && stk.top().first <= h) { //입력된 키가 크거나 같을 때 점검 시작
+			ret += stk.top().second; //스택에 있는 사람들을 top부터 차례대로 만날 수 있다
 
-			if (stk.top().first == h) { //특별히 스택에서 같은 키를 가진 사람을 만날 때
-				cnt += stk.top().second; //입력된 사람과 같은 키인 사람의 수를 포용한다
+			if (stk.top().first == h) { //특별히 같은 키를 가진 사람을 만날 때
+				cnt += stk.top().second; //현재 키에 대한 cnt에 만난 사람의 cnt를 추가하여 갱신한다
 			}
 
 			stk.pop();
 		}
 
-		if (!stk.empty()) { //입력된 키보다 큰 키가 남아있는 경우, 결과 쌍에 1 추가
+		if (!stk.empty()) { //스택에 입력된 키보다 큰 키가 남아있는 경우, 결과 쌍에 1 추가(이전 사람들은 볼 수 없으므로)
 			ret++;
 		}
 
